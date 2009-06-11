572b3c893669dc96c2d06a7d6bcc1ad08cdc0069
chead <chead@localhost>
2009-06-11 05:08:06 +0000

    Switch to timer2 for RTC, freeing up timer0 for other use.

--- firmware/Bot/rtc.c
+++ firmware/Bot/rtc.c
@@ -5,20 +5,21 @@
 
 volatile unsigned long rtc_counter;
 
-ISR(TIMER0_COMP_vect, ISR_BLOCK) {
+ISR(TIMER2_COMP_vect, ISR_BLOCK) {
 	rtc_counter++;
 }
 
 void rtc_init(void) {
-#if F_CPU % 128 != 0 || (F_CPU / 128) % 1000 != 0 || F_CPU / 128 / 1000 > 255
+#define PRESCALE_BITS (_BV(CS21) | _BV(CS20));
+#define PRESCALE_VALUE 64
+#if F_CPU % PRESCALE_VALUE != 0 || (F_CPU / PRESCALE_VALUE) % 1000 != 0 || F_CPU / PRESCALE_VALUE / 1000 > 255
 #error Fix the math in this code!
 #endif
 	// NOTE: We're using the primary 16MHz crystal here.
 	// Should we use the 32768Hz crystal on TOsc?
 	// It's probably a precision crystal, but it would give worse theoretical results (not exactly 1ms)!
-	rtc_counter = 1;
-	TCCR0 = _BV(WGM01) | _BV(CS02) | _BV(CS00);
-	OCR0 = F_CPU / 128 / 1000;
-	TIMSK |= _BV(OCIE0);
+	TCCR2 = _BV(WGM21) | PRESCALE_BITS;
+	OCR2 = F_CPU / PRESCALE_VALUE / 1000;
+	TIMSK |= _BV(OCIE2);
 }
 
