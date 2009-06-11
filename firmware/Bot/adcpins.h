13f04a0aef6e57bdbb25bff455f86f70910e119d
chead <chead@localhost>
2009-06-11 19:52:36 +0000

    Use new ADC framework.

--- firmware/Bot/adcpins.h
+++ firmware/Bot/adcpins.h
@@ -9,24 +9,20 @@
 void adc_init(void);
 
 /*
+ * Takes a set of ADC samples.
+ */
+void adc_sample(void);
+
+/*
  * DO NOT USE THIS VARIABLE!
  */
-extern volatile int16_t adc_results[8];
+extern int16_t adc_results[8];
 
 /*
  * Reads a value from the ADC.
  */
-static int16_t adc_read(uint8_t pin) __attribute__((__always_inline__, __unused__));
-static int16_t adc_read(uint8_t pin) {
-	uint8_t oldsreg;
-	int16_t ret;
-
-	oldsreg = SREG;
-	cli();
-	ret = adc_results[pin];
-	SREG = oldsreg;
-	return ret;
-}
+#define adc_read(pin) \
+	adc_results[pin]
 
 #endif
 
