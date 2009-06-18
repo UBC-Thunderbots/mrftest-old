874e79c1771f9126fbf9c67294097217916424a1
simonsyd <simonsyd@localhost>
2009-06-18 03:47:02 +0000

    geometry stuff

--- software/include/AI/RobotAlgorithmUtils.h
+++ software/include/AI/RobotAlgorithmUtils.h
@@ -34,5 +34,37 @@ bool seg_crosses_seg(const Vector2 &a1, const Vector2 &a2, const Vector2 &b1, co
 // returns an index
 // int get_nearest_player(const Vector2& P, const AITeam& team, int mask = 0);
 
+// a = goal post position
+// b = other goal post position
+// c = ball position
+// the goalie will move tangent to a
+// R = radius of goalie defence circle
+// r = radius of robot
+Vector2 calcBlockGoalie(const Vector2& a, const Vector2& b, const Vector2& c,
+	double R, double r);
+
+// reflects the ray r incident on origin, with normal n
+Vector2 reflect(const Vector2&v, const Vector2& n);
+
+// a = goal post position
+// c = ball position
+// g = goalie position
+// returns the other ray that is not blocked by goalie
+Vector2 calcBlockOtherRay(const Vector2& a, const Vector2& c, const Vector2& g);
+
+// a = goal post position
+// b = other goal post position
+// c = ball position
+// g = goalie position
+// checks if goalie blocks goal post
+bool goalieBlocksGoalPost(const Vector2& a, const Vector2& b, const Vector2& c, const Vector2& g);
+
+// a = goal post position
+// b = other goal post position
+// c = ball position
+// g = goalie position
+// finds a defender position to block the ball
+Vector2 defenderBlocksGoalPost(const Vector2& a, const Vector2& b, const Vector2& c, const Vector2& g, double r);
+
 #endif
 
--- software/src/AI/RobotAlgorithmUtils.cpp
+++ software/src/AI/RobotAlgorithmUtils.cpp
@@ -6,7 +6,9 @@
 
 typedef std::pair<Vector2, Vector2> Line;
 
-const double eps = 1e-9;
+namespace {
+	const double eps = 1e-9;
+}
 
 Vector2 line_intersect(const Vector2 &a, const Vector2 &b, const Vector2 &c, const Vector2 &d) {
 	assert((d - c).cross(b - a) != 0);
@@ -34,5 +36,36 @@ bool seg_crosses_seg(const Vector2& a1, const Vector2& a2, const Vector2 &b1, co
 		* sign((a2 - a1).cross(b2 - a1)) <= 0 &&
 		sign((b2 - b1).cross(a1 - b1))
 		* sign((b2 - b1).cross(a2 - b1)) <= 0;
+}
+
+Vector2 calcBlockGoalie(const Vector2& a, const Vector2& b, const Vector2& c,
+	double R, double r) {
+	// P = intersection of the ray and defence circle
+	Vector2 normal = c - a;
+	normal /= normal.length();
+	Vector2 P = a + normal * R;
+	Vector2 tangent;
+	if((a - c).cross(b - c) > 0) tangent = normal.rotate(-90);
+	else tangent = normal.rotate(90);
+	return P + tangent * r;
+}
+
+Vector2 reflect(const Vector2&v, const Vector2& n) {
+	Vector2 normal = n / n.length();
+	return 2 * v.dot(normal) * normal - v;
+}
+
+Vector2 calcBlockOtherRay(const Vector2& a, const Vector2& c, const Vector2& g) {
+	return reflect(a - c, g - c);
+}
+
+bool goalieBlocksGoalPost(const Vector2& a, const Vector2& b, const Vector2& c, const Vector2& g) {
+	Vector2 R = reflect(a - c, g - c);
+	return (R.cross(b - c) < -eps);
+}
+
+Vector2 defenderBlocksGoalPost(const Vector2& a, const Vector2& b, const Vector2& c, const Vector2& g, double r) {
+	Vector2 R = reflect(a - c, g- c);
+	return calc_block_ray(R, b - c, r) + c;
 }
 
