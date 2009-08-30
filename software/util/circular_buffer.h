e1239311fcec3a6853817f9ebda16ecf624ff7e7
chead <chead@localhost>
2009-08-30 22:58:48 +0000

    Implement the circular buffer.

--- software/util/circular_buffer.h
+++ software/util/circular_buffer.h
@@ -12,24 +12,32 @@ class circular_buffer {
 		//
 		// Constructs a new circular buffer.
 		//
-		circular_buffer();
+		circular_buffer() : ptr(0) {
+		}
 
 		//
 		// Gets the object at position "i" in the buffer, where 0 is the
 		// most-recently added, 1 is the next-most-recently-added, and so on.
 		//
-		const T &operator[](const std::size_t i) const;
+		const T &operator[](const std::size_t i) const {
+			return data[(ptr + i) % size];
+		}
 
 		//
 		// Gets the object at position "i" in the buffer, where 0 is the
 		// most-recently added, 1 is the next-most-recently-added, and so on.
 		//
-		T &operator[](const std::size_t i);
+		T &operator[](const std::size_t i) {
+			return data[(ptr + i) % size];
+		}
 
 		//
 		// Adds a new object to the buffer.
 		//
-		void add(const T &t);
+		void add(const T &t) {
+			ptr = (ptr + size - 1) % size;
+			data[ptr] = t;
+		}
 
 		//
 		// The size of the buffer.
