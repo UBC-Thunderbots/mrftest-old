ea3cc5af41a0e5786defd304a0f550f0db4f449b
chead <chead@localhost>
2009-09-21 06:07:15 +0000

    Add a file specifying the timestep rate.

---
+++ software/world/timestep.h
@@ -0,0 +1,12 @@
+#ifndef WORLD_TIMESTEP_H
+#define WORLD_TIMESTEP_H
+
+namespace {
+	//
+	// The number of AI timesteps per second.
+	//
+	const unsigned int TIMESTEPS_PER_SECOND = 15;
+}
+
+#endif
+
