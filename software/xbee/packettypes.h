e21949574d370e7721ae9278bcf173e80f5ed185
chead <chead@localhost>
2009-10-21 23:30:01 +0000

    Detemplatize a bit.

--- software/xbee/packettypes.h
+++ software/xbee/packettypes.h
@@ -18,7 +18,6 @@ namespace xbeepacket {
 	const uint8_t REMOTE_AT_REQUEST_APIID = 0x17;
 	const uint8_t REMOTE_AT_REQUEST_OPTION_APPLY = 0x02;
 
-	template<std::size_t T_value_length>
 	struct REMOTE_AT_RESPONSE {
 		uint8_t apiid;
 		uint8_t frame;
@@ -26,7 +25,7 @@ namespace xbeepacket {
 		uint8_t address16[2];
 		uint8_t command[2];
 		uint8_t status;
-		uint8_t value[T_value_length];
+		uint8_t value[];
 	} __attribute__((__packed__));
 	const uint8_t REMOTE_AT_RESPONSE_APIID = 0x97;
 	const uint8_t REMOTE_AT_RESPONSE_STATUS_OK = 0;
@@ -35,25 +34,21 @@ namespace xbeepacket {
 	const uint8_t REMOTE_AT_RESPONSE_STATUS_INVALID_PARAMETER = 3;
 	const uint8_t REMOTE_AT_RESPONSE_STATUS_NO_RESPONSE = 4;
 
-	template<typename T_value>
-	struct TRANSMIT {
+	struct TRANSMIT_HDR {
 		uint8_t apiid;
 		uint8_t frame;
 		uint8_t address[8];
 		uint8_t options;
-		T_value data;
 	} __attribute__((__packed__));
 	const uint8_t TRANSMIT_APIID = 0x00;
 	const uint8_t TRANSMIT_OPTION_DISABLE_ACK = 0x01;
 	const uint8_t TRANSMIT_OPTION_BROADCAST_PANID = 0x04;
 
-	template<typename T_value>
-	struct RECEIVE {
+	struct RECEIVE_HDR {
 		uint8_t apiid;
 		uint8_t address[8];
 		uint8_t rssi;
 		uint8_t options;
-		T_value data;
 	} __attribute__((__packed__));
 	const uint8_t RECEIVE_APIID = 0x80;
 	const uint8_t RECEIVE_OPTION_BROADCAST_ADDRESS = 0x02;
