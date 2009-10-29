0eb664e5efaa91e7dbdeffc27ef4ce302084cac0
chead <chead@localhost>
2009-10-29 04:33:52 +0000

    Make file_descriptor have auto_ptr semantics.

--- software/util/fd.cpp
+++ software/util/fd.cpp
@@ -1,10 +1,10 @@
 #include "util/fd.h"
 #include <unistd.h>
 
-file_descriptor::file_descriptor(int fd) : fd(fd) {
-}
-
-file_descriptor::~file_descriptor() {
-	close(fd);
+void file_descriptor::close() {
+	if (fd != -1) {
+		::close(fd);
+		fd = -1;
+	}
 }
 
--- software/util/fd.h
+++ software/util/fd.h
@@ -1,27 +1,65 @@
 #ifndef UTIL_FD_H
 #define UTIL_FD_H
 
-#include "util/noncopyable.h"
+#include <cassert>
 
 //
 // A file descriptor that is safely closed on destruction.
+// Ownership semantics are equivalent to std::auto_ptr.
 //
-class file_descriptor : public virtual noncopyable {
+class file_descriptor {
 	public:
 		//
 		// Constructs a new file_descriptor.
 		//
-		file_descriptor(int fd);
+		file_descriptor(int fd) : fd(fd) {
+		}
+
+		//
+		// Copies a file_descriptor, transferring ownership.
+		//
+		file_descriptor(file_descriptor &copyref) {
+			assert(copyref.fd != -1);
+			fd = copyref.fd;
+			copyref.fd = -1;
+		}
 
 		//
 		// Destroys a file_descriptor.
 		//
-		virtual ~file_descriptor();
+		virtual ~file_descriptor() {
+			close();
+		}
+
+		//
+		// Assigns a new value to a file_descriptor.
+		//
+		file_descriptor &operator=(file_descriptor &assgref) {
+			assert(assgref.fd != -1);
+			close();
+			fd = assgref.fd;
+			assgref.fd = -1;
+		}
+
+		//
+		// Assigns a new value to a file_descriptor.
+		//
+		file_descriptor &operator=(int newfd) {
+			assert(newfd != -1);
+			close();
+			fd = newfd;
+		}
+
+		//
+		// Closes the descriptor.
+		//
+		void close();
 
 		//
 		// Returns the descriptor.
 		//
 		operator int() const {
+			assert(fd != -1);
 			return fd;
 		}
 
