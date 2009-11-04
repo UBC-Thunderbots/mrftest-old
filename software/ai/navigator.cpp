ae2b22a5ec57c8dabbeeb439c9ed228890b669b2
chead <chead@localhost>
2009-11-04 01:55:53 +0000

    Fix indentation style to match other files.

--- software/ai/navigator.cpp
+++ software/ai/navigator.cpp
@@ -1,11 +1,6 @@
 #include "ai/navigator.h"
 #include "world/field.h"
 
-navigator::navigator(player::ptr player, field::ptr field, ball::ptr ball, team::ptr team) : 
-  the_player(player), 
-  the_field(field), 
-  the_ball(ball), 
-  the_team(team) 
-{
+navigator::navigator(player::ptr player, field::ptr field, ball::ptr ball, team::ptr team) : the_player(player), the_field(field), the_ball(ball), the_team(team) {
 }
 
