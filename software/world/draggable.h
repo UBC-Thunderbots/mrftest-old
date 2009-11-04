e27867bfc357a3b2926af2e910eeb24f04ffa6da
chead <chead@localhost>
2009-11-04 23:36:45 +0000

    Make draggable extend byref so visualizer can store a draggable::ptr.

--- software/world/draggable.h
+++ software/world/draggable.h
@@ -2,12 +2,19 @@
 #define WORLD_DRAGGABLE_H
 
 #include "geom/point.h"
+#include "util/byref.h"
+#include <glibmm.h>
 
 //
 // An object that can be dragged around in the visualizer.
 //
-class draggable {
+class draggable : public virtual byref {
 	public:
+		//
+		// A pointer to a draggable.
+		//
+		typedef Glib::RefPtr<draggable> ptr;
+
 		//
 		// Sets the position of the object. This should ONLY be called from the
 		// UI, not from the AI!
