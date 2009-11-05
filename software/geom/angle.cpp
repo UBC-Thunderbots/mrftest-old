da3c821b302582dee5ad209ebe6667e4d642d94a
chead <chead@localhost>
2009-11-05 22:50:23 +0000

    Remove unused TWO_PI symbol. Redefine PI as a const instead of #define.

--- software/geom/angle.cpp
+++ software/geom/angle.cpp
@@ -1,9 +1,12 @@
 #include "geom/angle.h"
+#include <cmath>
+
+const double PI = std::acos(-1.0);
 
 double angle_mod(double a) {
-	a -= TWO_PI * rint(a / TWO_PI);
-	if (a < PI) a += TWO_PI;
-	if (a > PI) a -= TWO_PI;
+	a -= 2 * PI * rint(a / (2 * PI));
+	if (a < PI) a += 2 * PI;
+	if (a > PI) a -= 2 * PI;
 	return a;
 }
 
--- software/geom/angle.h
+++ software/geom/angle.h
@@ -1,11 +1,7 @@
 #ifndef GEOM_ANGLE_H
 #define GEOM_ANGLE_H
 
-#include <cmath>
-
-#define PI (std::acos(-1.))
-#define TWO_PI (PI * 2)
-
+extern const double PI;
 double angle_mod(double angle);
 
 #endif
