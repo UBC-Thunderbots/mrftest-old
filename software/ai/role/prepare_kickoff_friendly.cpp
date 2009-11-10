6768915d887c4f16e0ec335a40e457c0a7fe1377
dyuckin <dyuckin@localhost>
2009-11-10 02:05:39 +0000

    implemented the prepare friendly kickoff role

--- software/ai/role/prepare_kickoff_friendly.cpp
+++ software/ai/role/prepare_kickoff_friendly.cpp
@@ -4,5 +4,23 @@ prepare_kickoff_friendly::prepare_kickoff_friendly(ball::ptr ball, field::ptr fi
 }
 
 void prepare_kickoff_friendly::tick(){
-	
+   for(int i=0; i<the_tactics.size(); i++) {
+        the_tactics[i]->tick();
+   }
+}
+
+void prepare_kickoff_friendly::set_robots(const std::vector<player::ptr> &robots) {
+    role::set_robots(robots);
+    
+    the_tactics.clear();
+    for(int i=0; i<robots.size() ; i++) {
+        move::ptr tactic( new move(the_ball, the_field, the_team, robots[i]));
+        the_tactics.push_back(tactic);
+    }
+    
+    the_tactics[0]->set_position(point( -1 * the_field->length()/10 , 0));
+    the_tactics[1]->set_position(point( -1 * the_field->length()/4, -1* the_field->width()/6 ));
+    the_tactics[2]->set_position(point( -1 * the_field->length()/4, the_field->width()/6 ));
+    the_tactics[3]->set_position(point( -1 * the_field->length() *0.4 , -1 * the_field->width()/4 ));
+    the_tactics[4]->set_position(point( -1 * the_field->length() * 0.4 , the_field->width()/4 ));
 }
