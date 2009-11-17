f41c06fbab6b41f538b75a4b43e9ee42621e6833
chead <chead@localhost>
2009-11-17 02:36:07 +0000

    Make window bigger by default.

--- software/uicomponents/visualizer.cpp
+++ software/uicomponents/visualizer.cpp
@@ -1,6 +1,7 @@
 #include "uicomponents/visualizer.h"
 
 visualizer::visualizer(const field::ptr field, const ball::ptr ball, const team::ptr west_team, const team::ptr east_team, clocksource &clk) : scale(1), xtranslate(0), ytranslate(0), the_field(field), the_ball(ball), west_team(west_team), east_team(east_team) {
+	set_size_request(600, 600);
 	add_events(Gdk::POINTER_MOTION_MASK);
 	add_events(Gdk::BUTTON_PRESS_MASK);
 	add_events(Gdk::BUTTON_RELEASE_MASK);
