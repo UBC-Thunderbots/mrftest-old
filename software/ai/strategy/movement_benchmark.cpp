403beb4cd4ad34a76d0f5b3a15f876dd74c432c1
simonsyd <simonsyd@localhost>
2009-11-17 02:06:38 +0000

    Improved movement benchmark

--- software/ai/strategy/movement_benchmark.cpp
+++ software/ai/strategy/movement_benchmark.cpp
@@ -28,30 +28,25 @@ namespace {
 		destination.x += 1;
 		time_steps = 0;
 		done = false;
-		dis_threshold = 0.1;
-		vel_threshold = 0.01;
+		dis_threshold = 1e-2;
+		vel_threshold = 1e-4;
 	}
 
 	void movement_benchmark::tick() {
 		if (!done) {
 			time_steps++;
-			double xDiff = the_team->get_player(0)->position().x - destination.x;
-			double yDiff = the_team->get_player(0)->position().y - destination.y;
-			if (xDiff < 0) xDiff *= -1;
-			if (yDiff < 0) yDiff *= -1;
+			point diff = the_team->get_player(0)->position() - destination;
 			point vel = the_team->get_player(0)->est_velocity();
-			if (vel.x < 0) vel.x *= -1;
-			if (vel.y < 0) vel.y *= -1;
-			std::cout << "position: " << xDiff << " " << yDiff << std::endl;
-			std::cout << "velocity: " << the_team->get_player(0)->est_velocity().x << " " << the_team->get_player(0)->est_velocity().x << std::endl;
-			if (xDiff < dis_threshold && yDiff < dis_threshold && vel.x < vel_threshold && vel.y < vel_threshold) {
+			std::cout << "position: " << diff.x << " " << diff.y << std::endl;
+			std::cout << "velocity: " << the_team->get_player(0)->est_velocity().x << " " << the_team->get_player(0)->est_velocity().y << std::endl;
+			if (diff.len() < dis_threshold && vel.len() < vel_threshold) {
 				std::cout << "time steps taken: " << time_steps << std::endl;
 				done = true;
 			}
-			move::ptr mover (new move(the_ball, the_field, the_team, the_team->get_player(0)));
-			mover->set_position(destination);
-			mover->tick();
 		}
+		move::ptr mover (new move(the_ball, the_field, the_team, the_team->get_player(0)));
+		mover->set_position(destination);
+		mover->tick();
 	}
 
 	void movement_benchmark::set_playtype(playtype::playtype) {
