1d400a0826643311e2537ba43fcb14d0caf8dce1
JasonChang <jasonchang@localhost>
2009-11-17 04:04:54 +0000

    Set a larger error tolerance for calculations to prevent engine from crashing

--- software/simulator/engines/ode_simulator.cpp
+++ software/simulator/engines/ode_simulator.cpp
@@ -56,9 +56,8 @@ namespace {
 				ballODE::ptr b(new ballODE(eworld, space));
 				the_ball = b;
 				
-
  				dWorldSetLinearDamping (eworld, 0.02);
-				dWorldSetCFM (eworld, 0.2);
+				dWorldSetCFM (eworld, 0.9);
  				
 			}
 			
@@ -66,13 +65,11 @@ namespace {
 				the_players.clear();
  				the_ball.reset();
  				dWorldDestroy (eworld);
- 				 dJointGroupDestroy (contactgroup);
-
+ 				dJointGroupDestroy (contactgroup);
 			}
 
 
 			void tick() {
-			
 				//check the world for possible collisions
 				//if there are colliding objects then call nearCallback
 				//nearCallback creates all necessary contact points and parameters
@@ -127,114 +124,114 @@ namespace {
 				return 0;
 			}
 
-	//
-	//if a shape interescts with the ground set the contact parameters
-	//
-	void handleCollisionWithGround(dGeomID o1, dGeomID o2){
-		int g1 = (o1 == the_ball->ballGeom);
-		int g2 = (o2 == the_ball->ballGeom);
-		double frict = MU;
-		int i=0;		
-		if ((g1 ^ g2)){
-			frict = MU*60;
-		}
-		  dBodyID b1 = dGeomGetBody(o1);
-		  dBodyID b2 = dGeomGetBody(o2);
+			//
+			//if a shape interescts with the ground set the contact parameters
+			//
+			void handleCollisionWithGround(dGeomID o1, dGeomID o2){
+				int g1 = (o1 == the_ball->ballGeom);
+				int g2 = (o2 == the_ball->ballGeom);
+				double frict = MU;
+				int i=0;		
+				if ((g1 ^ g2)){
+					frict = MU*60;
+				}
+				  dBodyID b1 = dGeomGetBody(o1);
+				  dBodyID b2 = dGeomGetBody(o2);
 
-		  dContact contact[3];		// up to 3 contacts per box
-		  for (i=0; i<3; i++) {
-		    contact[i].surface.mode = dContactSoftCFM | dContactApprox1;
-		    contact[i].surface.mu = frict;
-		    contact[i].surface.soft_cfm = 0.01;
-		  }
-		  if (int numc = dCollide (o1,o2,3,&contact[0].geom,sizeof(dContact))) {
-		    for (i=0; i<numc; i++) {
-		      dJointID c = dJointCreateContact (eworld,contactgroup,contact+i);
-		      dJointAttach (c,b1,b2);
-		    }
-		  }
-	}
-	
-	//
-	//if a shape interescts with the ball set the contact parameters
-	//
-	void handleBallCollision (dGeomID o1, dGeomID o2){
-		int i=0;
-		  dBodyID b1 = dGeomGetBody(o1);
-		  dBodyID b2 = dGeomGetBody(o2);
-		  dContact contact[3];		// up to 3 contacts per box
-		  for (i=0; i<3; i++) {
-		    contact[i].surface.mode = dContactSoftCFM | dContactApprox1;
-		    contact[i].surface.mu = MU;
-		    contact[i].surface.soft_cfm = 0.01;
-		  }
-		  if (int numc = dCollide (o1,o2,3,&contact[0].geom,sizeof(dContact))) {
-		    for (i=0; i<numc; i++) {
-		      dJointID c = dJointCreateContact (eworld,contactgroup,contact+i);
-		      dJointAttach (c,b1,b2);
-		    }
-		  }	
-	}
-	
-	//
-	//if ground or ball isn't invloved, we assume a robot robot collision
-	//
-	void handleRobotRobotCollision (dGeomID o1, dGeomID o2){
-		int i=0;
-		const dReal* pos1 = dGeomGetPosition (o1);
-		const dReal* pos2 = dGeomGetPosition (o2);
-		point p1 = point(pos1[0],pos1[1]);
-		point p2 = point(pos2[0],pos2[1]);
-		point dis = p1-p2;
-		//if (dis.len>0.02) we assume that are components from same robot
-		//as such we will ignore it
-		//this is a pretty bad hack and needs to be changed to check whether the geoms come
-		//from the same robot
-		if(dis.len()>0.02){
-			  dBodyID b1 = dGeomGetBody(o1);
-		  	  dBodyID b2 = dGeomGetBody(o2);
-			  dContact contact[3];		// up to 3 contacts per box
-			  for (i=0; i<3; i++) {
-			    contact[i].surface.mode = dContactSoftCFM | dContactApprox1;
-			    contact[i].surface.mu = MU;
-			    contact[i].surface.soft_cfm = 0.01;
-			  }
-			  if (int numc = dCollide (o1,o2,3,&contact[0].geom,sizeof(dContact))) {
-			    for (i=0; i<numc; i++) {
-			      dJointID c = dJointCreateContact (eworld,contactgroup,contact+i);
-			      dJointAttach (c,b1,b2);
-			    }
-			  }
-		}
-	}
-
-	//			
-	//This gets called every time we have two shpaes in the world that intersect
-	//for every pair of intersecting shapes we need to decide what to do with them
-	//
-	void nearCallback (dGeomID o1, dGeomID o2)
-	{
-		int groundCollision;
-		int notGroundCollision;
-		int g1 = (o1 == ground);
-		int g2 = (o2 == ground);
-		groundCollision = (g1 ^ g2);
-		notGroundCollision = !groundCollision;				
-		if (groundCollision) {
-			handleCollisionWithGround(o1, o2);
-		}else if(notGroundCollision) {
-			int ballCollision;
-			g1 = (o1 == the_ball->ballGeom);
-			g2 = (o2 == the_ball->ballGeom);
-			ballCollision = (g1 ^ g2);			
-			if (ballCollision){
-				handleBallCollision(o1, o2);
-			}else{
-				handleRobotRobotCollision (o1, o2);
+				  dContact contact[3];		// up to 3 contacts per box
+				  for (i=0; i<3; i++) {
+				    contact[i].surface.mode = dContactSoftCFM | dContactApprox1;
+				    contact[i].surface.mu = frict;
+				    contact[i].surface.soft_cfm = 0.01;
+				  }
+				  if (int numc = dCollide (o1,o2,3,&contact[0].geom,sizeof(dContact))) {
+				    for (i=0; i<numc; i++) {
+				      dJointID c = dJointCreateContact (eworld,contactgroup,contact+i);
+				      dJointAttach (c,b1,b2);
+				    }
+				  }
 			}
-		}
-	}
-	
+			
+			//
+			//if a shape interescts with the ball set the contact parameters
+			//
+			void handleBallCollision (dGeomID o1, dGeomID o2){
+				int i=0;
+				  dBodyID b1 = dGeomGetBody(o1);
+				  dBodyID b2 = dGeomGetBody(o2);
+				  dContact contact[3];		// up to 3 contacts per box
+				  for (i=0; i<3; i++) {
+				    contact[i].surface.mode = dContactSoftCFM | dContactApprox1;
+				    contact[i].surface.mu = MU;
+				    contact[i].surface.soft_cfm = 0.01;
+				  }
+				  if (int numc = dCollide (o1,o2,3,&contact[0].geom,sizeof(dContact))) {
+				    for (i=0; i<numc; i++) {
+				      dJointID c = dJointCreateContact (eworld,contactgroup,contact+i);
+				      dJointAttach (c,b1,b2);
+				    }
+				  }	
+			}
+			
+			//
+			//if ground or ball isn't invloved, we assume a robot robot collision
+			//
+			void handleRobotRobotCollision (dGeomID o1, dGeomID o2){
+				int i=0;
+				const dReal* pos1 = dGeomGetPosition (o1);
+				const dReal* pos2 = dGeomGetPosition (o2);
+				point p1 = point(pos1[0],pos1[1]);
+				point p2 = point(pos2[0],pos2[1]);
+				point dis = p1-p2;
+				//if (dis.len>0.02) we assume that are components from same robot
+				//as such we will ignore it
+				//this is a pretty bad hack and needs to be changed to check whether the geoms come
+				//from the same robot
+				if(dis.len()>0.02){
+					  dBodyID b1 = dGeomGetBody(o1);
+				  	  dBodyID b2 = dGeomGetBody(o2);
+					  dContact contact[3];		// up to 3 contacts per box
+					  for (i=0; i<3; i++) {
+					    contact[i].surface.mode = dContactSoftCFM | dContactApprox1;
+					    contact[i].surface.mu = MU;
+					    contact[i].surface.soft_cfm = 0.01;
+					  }
+					  if (int numc = dCollide (o1,o2,3,&contact[0].geom,sizeof(dContact))) {
+					    for (i=0; i<numc; i++) {
+					      dJointID c = dJointCreateContact (eworld,contactgroup,contact+i);
+					      dJointAttach (c,b1,b2);
+					    }
+					  }
+				}
+			}
+
+			//			
+			//This gets called every time we have two shpaes in the world that intersect
+			//for every pair of intersecting shapes we need to decide what to do with them
+			//
+			void nearCallback (dGeomID o1, dGeomID o2)
+			{
+				int groundCollision;
+				int notGroundCollision;
+				int g1 = (o1 == ground);
+				int g2 = (o2 == ground);
+				groundCollision = (g1 ^ g2);
+				notGroundCollision = !groundCollision;				
+				if (groundCollision) {
+					handleCollisionWithGround(o1, o2);
+				}else if(notGroundCollision) {
+					int ballCollision;
+					g1 = (o1 == the_ball->ballGeom);
+					g2 = (o2 == the_ball->ballGeom);
+					ballCollision = (g1 ^ g2);			
+					if (ballCollision){
+						handleBallCollision(o1, o2);
+					}else{
+						handleRobotRobotCollision (o1, o2);
+					}
+				}
+			}
+			
 			static void nearCallbackThunk(void *data, dGeomID o1, dGeomID o2) {
 				sim_engine *engine = reinterpret_cast<sim_engine *>(data);
 				engine->nearCallback(o1, o2);
