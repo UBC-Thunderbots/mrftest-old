7b45f8ed8658dd2f4f4ab4346121dd94d80e9c51
simonsyd <simonsyd@localhost>
2009-11-24 02:03:48 +0000

    Much improved robot movement benchmark. Has multiple tasks and also does orientation.

--- software/ai/strategy/movement_benchmark.cpp
+++ software/ai/strategy/movement_benchmark.cpp
@@ -2,8 +2,12 @@
 #include "ai/tactic.h"
 #include "ai/tactic/move.h"
 #include <iostream>
+#include <vector>
 
 // This benchmark records how long it takes for a robot to travel and stop at a point 1 meter away.
+
+// NOTE:
+// the first task centers the robot, so the timing does not start until after that
 
 namespace {
 	class movement_benchmark : public strategy {
@@ -16,42 +20,56 @@ namespace {
 			void robot_added(void);
 			void robot_removed(unsigned int index, robot::ptr r);
 		private:
-			point destination;
+			std::vector<std::pair<point, double> > tasks;
 			int time_steps;
-			bool done;
+			size_t done;
 			double dis_threshold;
 			double vel_threshold;
+			double ori_threshold;
 	};
 
-	movement_benchmark::movement_benchmark(ball::ptr ball, field::ptr field, controlled_team::ptr team, playtype_source &pt_src) : strategy(ball, field, team, pt_src) {
-		destination = team->get_player(0)->position();
-		destination.x += 1;
+	// A set of tasks; use this for now
+	const std::pair<point, double> default_tasks[] =
+	{
+		std::make_pair(point(0, 0), 0),
+		std::make_pair(point(1, 0), PI / 2),
+		std::make_pair(point(0, 0), PI),
+		std::make_pair(point(-1, 0), 0),
+		std::make_pair(point(0, 1), PI / 2),
+		std::make_pair(point(0, 0), 0),
+		std::make_pair(point(0, -1), PI),
+		std::make_pair(point(0, 0), 0),
+	};
+	const int default_tasks_n = sizeof(default_tasks) / sizeof(default_tasks[0]);
+
+	movement_benchmark::movement_benchmark(ball::ptr ball, field::ptr field, controlled_team::ptr team, playtype_source &pt_src) : strategy(ball, field, team, pt_src), tasks(default_tasks, default_tasks + default_tasks_n), done(0) {
 		time_steps = 0;
 		done = false;
 		dis_threshold = 1e-1;
 		vel_threshold = 1e-2;
+		ori_threshold = 1e-1;
 	}
 
 	void movement_benchmark::tick() {
-		if (!done) {
-			time_steps++;
-			point diff = the_team->get_player(0)->position() - destination;
-			point vel = the_team->get_player(0)->est_velocity();
-			std::cout << "position difference: " << diff.x << " " << diff.y << std::endl;
+		if (done < tasks.size()) {
+			if (done > 0) time_steps++;
+			point diff_pos = the_team->get_player(0)->position() - tasks[done].first;
+			double diff_ori = angle_mod(the_team->get_player(0)->orientation() - tasks[done].second);
+			point vel_pos = the_team->get_player(0)->est_velocity();
+			std::cout << "movement benchmark task #" << done << std::endl;
+			std::cout << "difference pos:" << diff_pos.x << " " << diff_pos.y << " ori:" << diff_ori << std::endl;
 			std::cout << "velocity: " << the_team->get_player(0)->est_velocity().x << " " << the_team->get_player(0)->est_velocity().y << std::endl;
-			if (diff.len() < dis_threshold && vel.len() < vel_threshold) {
+			if (diff_pos.len() < dis_threshold && vel_pos.len() < vel_threshold && abs(diff_ori) < ori_threshold) {
 				std::cout << "time steps taken: " << time_steps << std::endl;
-				done = true;
+				++done;
 			}
 		}
-		move::ptr mover (new move(the_ball, the_field, the_team, the_team->get_player(0)));
-		mover->set_position(destination);
-		mover->tick();
+		the_team->get_player(0)->move(tasks[done].first, tasks[done].second);
 	}
 
 	void movement_benchmark::set_playtype(playtype::playtype) {
 	}
-	
+
 	Gtk::Widget *movement_benchmark::get_ui_controls() {
 		return 0;
 	}
