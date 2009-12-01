d3863aabbbeef45d5d1a79f7f35f891159032e95
byronknoll <byronknoll@localhost>
2009-12-01 02:59:07 +0000

    fixed warnings

--- software/robot_controller/lazy_controller.cpp
+++ software/robot_controller/lazy_controller.cpp
@@ -24,10 +24,10 @@ namespace {
 lazy_controller::lazy_controller(player_impl::ptr plr) : plr(plr) {
 }
 
-void lazy_controller::move(const point &new_position, double new_orientation, point &linear_velocity, double &angular_velocity) {
-	const point &current_position = plr->position();
+void lazy_controller::move(const point &, double new_orientation, point &linear_velocity, double &angular_velocity) {
 	const double current_orientation = plr->orientation();
 	angular_velocity = angle_mod(new_orientation - current_orientation);
+	linear_velocity = point();
 }
 
 robot_controller_factory &lazy_controller::get_factory() const {
