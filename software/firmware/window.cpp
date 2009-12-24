cdd5c618829701c85fa9bc178b2eb47a177daec2
chead <chead@localhost>
2009-12-24 19:23:58 +0000

    Display total time taken.

--- software/firmware/window.cpp
+++ software/firmware/window.cpp
@@ -148,7 +148,7 @@ namespace {
 			void upload_error(const Glib::ustring &message) {
 				Gtk::MessageDialog md(*this, message, false, Gtk::MESSAGE_ERROR, Gtk::BUTTONS_OK, true);
 				md.run();
-				response(Gtk::RESPONSE_ACCEPT);
+				response(Gtk::RESPONSE_CANCEL);
 			}
 	};
 }
@@ -218,10 +218,20 @@ class firmware_window_impl : public Gtk::Window {
 				return;
 			}
 
-			upload up(modem, current_address, ihex);
-			upload_dialog dlg(*this, up);
-			Glib::signal_idle().connect(sigc::bind_return(sigc::mem_fun(up, &upload::start), false));
-			dlg.run();
+			int resp;
+			Glib::Timer timer;
+			{
+				upload up(modem, current_address, ihex);
+				upload_dialog dlg(*this, up);
+				Glib::signal_idle().connect(sigc::bind_return(sigc::mem_fun(up, &upload::start), false));
+				resp = dlg.run();
+			}
+			if (resp == Gtk::RESPONSE_ACCEPT) {
+				timer.stop();
+				const Glib::ustring &msg = Glib::ustring::compose("Upload completed in %1s.", timer.elapsed());
+				Gtk::MessageDialog md(*this, msg, false, Gtk::MESSAGE_INFO, Gtk::BUTTONS_OK, true);
+				md.run();
+			}
 		}
 };
 
