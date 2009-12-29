0e46f4ae572cfe7ee3d5b1e89b4191af3249ea5c
simonsyd <simonsyd@localhost>
2009-12-29 00:43:26 +0000

    Now checks if a robot is oriented correctly.

--- software/ai/strategy/movement_benchmark.cpp
+++ software/ai/strategy/movement_benchmark.cpp
@@ -4,6 +4,7 @@
 #include "gtkmm/button.h"
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 // This benchmark records how long it takes for a robot to travel and stop at a point 1 meter away.
 
@@ -25,9 +26,11 @@ namespace {
 			std::vector<std::pair<point, double> > tasks;
 			int time_steps;
 			size_t done;
-			double dis_threshold;
-			double vel_threshold;
-			double ori_threshold;
+			double pos_dis_threshold;
+			double pos_vel_threshold;
+			double ori_dis_threshold;
+			double ori_vel_threshold;
+			double prev_ori;
 			Gtk::Button* reset_button;
 	};
 
@@ -45,12 +48,13 @@ namespace {
 	};
 	const int default_tasks_n = sizeof(default_tasks) / sizeof(default_tasks[0]);
 
-	movement_benchmark::movement_benchmark(ball::ptr ball, field::ptr field, controlled_team::ptr team, playtype_source &pt_src) : strategy(ball, field, team, pt_src), tasks(default_tasks, default_tasks + default_tasks_n), done(0) {
+	movement_benchmark::movement_benchmark(ball::ptr ball, field::ptr field, controlled_team::ptr team, playtype_source &pt_src) : strategy(ball, field, team, pt_src), tasks(default_tasks, default_tasks + default_tasks_n), done(0), prev_ori(0) {
 		time_steps = 0;
 		done = false;
-		dis_threshold = 1e-1;
-		vel_threshold = 1e-2;
-		ori_threshold = 1e-1;
+		pos_dis_threshold = 1e-1;
+		pos_vel_threshold = 1e-1;
+		ori_dis_threshold = 1e-1;
+		ori_vel_threshold = 1e-1;
 		reset_button = new Gtk::Button("Reset");
 		reset_button->signal_clicked().connect(sigc::mem_fun(*this,&movement_benchmark::strategy_reset));
 	}
@@ -62,17 +66,20 @@ namespace {
 	
 	void movement_benchmark::tick() {
 		if (done < tasks.size()) {
-			if (done > 0) time_steps++;
-			point diff_pos = the_team->get_player(0)->position() - tasks[done].first;
-			double diff_ori = angle_mod(the_team->get_player(0)->orientation() - tasks[done].second);
-			point vel_pos = the_team->get_player(0)->est_velocity();
+			if (done == 0) time_steps = 0;
+			else if (done > 0) time_steps++;
+			const point diff_pos = the_team->get_player(0)->position() - tasks[done].first;
+			const point vel_pos = the_team->get_player(0)->est_velocity();
+			const double diff_ori = angle_mod(the_team->get_player(0)->orientation() - tasks[done].second);
+			const double vel_ori = angle_mod(the_team->get_player(0)->orientation() - prev_ori);
 			std::cout << "movement benchmark task #" << done << std::endl;
-			std::cout << "difference pos:" << diff_pos.x << " " << diff_pos.y << " ori:" << diff_ori << std::endl;
-			std::cout << "velocity: " << the_team->get_player(0)->est_velocity().x << " " << the_team->get_player(0)->est_velocity().y << std::endl;
-			if (diff_pos.len() < dis_threshold && vel_pos.len() < vel_threshold && abs(diff_ori) < ori_threshold) {
+			std::cout << "displace pos:" << diff_pos.x << " " << diff_pos.y << " ori:" << diff_ori << std::endl;
+			std::cout << "velocity pos:" << the_team->get_player(0)->est_velocity().x << " " << the_team->get_player(0)->est_velocity().y << " ori:" << vel_ori << std::endl;
+			if (diff_pos.len() < pos_dis_threshold && vel_pos.len() < pos_vel_threshold && fabs(diff_ori) < ori_dis_threshold && fabs(vel_ori) < ori_vel_threshold) {
 				std::cout << "time steps taken: " << time_steps << std::endl;
 				++done;
 			}
+			prev_ori = the_team->get_player(0)->orientation();
 		}
 		the_team->get_player(0)->move(tasks[done].first, tasks[done].second);
 	}
