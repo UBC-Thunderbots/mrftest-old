127ea51f41bd93fa25a7d78e2f0147e6627a8f53
chead <chead@localhost>
2010-02-07 11:05:59 +0000

    Fix line endings. Fix include paths. Fix indentation.

--- software/util/ball_filter.cpp
+++ software/util/ball_filter.cpp
@@ -1,104 +1,105 @@
+#include "world/timestep.h"
+#include <cmath>
 #include <complex>
 #include <list>
 #include <utility>
 #include <vector>
-#include "math.h"
-#include "./world/timestep.h"
 
 using namespace std;
 
 typedef complex<double> pt;
 
 struct circle {
-    pt center;
-    double certainty;
-	
+	pt center;
+	double certainty;
+
 	bool operator==(circle other) {
 		return (center == other.center);
 	}
 };
 
 class ball_filter {
-    private:
-        static const double RADIUS = 0.6;
-	static const double DECAY_TIME_CONSTANT = 0.2885; //decay constant in seconds
-        static double DECAY_RATE;
-        static const double DELETE_THRESHOLD = 0.02; // stores < 50 circles
-        static list<circle> circles;
-    
-    public:
-        static void init_filter() {
-            circles.clear();
-            circle c;
-            c.center = pt(0, 0);
-            c.certainty = DELETE_THRESHOLD;
-            circles.push_back(c);
-            DECAY_RATE = 1 - exp(-1.0 / static_cast<double>(TIMESTEPS_PER_SECOND) / DECAY_TIME_CONSTANT);
-        }
-        
-        static pt filter(const vector<pair<pt, double> >& obs) {
-          pt max_point;
-          double max_cert = 0.0;
-          
-          if (obs.empty()) {
-            for (list<circle>::iterator it = circles.begin(); it != circles.end(); it++) {
-                it->certainty = (1.0 - DECAY_RATE)*it->certainty;
-            }
-          }
-          else {
-            for (int i = 0; i < int(obs.size()); i++) {
-                if (max_cert < obs[i].second) {
-                    max_point = obs[i].first;
-                    max_cert = obs[i].second;
-                }
-            }
-            
-            vector<circle> containing;
-            for (list<circle>::iterator it = circles.begin(); it != circles.end(); it++) {
-                if (abs(max_point - it->center) < RADIUS) {
-                    containing.push_back(*it);
-                    it->center = max_point;
-                }
-                else { it->certainty = (1.0 - DECAY_RATE)*it->certainty; }
-            }
-            
-            if (containing.empty()) {
+	private:
+		static const double RADIUS = 0.6;
+		static const double DECAY_TIME_CONSTANT = 0.2885; //decay constant in seconds
+		static double DECAY_RATE;
+		static const double DELETE_THRESHOLD = 0.02; // stores < 50 circles
+		static list<circle> circles;
 
-                circle c;
-                c.center = max_point;
-                c.certainty = DECAY_RATE;
-                circles.push_back(c);
-            }
-            else {
-                double anti_cert = 1.0;
-                for (vector<circle>::iterator it = containing.begin(); it != containing.end(); ++it) {
-                    anti_cert *= 1.0 - (*it).certainty;
-                    if (it != containing.begin()) {
-						for (list<circle>::iterator shit = circles.begin(); shit != circles.end(); ++shit) {
-							if( (*shit) == (*it) )
-		                        circles.erase(shit);
-						}	
+	public:
+		static void init_filter() {
+			circles.clear();
+			circle c;
+			c.center = pt(0, 0);
+			c.certainty = DELETE_THRESHOLD;
+			circles.push_back(c);
+			DECAY_RATE = 1 - exp(-1.0 / static_cast<double>(TIMESTEPS_PER_SECOND) / DECAY_TIME_CONSTANT);
+		}
 
+		static pt filter(const vector<pair<pt, double> >& obs) {
+			pt max_point;
+			double max_cert = 0.0;
+
+			if (obs.empty()) {
+				for (list<circle>::iterator it = circles.begin(); it != circles.end(); it++) {
+					it->certainty = (1.0 - DECAY_RATE)*it->certainty;
+				}
+			}
+			else {
+				for (int i = 0; i < int(obs.size()); i++) {
+					if (max_cert < obs[i].second) {
+						max_point = obs[i].first;
+						max_cert = obs[i].second;
 					}
-                }
-                anti_cert *= 1.0 - DECAY_RATE;
-                containing[0].certainty = 1.0 - anti_cert;
-            }
-            
-            for (list<circle>::iterator it = circles.begin(); it != circles.end(); ) {
-                if (it->certainty < DELETE_THRESHOLD) it = circles.erase(it);
-                else it++;
-            }
-          }
-            
-            max_cert = 0.0;
-            list<circle>::iterator max_point_it = circles.begin();
-            for (list<circle>::iterator it = circles.begin(); it != circles.end(); it++) {
-                if (max_cert < it->certainty) {
-                    max_cert = it->certainty;
-                    max_point_it = it;
-                }
-            }
-            return max_point_it->center;
-        }
+				}
+
+				vector<circle> containing;
+				for (list<circle>::iterator it = circles.begin(); it != circles.end(); it++) {
+					if (abs(max_point - it->center) < RADIUS) {
+						containing.push_back(*it);
+						it->center = max_point;
+					}
+					else { it->certainty = (1.0 - DECAY_RATE)*it->certainty; }
+				}
+
+				if (containing.empty()) {
+
+					circle c;
+					c.center = max_point;
+					c.certainty = DECAY_RATE;
+					circles.push_back(c);
+				}
+				else {
+					double anti_cert = 1.0;
+					for (vector<circle>::iterator it = containing.begin(); it != containing.end(); ++it) {
+						anti_cert *= 1.0 - (*it).certainty;
+						if (it != containing.begin()) {
+							for (list<circle>::iterator shit = circles.begin(); shit != circles.end(); ++shit) {
+								if( (*shit) == (*it) )
+									circles.erase(shit);
+							}	
+
+						}
+					}
+					anti_cert *= 1.0 - DECAY_RATE;
+					containing[0].certainty = 1.0 - anti_cert;
+				}
+
+				for (list<circle>::iterator it = circles.begin(); it != circles.end(); ) {
+					if (it->certainty < DELETE_THRESHOLD) it = circles.erase(it);
+					else it++;
+				}
+			}
+
+			max_cert = 0.0;
+			list<circle>::iterator max_point_it = circles.begin();
+			for (list<circle>::iterator it = circles.begin(); it != circles.end(); it++) {
+				if (max_cert < it->certainty) {
+					max_cert = it->certainty;
+					max_point_it = it;
+				}
+			}
+			return max_point_it->center;
+		}
 };
+
