fdc93b148e4ef5e0ec2e11f9ff5b5a9c9f5a20b4
chead <chead@localhost>
2010-03-09 00:40:15 +0000

    Add ability to remotely ugprade PIC firmware.

---
+++ software/firmware/pic.h
@@ -0,0 +1,45 @@
+#ifndef FIRMWARE_PIC_H
+#define FIRMWARE_PIC_H
+
+#include "firmware/bootproto.h"
+#include "firmware/watchable_operation.h"
+#include "util/ihex.h"
+#include "xbee/xbee.h"
+
+//
+// An operation to upload data to be burned into the PIC.
+//
+class pic_upload : public watchable_operation, public sigc::trackable {
+	public:
+		//
+		// Constructs an uploader.
+		//
+		pic_upload(xbee &modem, uint64_t bot, const intel_hex &data);
+
+		//
+		// Starts the upload process.
+		//
+		void start();
+
+		//
+		// The number of bytes in a page.
+		//
+		static const unsigned int PAGE_BYTES = 64;
+
+	private:
+		xbee &modem;
+		uint64_t bot;
+		const intel_hex &data;
+		bootproto proto;
+		unsigned int pages_written;
+
+		void enter_bootloader_done();
+		void ident_received(const void *);
+		void fuses_received(const void *);
+		void do_work();
+		void page_written(const void *);
+		void upgrade_enabled(const void *);
+};
+
+#endif
+
--- software/firmware/window.cpp
+++ software/firmware/window.cpp
@@ -1,5 +1,6 @@
 #include "firmware/emergency_erase.h"
 #include "firmware/fpga.h"
+#include "firmware/pic.h"
 #include "firmware/window.h"
 #include "uicomponents/bot_chooser.h"
 #include "util/ihex.h"
@@ -39,7 +40,7 @@ namespace {
 
 class firmware_window_impl : public Gtk::Window {
 	public:
-		firmware_window_impl(xbee &modem, xmlpp::Element *xmlworld) : modem(modem), bot_frame("Bot"), bot_controls(xmlworld, *this), file_frame("Firmware File"), start_upload_button(Gtk::Stock::EXECUTE), emergency_erase_button("Emergency Erase") {
+		firmware_window_impl(xbee &modem, xmlpp::Element *xmlworld) : modem(modem), bot_frame("Bot"), bot_controls(xmlworld, *this), file_frame("Firmware File"), file_fpga_button(file_target_group, "FPGA"), file_pic_button(file_target_group, "PIC"), start_upload_button(Gtk::Stock::EXECUTE), emergency_erase_button("Emergency Erase") {
 			set_title("Firmware Uploader");
 
 			bot_controls.signal_address_changed().connect(sigc::mem_fun(*this, &firmware_window_impl::address_changed));
@@ -48,13 +49,18 @@ class firmware_window_impl : public Gtk::Window {
 
 			Gtk::FileFilter *filter = new Gtk::FileFilter();
 			filter->set_name("Intel HEX Files");
+			filter->add_pattern("*.hex");
 			filter->add_pattern("*.mcs");
 			file_chooser.add_filter(*filter);
 			filter = new Gtk::FileFilter();
 			filter->set_name("All Files");
 			filter->add_pattern("*");
 			file_chooser.add_filter(*filter);
-			file_frame.add(file_chooser);
+			file_vbox.add(file_chooser);
+			file_target_hbox.pack_start(file_fpga_button);
+			file_target_hbox.pack_start(file_pic_button);
+			file_vbox.pack_start(file_target_hbox);
+			file_frame.add(file_vbox);
 			vbox.pack_start(file_frame, false, false);
 
 			start_upload_button.signal_clicked().connect(sigc::mem_fun(*this, &firmware_window_impl::start_upload));
@@ -86,7 +92,11 @@ class firmware_window_impl : public Gtk::Window {
 		bot_chooser bot_controls;
 
 		Gtk::Frame file_frame;
+		Gtk::VBox file_vbox;
 		Gtk::FileChooserButton file_chooser;
+		Gtk::HBox file_target_hbox;
+		Gtk::RadioButtonGroup file_target_group;
+		Gtk::RadioButton file_fpga_button, file_pic_button;
 
 		Gtk::Button start_upload_button;
 		Gtk::Button emergency_erase_button;
@@ -98,21 +108,37 @@ class firmware_window_impl : public Gtk::Window {
 
 		void start_upload() {
 			const Glib::ustring &filename = file_chooser.get_filename();
-			intel_hex ihex;
-			ihex.add_section(0, 16 * 1024 * 1024 / 8);
-			try {
-				ihex.load(filename);
-			} catch (const std::runtime_error &exp) {
-				Gtk::MessageDialog md(*this, exp.what(), false, Gtk::MESSAGE_ERROR, Gtk::BUTTONS_OK, true);
-				md.run();
-				return;
-			}
 
 			Glib::Timer timer;
-			{
+			if (file_fpga_button.get_active()) {
+				intel_hex ihex;
+				ihex.add_section(0, 16 * 1024 * 1024 / 8);
+				try {
+					ihex.load(filename);
+				} catch (const std::runtime_error &exp) {
+					Gtk::MessageDialog md(*this, exp.what(), false, Gtk::MESSAGE_ERROR, Gtk::BUTTONS_OK, true);
+					md.run();
+					return;
+				}
 				fpga_upload up(modem, current_address, ihex);
 				working_dialog dlg(*this, up);
 				Glib::signal_idle().connect(sigc::bind_return(sigc::mem_fun(up, &fpga_upload::start), false));
+				dlg.run();
+			} else {
+				intel_hex ihex;
+				ihex.add_section(0x0, 0x800);
+				ihex.add_section(0x800, 0x3FFF - 0x800 + 1);
+				ihex.add_section(0x300000, 16);
+				try {
+					ihex.load(filename);
+				} catch (const std::runtime_error &exp) {
+					Gtk::MessageDialog md(*this, exp.what(), false, Gtk::MESSAGE_ERROR, Gtk::BUTTONS_OK, true);
+					md.run();
+					return;
+				}
+				pic_upload up(modem, current_address, ihex);
+				working_dialog dlg(*this, up);
+				Glib::signal_idle().connect(sigc::bind_return(sigc::mem_fun(up, &pic_upload::start), false));
 				dlg.run();
 			}
 			timer.stop();
