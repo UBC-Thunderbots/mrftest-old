c67be266df6bfd118b2ead074c27b82bf9bbdde2
byronknoll <byronknoll@localhost>
2010-04-03 21:52:54 +0000

    Took into account ball acceleration

--- software/ai/tactic/chase.cpp
+++ software/ai/tactic/chase.cpp
@@ -10,7 +10,10 @@ void chase::tick()
 {
 //	std::cout << (the_ball->position()+the_ball->est_velocity()/2) << std::endl;	
 //	std::cout << the_ball->est_velocity() << std::endl;
-	move_tactic->set_position(the_ball->position()+the_ball->est_velocity()/2);
+	//move_tactic->set_position(the_ball->position()+the_ball->est_velocity()/2);
+	// predict ball position based on velocity and accleration
+ 	//NOTE THAT MOVING BALL MANUALLY WITH CURSOR CAN GIVE VERY LARGE VELOCITY AND ACCELERATION
+	move_tactic->set_position(the_ball->position()+the_ball->est_velocity()/2 + the_ball->est_acceleration()/8);
 
 	move_tactic->tick();
 }
