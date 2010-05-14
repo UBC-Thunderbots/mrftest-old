5e968bc9e6814c96aa7562c8c52e4e8208d5a089
simonsyd <simonsyd@localhost>
2010-05-14 05:29:38 +0000

    Halt now stops the robot from changing direction.

--- software/ai/role/halt.cpp
+++ software/ai/role/halt.cpp
@@ -1,11 +1,3 @@
-/* ==================================================
-Update History:
-
-Name               Date               Remark
-Kenneth            23 Jan 2010        Initial implementation
-
-===================================================*/
-
 #include "ai/role/halt.h"
 #include "ai/tactic/move.h"
 
@@ -13,22 +5,11 @@ halt::halt(ball::ptr ball, field::ptr field, controlled_team::ptr team) : role(b
 }
 
 void halt::tick(){
-    for (unsigned int i = 0; i < the_robots.size(); i++)
-    {
-        /////////////////
-        // This cannot stop the robot. Not sure if this is a simulator problem.
-        ////////////////////
-        tactics[i]->set_position(the_robots[i]->position());
-        tactics[i]->tick();
+    for (size_t i = 0; i < the_robots.size(); i++) {
+        the_robots[i]->move(the_robots[i]->position(), the_robots[i]->orientation());
     } 
 }
 
 void halt::robots_changed() {
-    tactics.clear();
-    for (unsigned int i = 0; i < the_robots.size(); i++)
-    {
-        tactics.push_back(move::ptr (new move(the_ball, the_field, the_team, the_robots[i])));
-        tactics[i]->set_position(the_robots[i]->position());
-    } 
 }
 
