422f2097bac7995abbd9e159e3e5b36326eea002
simonsyd <simonsyd@localhost>
2010-05-14 06:00:27 +0000

    Cleaned up very messy code. Also fixed a bug where if a robot stops it should face towards the ball.

--- software/ai/navigator/robot_navigator.cpp
+++ software/ai/navigator/robot_navigator.cpp
@@ -2,374 +2,283 @@
 #include <iostream>
 #include <cstdlib>
 
-  const double SLOW_AVOIDANCE_SPEED=1.0;
-  const double FAST_AVOIDANCE_SPEED=2.0;
-
-robot_navigator::robot_navigator(player::ptr player, field::ptr field, ball::ptr ball, team::ptr team) : 
-  navigator(player, field, ball, team), destInitialized(false), outOfBoundsMargin(field->width() / 20.0),
-  maxLookahead(1.0), avoidance_factor(2), slow_avoidance_factor(0.5), fast_avoidance_factor(2.0)
-{
-
+namespace {
+	const double SLOW_AVOIDANCE_SPEED=1.0;
+	const double FAST_AVOIDANCE_SPEED=2.0;
 }
 
-double robot_navigator::get_avoidance_factor(){
-
-double robot_speed = the_player->est_velocity().len();
-
-if(robot_speed < SLOW_AVOIDANCE_SPEED){
-return get_slow_avoidance_factor();
+robot_navigator::robot_navigator(player::ptr player, field::ptr field, ball::ptr ball, team::ptr team) : navigator(player, field, ball, team),
+	dest_initialized(false), outofbounds_margin(field->width() / 20.0),
+	max_lookahead(1.0), avoidance_factor(2), slow_avoidance_factor(0.5), fast_avoidance_factor(2.0) {
 }
 
-if(robot_speed > FAST_AVOIDANCE_SPEED){
-return get_fast_avoidance_factor();
-}
+double robot_navigator::get_avoidance_factor() const {
 
-assert(SLOW_AVOIDANCE_SPEED < FAST_AVOIDANCE_SPEED);
+	double robot_speed = the_player->est_velocity().len();
 
-double slowness = (robot_speed - FAST_AVOIDANCE_SPEED)/(SLOW_AVOIDANCE_SPEED - FAST_AVOIDANCE_SPEED);
-double fastness = ( SLOW_AVOIDANCE_SPEED - robot_speed)/(SLOW_AVOIDANCE_SPEED - FAST_AVOIDANCE_SPEED);
+	if(robot_speed < SLOW_AVOIDANCE_SPEED){
+		return get_slow_avoidance_factor();
+	}
 
-return get_slow_avoidance_factor()*slowness + get_fast_avoidance_factor()*fastness;
+	if(robot_speed > FAST_AVOIDANCE_SPEED){
+		return get_fast_avoidance_factor();
+	}
 
+	assert(SLOW_AVOIDANCE_SPEED < FAST_AVOIDANCE_SPEED);
+
+	double slowness = (robot_speed - FAST_AVOIDANCE_SPEED)/(SLOW_AVOIDANCE_SPEED - FAST_AVOIDANCE_SPEED);
+	double fastness = ( SLOW_AVOIDANCE_SPEED - robot_speed)/(SLOW_AVOIDANCE_SPEED - FAST_AVOIDANCE_SPEED);
+
+	return get_slow_avoidance_factor()*slowness + get_fast_avoidance_factor()*fastness;
 }
 
 void robot_navigator::tick() {
-  //tell it which way to go
-  if(destInitialized)
-    {
-      
-      point nowDest;
 
-      // if we have the ball, adjust our destination to ensure that we
-      // don't take the ball out of bounds, otherwise, head to our
-      // assigned destination
-      if (the_player->has_ball())
-	{
-     
-	  nowDest = clip_point(currDest,
-			       point(-the_field->length()/2 + outOfBoundsMargin,
-				     -the_field->width()/2 + outOfBoundsMargin),
-			       point(the_field->length()/2 - outOfBoundsMargin,
-				     the_field->width()/2 - outOfBoundsMargin));
-	}
-      else
-	{
-	  nowDest = currDest;
+	if(!dest_initialized) return;
+
+	point nowdest;
+
+	point balldirection = the_ball->position() - the_player->position();
+
+	// if we have the ball, adjust our destination to ensure that we
+	// don't take the ball out of bounds, otherwise, head to our
+	// assigned destination
+	if (the_player->has_ball()) {
+		nowdest = clip_point(curr_dest, point(-the_field->length()/2 + outofbounds_margin, -the_field->width()/2 + outofbounds_margin),
+				point(the_field->length()/2 - outofbounds_margin, the_field->width()/2 - outofbounds_margin));
+	} else {
+		nowdest = curr_dest;
 	}
 
-      point direction = nowDest - the_player->position();
+	point direction = nowdest - the_player->position();
 
-      if (direction.len() < 0.01)
-	{
-	  return;
+	// at least face the ball
+	if (direction.len() < 0.01) {
+		if (!the_player->has_ball())
+			the_player->move(the_player->position(), atan2(balldirection.y, balldirection.x));
+		return;
 	}
 
-      double dirlen = direction.len();
-      direction = direction / direction.len();
+	double dirlen = direction.len();
+	direction = direction / direction.len();
 
-      point leftdirection = direction;
-      point rightdirection = direction;
+	point leftdirection = direction;
+	point rightdirection = direction;
 
-      double rotationangle = 0.0;
+	double angle = 0.0;
 
-      bool undiverted = true;
-      bool stop = false;
-      bool chooseleft;
-      while (true)
-	{
-	  //std::cout << "path changed" <<std::endl;
-	  
-	  //it shouldn't take that many checks to get a good direction
-	  leftdirection = direction.rotate(rotationangle);
-	  rightdirection = direction.rotate(-rotationangle);
+	bool undiverted = true;
+	bool stop = false;
+	bool chooseleft;
+	while (true) {
+		//std::cout << "path changed" <<std::endl;
 
-	  if (check_vector(the_player->position(), nowDest, leftdirection.rotate(2.5 * PI / 180.0)))
-	    {
-	      if (check_vector(the_player->position(), nowDest, leftdirection.rotate(-2.5 * PI / 180.0)))
-		{
-		  chooseleft = true;
-		  break;
+		//it shouldn't take that many checks to get a good direction
+		leftdirection = direction.rotate(angle);
+		rightdirection = direction.rotate(-angle);
+
+		if (check_vector(the_player->position(), nowdest, leftdirection.rotate(2.5 * PI / 180.0))) {
+			if (check_vector(the_player->position(), nowdest, leftdirection.rotate(-2.5 * PI / 180.0))) {
+				chooseleft = true;
+				break;
+			}
 		}
-	    }
-	  else if (check_vector(the_player->position(), nowDest, rightdirection.rotate(-2.5 * PI / 180.0)))
-	    {
-	      if (check_vector(the_player->position(), nowDest, rightdirection.rotate(2.5 * PI / 180.0)))
-		{
-		  chooseleft = false;
-		  break;
+		else if (check_vector(the_player->position(), nowdest, rightdirection.rotate(-2.5 * PI / 180.0))) {
+			if (check_vector(the_player->position(), nowdest, rightdirection.rotate(2.5 * PI / 180.0))) {
+				chooseleft = false;
+				break;
+			}
 		}
-	    }
-	  
-	  // if we can't find a path within 90 degrees
-	  // go straight towards our destination
-	  if (rotationangle > 100.0 * PI / 180.0)
-	    {
-	      leftdirection = rightdirection = direction;
-	      stop = true;
-	      break;
-	    }
-	  rotationangle += 1.0 * PI / 180.0;//rotate by 1 degree each
-					    //time
+
+		// if we can't find a path within 90 degrees
+		// go straight towards our destination
+		if (angle > 100.0 * PI / 180.0) {
+			leftdirection = rightdirection = direction;
+			stop = true;
+			break;
+		}
+		angle += 1.0 * PI / 180.0;//rotate by 1 degree each
+		//time
 	}
-      undiverted = rotationangle < 1e-5;
-      if(stop)
-	{
-	  point balldest = the_ball->position() - the_player->position();
-	  the_player->move(the_player->position(), atan2(balldest.y, balldest.x));
+	undiverted = angle < 1e-5;
+
+	if(stop) {
+		the_player->move(the_player->position(), atan2(balldirection.y, balldirection.x));
+		return;
 	}
-      else
-	{
-	  point selected_direction;
-	  if (chooseleft)
-	    {
-	      // select the left vector
-	      selected_direction = leftdirection;     
-	    }
-	  else
-	    {
-	      // select the right vector
-	      selected_direction = rightdirection;
-	    }
-	  
-	  if (undiverted)
-	    {
-	      point balldest = the_ball->position() - the_player->position();
-	      the_player->move(nowDest, atan2(balldest.y, balldest.x));
-	    }
-	  else
-	    {
-	      // maximum warp
-	      point balldest = the_ball->position() - the_player->position();
-	      the_player->move(the_player->position() + selected_direction*std::min(dirlen,1.0), atan2(balldest.y, balldest.x));
-	    }
+
+	point selected_direction = (chooseleft) ? leftdirection : rightdirection;
+
+	if (undiverted) {
+		the_player->move(nowdest, atan2(balldirection.y, balldirection.x));
+	} else {
+		// maximum warp
+		the_player->move(the_player->position() + selected_direction*std::min(dirlen,1.0), atan2(balldirection.y, balldirection.x));
 	}
-    }
 }
 
 void robot_navigator::set_point(const point &destination) {
-  //set new destinatin point
-  destInitialized = true;
-  /*currDest = clip_point(destination,
-			point(-the_field->length()/2,-the_field->width()/2),
-			point(the_field->length()/2,the_field->width()/2));*/
-  currDest = destination;
+	//set new destinatin point
+	dest_initialized = true;
+	/*curr_dest = clip_point(destination,
+	  point(-the_field->length()/2,-the_field->width()/2),
+	  point(the_field->length()/2,the_field->width()/2));*/
+	curr_dest = destination;
 }
 
 /**
-intended to specify how far robot should travel after making a path correction
-not final! 
-implement or delete function soon
+  intended to specify how far robot should travel after making a path correction
+  not final! 
+  implement or delete function soon
 
- \param correction_size - amount of distance travelled to correct path
- 
-**/
+  \param correction_size - amount of distance travelled to correct path
+
+ **/
 void robot_navigator::set_correction_step_size(double) {
 #warning "implement or delete function soon"
 }
 /**
-normally the navigator sets the robot orientation to be towards the ball
-use this if you want to override this behaviour
-this only sets the desired orientation for one timestep
-\param orientation
-*/
+  normally the navigator sets the robot orientation to be towards the ball
+  use this if you want to override this behaviour
+  this only sets the desired orientation for one timestep
+  \param orientation
+ */
 void robot_navigator::set_desired_robot_orientation(double) {
 #warning "implement function"
 }
 
 bool robot_navigator::robot_avoids_ball() {
-return avoid_ball;
+	return avoid_ball;
 }
 
 void robot_navigator::set_robot_avoids_ball(bool avoid){
-avoid_ball=avoid;
+	avoid_ball=avoid;
 }
 
 /**
-get whether the robot avoid it's own goal
-*/
+  get whether the robot avoid it's own goal
+ */
 bool robot_navigator::robot_avoids_goal() {
 #warning "implement or delete function soon"
-return false;
+	return false;
 }
 
 /**
-make the robot avoid it's own goal
-\param avoid whether to avoid it's own goal
-*/
+  make the robot avoid it's own goal
+  \param avoid whether to avoid it's own goal
+ */
 void robot_navigator::set_robot_avoids_goal(bool) {
 #warning "implement or delete function soon"
 }
 
 /**
-get whether the robot is set to stay on it's own half
-*/
+  get whether the robot is set to stay on it's own half
+ */
 bool robot_navigator::robot_stays_on_own_half() {
 #warning "implement function"
-return false;
+	return false;
 }
 
 /**
-make the robot stay on it's own half
-\param avoid whether to make robot stay on it's own half
-*/
+  make the robot stay on it's own half
+  \param avoid whether to make robot stay on it's own half
+ */
 void robot_navigator::set_robot_stays_on_own_half(bool) {
 #warning "implement function"
 }
 
 /**
-get whether the robot avoid it's opponents goal
-*/
+  get whether the robot avoid it's opponents goal
+ */
 bool robot_navigator::robot_stays_away_from_opponent_goal() {
 #warning "implement function"
-return false;
+	return false;
 }
 
 /**
-make the robot avoid it's opponents goal
-\param avoid whether to avoid it's opponents goal
-*/
+  make the robot avoid it's opponents goal
+  \param avoid whether to avoid it's opponents goal
+ */
 void robot_navigator::set_robot_stays_away_from_opponent_goal(bool) {
 #warning "implement function"
 }
 
 /**
-set how much the robot should avoid the opponents goal by
-\param amount the amount that the robot should avoid goal by
-*/
+  set how much the robot should avoid the opponents goal by
+  \param amount the amount that the robot should avoid goal by
+ */
 void robot_navigator::set_robot_avoid_opponent_goal_amount(double) {
 #warning "implement function"
 }
 
 bool robot_navigator::robot_is_in_desired_state() {
 #warning "implement function"
-return false;
+	return false;
 }
 
 bool robot_navigator::robot_is_in_desired_location() {
 #warning "implement function"
-return false;
+	return false;
 }
 
 bool robot_navigator::robot_is_in_desired_orientation(){
 #warning "implement function"
-return false;
+	return false;
 }
 
-point robot_navigator::clip_point(point p, point bound1, point bound2)
-{
-  point rv;
+point robot_navigator::clip_point(const point& p, const point& bound1, const point& bound2) {
 
-  double minx,maxx,miny,maxy;
+	double minx = std::min(bound1.x, bound2.x);
+	double miny = std::min(bound1.y, bound2.y);
+	double maxx = std::max(bound1.x, bound2.x);
+	double maxy = std::max(bound1.y, bound2.y);
 
-  if (bound1.x < bound2.x)
-    {
-      minx = bound1.x;
-      maxx = bound2.x;
-    }
-  else
-    {
-      minx = bound2.x;
-      maxx = bound1.x;
-    }
+	point ret = p;
 
-  if (bound1.y < bound2.y)
-    {
-      miny = bound1.y;
-      maxy = bound2.y;
-    }
-  else
-    {
-      miny = bound2.y;
-      maxy = bound1.y;
-    }
+	if (p.x < minx) ret.x = minx;
+	else if (p.x > maxx) ret.x = maxx;      
 
-  if (p.x < minx)
-    {
-      rv.x = minx;
-    }
-  else if (p.x > maxx)
-    {
-      rv.x = maxx;      
-    }
-  else
-    {
-      rv.x = p.x;
-    }
+	if (p.y < miny) ret.y = miny;
+	else if (p.y > maxy) ret.y = maxy;
 
-  if (p.y < miny)
-    {
-      rv.y = miny;
-    }
-  else if (p.y > maxy)
-    {
-      rv.y = maxy;
-    }
-  else
-    {
-      rv.y = p.y;
-    }
-
-  return rv;
+	return ret;
 }
 
-bool robot_navigator::check_vector(point start, point dest, point direction)
-{
-
-  point startdest = dest - start;
-
-  double lookahead = std::min(startdest.len(), maxLookahead);
-
-  for (size_t i = 0; i < the_team->size() + the_team->other()->size(); i++)
-    {
-      robot::ptr rob;
-
-      if (i >= the_team->size())
-	{
-	  rob = the_team->other()->get_robot(i - the_team->size());
-	}
-      else
-	{
-	  rob = the_team->get_robot(i);
-	}
-      
-      if(rob != this->the_player)
-	{
-	  point rp = rob->position() - start;
-	  //rp/= rp.len();
-	  double len = rp.dot(direction);
-
-	  // ignore robots behind us
-	  if (len > 0)
-	    {
-	      double d = sqrt(rp.dot(rp) - len*len);
-	  
-	      if (len < lookahead && d < 2*get_avoidance_factor()*robot::MAX_RADIUS)
-		{
-		  //std::cout << "Checked FALSE" << std::endl;
-		  return false;
+bool robot_navigator::check_vector(const point& start, const point& dest, const point& direction) const {
+	const point startdest = dest - start;
+	const double lookahead = std::min(startdest.len(), max_lookahead);
+	for (size_t i = 0; i < the_team->size() + the_team->other()->size(); i++) {
+		robot::ptr rob;
+		if (i >= the_team->size()) {
+			rob = the_team->other()->get_robot(i - the_team->size());
+		} else {
+			rob = the_team->get_robot(i);
 		}
-	    }
-	}
-    }
-    
-if(avoid_ball){
-//check ball
-point ballVec = the_ball->position() - start;
-double len = ballVec.dot(direction);
-	  if (len > 0)
-	    {
-	      double d = sqrt(ballVec.dot(ballVec) - len*len);
-	  
-	      if (len < lookahead && d < get_avoidance_factor()*robot::MAX_RADIUS)
-		{
-		  //std::cout << "Checked FALSE" << std::endl;
-		  return false;
+		if(rob == this->the_player) continue;
+		const point rp = rob->position() - start;
+		const double len = rp.dot(direction);
+
+		if (len <= 0) continue;
+		const double d = sqrt(rp.dot(rp) - len*len);
+
+		if (len < lookahead && d < 2 * get_avoidance_factor() * robot::MAX_RADIUS) {
+			return false;
 		}
-	    }
+	}
 
+	if(avoid_ball) {
+		//check ball
+		point ballVec = the_ball->position() - start;
+		double len = ballVec.dot(direction);
+		if (len > 0) {
+			double d = sqrt(ballVec.dot(ballVec) - len*len);
+			if (len < lookahead && d < get_avoidance_factor()*robot::MAX_RADIUS) {
+				//std::cout << "Checked FALSE" << std::endl;
+				return false;
+			}
+		}
+	}
+	//std::cout << "Checked TRUE" << std::endl;
+	return true;
 }
 
-  //std::cout << "Checked TRUE" << std::endl;
-  return true;
-
-}
--- software/ai/navigator/robot_navigator.h
+++ software/ai/navigator/robot_navigator.h
@@ -1,80 +1,79 @@
-#ifndef AI_NAVIGATOR_TESTNAVIGATOR_H
-#define AI_NAVIGATOR_TESTNAVIGATOR_H
+#ifndef AI_NAVIGATOR_ROBOT_NAVIGATOR_H
+#define AI_NAVIGATOR_ROBOT_NAVIGATOR_H
 
 #include "ai/navigator.h"
 
 class robot_navigator : public navigator {
- public:
-typedef Glib::RefPtr<robot_navigator> ptr;
-  robot_navigator(player::ptr player, field::ptr field, ball::ptr ball, team::ptr team);
-  
-  void tick();
-  void set_point(const point& destination);
+	public:
+		typedef Glib::RefPtr<robot_navigator> ptr;
+		robot_navigator(player::ptr player, field::ptr field, ball::ptr ball, team::ptr team);
 
-  //set the amount of avoidance 
-  void set_slow_avoidance_factor(double factor){
-  slow_avoidance_factor=factor;
-  }
-  
-  //get the amount of avoidance 
-  double get_slow_avoidance_factor(){ 
-  return slow_avoidance_factor;
-  }
+		void tick();
+		void set_point(const point& destination);
 
-  //set the amount of avoidance 
-  void set_fast_avoidance_factor(double factor){
-  fast_avoidance_factor = factor;
-  }
+		//set the amount of avoidance 
+		void set_slow_avoidance_factor(double factor){
+			slow_avoidance_factor=factor;
+		}
 
-  //get the amount of avoidance 
-  double get_fast_avoidance_factor(){
-  return fast_avoidance_factor;
-  }
-  
-  //get the agression factor that is associateds with the given speed
-  double get_avoidance_factor();
+		//get the amount of avoidance 
+		double get_slow_avoidance_factor() const {
+			return slow_avoidance_factor;
+		}
 
-  void set_correction_step_size(double correction_size);  
-  void set_desired_robot_orientation(double orientation);
-  
-  void set_robot_avoid_ball_amount(int amount);   
-  bool robot_avoids_ball();
-  void set_robot_avoids_ball(bool avoid);
-  
-  bool robot_avoids_goal();
-  void set_robot_avoids_goal(bool avoid);
-  
-  bool robot_stays_on_own_half();
-  void set_robot_stays_on_own_half(bool avoid);
+		//set the amount of avoidance 
+		void set_fast_avoidance_factor(double factor){
+			fast_avoidance_factor = factor;
+		}
 
-  void set_robot_avoid_opponent_goal_amount(double amount);
-  bool robot_stays_away_from_opponent_goal();
-  void set_robot_stays_away_from_opponent_goal(bool avoid);
-  
-  bool robot_is_in_desired_state();
-  bool robot_is_in_desired_location();
-  bool robot_is_in_desired_orientation();
+		//get the amount of avoidance 
+		double get_fast_avoidance_factor() const {
+			return fast_avoidance_factor;
+		}
 
+		//get the agression factor that is associateds with the given speed
+		double get_avoidance_factor() const;
 
-    
- private:
-  point clip_point(point p, point bound1, point bound2);
-  bool check_vector(point start, point dest, point direction);
+		void set_correction_step_size(double correction_size);  
+		void set_desired_robot_orientation(double orientation);
 
-  bool destInitialized;//has a destination been specified?
-  point currDest;//current destination
-  float outOfBoundsMargin;//distance to remain from sidelines to prevent from going oob
-  double maxLookahead;
-  double avoidance_factor; //smaller factor makes robot more aggressive (i.e. less eager to avoid obstacles)
-  double slow_avoidance_factor;
-  double fast_avoidance_factor; 
-  bool avoid_ball;
-  bool avoids_goal;
-  double avoid_goal_amount;
-  bool robot_stays_on_half;
-  bool robot_avoids_opponent_goal;
-  double avoid_opponent_goal_amount;
-  
+		void set_robot_avoid_ball_amount(int amount);   
+		bool robot_avoids_ball();
+		void set_robot_avoids_ball(bool avoid);
+
+		bool robot_avoids_goal();
+		void set_robot_avoids_goal(bool avoid);
+
+		bool robot_stays_on_own_half();
+		void set_robot_stays_on_own_half(bool avoid);
+
+		void set_robot_avoid_opponent_goal_amount(double amount);
+		bool robot_stays_away_from_opponent_goal();
+		void set_robot_stays_away_from_opponent_goal(bool avoid);
+
+		bool robot_is_in_desired_state();
+		bool robot_is_in_desired_location();
+		bool robot_is_in_desired_orientation();
+
+		static point clip_point(const point& p, const point& bound1, const point& bound2);
+
+	private:
+		bool check_vector(const point& start, const point& dest, const point& direction) const;
+
+		bool dest_initialized;//has a destination been specified?
+		point curr_dest;//current destination
+		double outofbounds_margin;//distance to remain from sidelines to prevent from going oob
+		double max_lookahead;
+		double avoidance_factor; //smaller factor makes robot more aggressive (i.e. less eager to avoid obstacles)
+		double slow_avoidance_factor;
+		double fast_avoidance_factor; 
+		bool avoid_ball;
+		bool avoids_goal;
+		double avoid_goal_amount;
+		bool robot_stays_on_half;
+		bool robot_avoids_opponent_goal;
+		double avoid_opponent_goal_amount;
+
 };
 
 #endif
