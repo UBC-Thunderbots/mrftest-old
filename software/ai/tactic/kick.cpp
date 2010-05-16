a0c6f377fdf2ec6a2fa9ac1f948b6a10b884efb3
simonsyd <simonsyd@localhost>
2010-05-16 01:16:39 +0000

    Changed to allow setting of strength.

--- software/ai/tactic/kick.cpp
+++ software/ai/tactic/kick.cpp
@@ -1,41 +1,31 @@
 #include "ai/tactic/kick.h"
+
 #include <iostream>
 
-kick::kick(ball::ptr ball, field::ptr field, controlled_team::ptr team, player::ptr player) : tactic(ball, field, team, player), turn_tactic(new turn(ball, field, team, player)), should_chip(true) {
+namespace {
+	const double TOL = 10;
 }
 
-void kick::set_target(const point& p) {
-	the_target = p;
-}
-
-void kick::set_chip(const bool& chip) {
-	should_chip = chip;
+kick::kick(ball::ptr ball, field::ptr field, controlled_team::ptr team, player::ptr player) : tactic(ball, field, team, player), turn_tactic(new turn(ball, field, team, player)), should_chip(false), chip_strength(1.0), kick_strength(1.0) {
 }
 
 void kick::tick() {
 	// turn towards the target
 	turn_tactic->set_direction(the_target);
 
-	const double TOL = 10;
-
-	if (!turn_tactic->is_turned(TOL)) 
+	if (!turn_tactic->is_turned(TOL)) {
 		turn_tactic->tick();	
-	else {
-//		std::cout << "kicking" << std::endl;
-		// assume maximum strength for now...
-		if (should_chip){
-			if (the_player->has_ball()){
-				the_player->chip(1);
-			}
-		}
-			
-		else{
-			if (the_player->has_ball()){
-				the_player->kick(1);
-			}
-		}
-			
+		return;
 	}
 
+	if (should_chip) {
+		if (the_player->has_ball()){
+			the_player->chip(chip_strength);
+		}
+	} else {
+		if (the_player->has_ball()){
+			the_player->kick(kick_strength);
+		}
+	}
 }
 
--- software/ai/tactic/kick.h
+++ software/ai/tactic/kick.h
@@ -4,9 +4,10 @@
 #include "ai/tactic.h"
 #include "ai/tactic/turn.h"
 
-//
-// 
-//
+/**
+ * Tactic for doing either chip or kick.
+ * By default, kicks at full strength.
+ */
 class kick : public tactic {
 	public:
 		//
@@ -24,15 +25,30 @@ class kick : public tactic {
 		//
 		void tick();
 
-		//
-		// Sets the target position for this kick tactic.
-		//
-		void set_target(const point& p);	
+		/**
+		 * Sets the target position for this kick tactic.
+		 */
+		void set_target(const point& p) {
+			the_target = p;
+		}
 
-		//
-		// Sets rather the player chips the ball or kicks the ball.
-		//
-		void set_chip(const bool& chip);
+		/**
+		 * Enables and sets the strength of the chipping.
+		 * Value between 0 and 1.
+		 */
+		void set_chip(const double& s = 1.0) {
+			chip_strength = s;
+			should_chip = true;
+		}
+
+		/**
+		 * Enables and sets the strength of the chipping.
+		 * Value between 0 and 1.
+		 */
+		void set_kick(const double& k = 1.0) {
+			kick_strength = k;
+			should_chip = false;
+		}
 
 	protected:		
 
@@ -44,6 +60,9 @@ class kick : public tactic {
 		// Holds if this tactic should chip the ball rather than simply kicking it
 		bool should_chip;
 
+		double chip_strength;
+
+		double kick_strength;
 };
 
 #endif
