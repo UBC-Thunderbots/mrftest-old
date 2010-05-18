e16904ad33113a756471bb71481687bd6b4d1125
simonsyd <simonsyd@localhost>
2010-05-18 19:38:04 +0000

    Simple Refactoring.

--- software/ai/tactic/chase.cpp
+++ software/ai/tactic/chase.cpp
@@ -1,20 +1,24 @@
 #include "ai/tactic/chase.h"
 #include "geom/angle.h"
 
+#include <iostream>
+
+namespace {
+	double VEL_FACTOR = 0.5;
+	double ACL_FACTOR = 0.125;
+}
+
 chase::chase(player::ptr player, world::ptr world) : the_player(player), the_world(world), move_tactic(player, world) {
 }
 
-#include <iostream>
-
-void chase::tick()
-{
+void chase::tick() {
 	const ball::ptr the_ball(the_world->ball());
 //	std::cout << (the_ball->position()+the_ball->est_velocity()/2) << std::endl;	
 //	std::cout << the_ball->est_velocity() << std::endl;
 	//move_tactic->set_position(the_ball->position()+the_ball->est_velocity()/2);
 	// predict ball position based on velocity and accleration
  	//NOTE THAT MOVING BALL MANUALLY WITH CURSOR CAN GIVE VERY LARGE VELOCITY AND ACCELERATION
-	move_tactic.set_position(the_ball->position()+the_ball->est_velocity()/2 + the_ball->est_acceleration()/8);
-
+	move_tactic.set_position(the_ball->position() + VEL_FACTOR * the_ball->est_velocity() + ACL_FACTOR * the_ball->est_acceleration());
 	move_tactic.tick();
 }
+
