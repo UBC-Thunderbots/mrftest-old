b0b549ef0b743a65f85bdc1787f4469e429eec8e
martincy <martincy@localhost>
2010-05-20 21:24:22 +0000

    Added implementation of execute_penalty_enemy.

--- software/ai/role/execute_penalty_enemy.cpp
+++ software/ai/role/execute_penalty_enemy.cpp
@@ -1,12 +1,87 @@
 #include "ai/role/execute_penalty_enemy.h"
+#include "ai/util.h"
 
-execute_penalty_enemy::execute_penalty_enemy(world::ptr world) : the_world(world) {
+execute_penalty_enemy::execute_penalty_enemy(world::ptr world) : the_world(world), 
+	starting_position(-the_world->field().length(), -the_world->field().goal_width() + robot::MAX_RADIUS),
+	ending_position(-the_world->field().length(), the_world->field().goal_width() - robot::MAX_RADIUS) {
+	should_patrol = true;
+	should_go_to_start = true;
 }
 
 void execute_penalty_enemy::tick(){
+	if (should_patrol) {
+		patrol();		
+	} else {
+		// not at ready position yet
+		if ((the_goalie->position() - starting_position).lensq() > ai_util::POS_CLOSE) {
+			move_to_start->tick();
+		} else if (!enemy_moved) {
+			// check whether the shooter has begun shooting the ball
+			enemy_moved = detect_enemy_movement();
+		} 
 
+		// if the shooter has made a move, move to the other corner right away
+		if (enemy_moved) {
+			move_to_end->tick();
+		}
+	}
 }
 
-void execute_penalty_enemy::robots_changed() {
+void execute_penalty_enemy::robots_changed() {	
+	the_goalie = the_robots[0];
+
+	move_to_start = move::ptr(new move(the_goalie, the_world));
+	move_to_start->set_position(starting_position);
+
+	move_to_end = move::ptr(new move(the_goalie, the_world));
+	move_to_end->set_position(ending_position);
+}
+
+void execute_penalty_enemy::patrol() {
+	if (should_go_to_start) {
+		// reached the start position
+		if ((the_goalie->position() - starting_position).lensq() <= ai_util::POS_CLOSE) {
+			should_go_to_start = false;
+			move_to_end->tick();
+		} else {
+			move_to_start->tick();
+		}
+	} else {
+		// reached the end position
+		if ((the_goalie->position() - ending_position).lensq() <= ai_util::POS_CLOSE) {
+			should_go_to_start = true;
+			move_to_start->tick();
+		} else {
+			move_to_end->tick();
+		}
+	}
+}
+
+bool execute_penalty_enemy::detect_enemy_movement() {
+	
+	if (!the_shooter) {
+		const team& enemy = the_world->enemy;
+		const point the_goal(-the_world->field().length(),0);
+
+		double min_dist = -1;
+		double dist;
+
+		// gets the shooter by looking at the closest enemy robot
+		for (unsigned int i = 0; i < enemy.size(); ++i) {
+			robot::ptr robot = enemy.get_robot(i);
+			dist = (robot->position() - the_goal).lensq();
+			if (min_dist == -1 || dist < min_dist) {
+				the_shooter = robot;
+				min_dist = dist;
+			}
+		}
+
+		last_orientation = the_shooter->orientation();
+	}
+	
+	bool ret = the_shooter->orientation() - last_orientation > ai_util::ORI_CLOSE;
+	last_orientation = the_shooter->orientation();
+
+	return ret;
 }
 
--- software/ai/role/execute_penalty_enemy.h
+++ software/ai/role/execute_penalty_enemy.h
@@ -2,6 +2,7 @@
 #define AI_ROLE_EXECUTE_PENALTY_ENEMY_H
 
 #include "ai/role/role.h"
+#include "ai/tactic/move.h"
 
 /**
  * Gets the robots to go to their execute_penalty_enemy positions.
@@ -31,7 +32,67 @@ class execute_penalty_enemy : public role {
 		void robots_changed();
 
 	private:
+		/**
+		 * Patrols between the starting position and ending position.
+		 */
+		void patrol();
+	
+		/**
+		 * Returns whether the shooter has made a move.
+		 */
+		bool detect_enemy_movement();
+
 		const world::ptr the_world;
+
+		/**
+		 * The starting position.
+		 */
+		const point starting_position;
+
+		/**
+		 * The ending position.
+		 */
+		const point ending_position;
+
+		/**
+		 * The tactic to move to the starting position.
+		 */
+		move::ptr move_to_start;
+
+		/**
+		 * The tactic to move to the ending position.
+		 */
+		move::ptr move_to_end;
+
+		/**
+		 * Whether the goalie should patrol or try to save the ball.
+		 */
+		bool should_patrol;
+
+		/**
+		 * Used for patrol to see if the robot should move to start or end.
+		 */
+		bool should_go_to_start;
+
+		/**
+		 * The shooter has begun shooting - move to the other corner as fast as possible.
+		 */
+		bool enemy_moved;
+
+		/**
+		 * The robot used for the goalie (the only robot assigned to this role.
+		 */
+		player::ptr the_goalie;
+		
+		/**
+		 * The enemy shooter.
+		 */
+		robot::ptr the_shooter;
+
+		/**
+		* The last orientation of the shooter.
+		*/
+		double last_orientation;
 };
 
 #endif
