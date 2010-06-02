cfcb90e1e7fd132eed864e21c0c563687c09f924
cedricl <cedricl@localhost>
2010-06-02 23:08:40 +0000

    Robot used to kick if target was within 10 radians. This is now fixed.

--- software/ai/tactic/kick.cpp
+++ software/ai/tactic/kick.cpp
@@ -1,10 +1,11 @@
 #include "ai/tactic/kick.h"
 #include "ai/util.h"
+#include "geom/angle.h"
 
 #include <iostream>
 
 namespace {
-	const double TOL = 10;
+	const double TOL = 10 / 180.0 * M_PI;
 }
 
 /*
@@ -35,8 +36,8 @@ void kick::tick() {
 
 	// turn towards the target
 	turn_tactic.set_direction(kick_target);
-	// if (angle_diff(kick_target.orientation(), the_player->orientation()) < ai_util::ORI_CLOSE) {
-	if (!turn_tactic.is_turned(TOL)) {
+	if (angle_diff((kick_target-the_player->position()).orientation(), the_player->orientation()) < ai_util::ORI_CLOSE) {
+	//if (!turn_tactic.is_turned(TOL)) {
 		turn_tactic.tick();	
 		return;
 	}
