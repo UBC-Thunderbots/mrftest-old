3e35407217ccad0486928f7125f006bc5c683d1d
simonsyd <simonsyd@localhost>
2010-06-05 23:58:07 +0000

    Allowed angle compensate.

--- software/robot_controller/tunable_adhoc_controller.cpp
+++ software/robot_controller/tunable_adhoc_controller.cpp
@@ -17,19 +17,23 @@ namespace {
 
 	const double DAMP = 0.5;
 
-	// enumerate the parameters
-	enum { PARAM_PROP = 0, PARAM_DIFF, PARAM_XY_RATIO, PARAM_THRESH, PARAM_A_PROP, PARAM_A_DIFF, };
+	const std::string PARAM_NAMES[] = {"Proportional", "Differential", "Y/X Ratio", "Maximum Speed", "Proportional Angle", "Differential Angle", "Maximum Angular Speed", "X/Angle speed ratio compensate"};
 
-	const double DEF_PROP = 2.0;
-	const double DEF_DIFF = 0;
+	// enumerate the parameters
+	enum { PARAM_PROP = 0, PARAM_DIFF, PARAM_XY_RATIO, PARAM_THRESH, PARAM_A_PROP, PARAM_A_DIFF, PARAM_A_THRESH, PARAM_XA_RATIO };
+
+	const double DEF_PROP = 6.0;
+	const double DEF_DIFF = 0.0;
 	const double DEF_XY_RATIO = 1.3;
-	const double DEF_THRESH = 1.0;
-	const double DEF_A_PROP = 4;
-	const double DEF_A_DIFF = 0;
+	const double DEF_THRESH = 4.0;
+	const double DEF_A_PROP = 4.0;
+	const double DEF_A_DIFF = 0.0;
+	const double DEF_A_THRESH = 100.0;
+	const double DEF_XA_RATIO = 0.0;
 
 	// array of defaults
-	const double arr_def[] = { DEF_PROP, DEF_DIFF, DEF_XY_RATIO, DEF_THRESH, DEF_A_PROP, DEF_A_DIFF, };
-	const int P = sizeof(arr_def) / sizeof(arr_def[0]);
+	const double ARR_DEF[] = { DEF_PROP, DEF_DIFF, DEF_XY_RATIO, DEF_THRESH, DEF_A_PROP, DEF_A_DIFF, DEF_A_THRESH, DEF_XA_RATIO };
+	const int P = sizeof(ARR_DEF) / sizeof(ARR_DEF[0]);
 
 	class tunable_adhoc_controller : public robot_controller, public tunable_controller {
 		public:
@@ -64,21 +68,14 @@ namespace {
 			double prev_new_ori;
 	};
 
-	const std::vector<double> param_default(arr_def, arr_def + P);
+	const std::vector<double> param_default(ARR_DEF, ARR_DEF + P);
 
 	tunable_adhoc_controller::tunable_adhoc_controller(player::ptr plr) : plr(plr), initialized(false), error_pos(10), error_ori(10) {
 		param = param_default;
 	}
 
 	const std::vector<std::string> tunable_adhoc_controller::get_params_name() const {
-		std::vector<std::string> ret;
-		ret.push_back("Proportional");
-		ret.push_back("Differential");
-		ret.push_back("Y/X Ratio");
-		ret.push_back("Threshold");
-		ret.push_back("Proportional Angle");
-		ret.push_back("Differential Angle");
-		return ret;
+		return std::vector<std::string>(PARAM_NAMES, PARAM_NAMES + P);
 	}
 
 	void tunable_adhoc_controller::move(const point &new_position, double new_orientation, point &linear_velocity, double &angular_velocity) {
@@ -103,13 +100,16 @@ namespace {
 		}
 
 		// update the previous
+		/*
 		for (int t = 9; t > 0; --t) {
 			error_pos[t] = error_pos[t - 1];
 			error_ori[t] = error_ori[t - 1];
 		}
 		error_pos[0] = new_dir;
 		error_ori[0] = new_da;
+		*/
 
+		/*
 		point accum_pos(0, 0);
 		double accum_ori(0);
 		for (int t = 9; t >= 0; --t) {
@@ -118,6 +118,7 @@ namespace {
 			accum_pos += error_pos[t];
 			accum_ori += error_ori[t];
 		}
+		*/
 
 		const double px = error_pos[0].x;
 		const double py = error_pos[0].y;
@@ -138,10 +139,15 @@ namespace {
 
 		linear_velocity.x = px * param[PARAM_PROP] + vx * param[PARAM_DIFF];
 		linear_velocity.y = (py * param[PARAM_PROP] + vy * param[PARAM_DIFF]) * param[PARAM_XY_RATIO];
-		if(linear_velocity.len() > param[PARAM_THRESH]) {
+		if (linear_velocity.len() > param[PARAM_THRESH]) {
 			linear_velocity *= param[PARAM_THRESH] / linear_velocity.len();
 		}
-		angular_velocity = pa * param[PARAM_A_PROP] + va * param[PARAM_A_DIFF];
+		angular_velocity = pa * param[PARAM_A_PROP] + va * param[PARAM_A_DIFF] + linear_velocity.x * param[PARAM_XA_RATIO];
+		if (angular_velocity > param[PARAM_A_THRESH]) {
+			angular_velocity = param[PARAM_A_THRESH];
+		} else if (angular_velocity < -param[PARAM_A_THRESH]) {
+			angular_velocity = -param[PARAM_A_THRESH];
+		}
 	}
 
 	void tunable_adhoc_controller::clear() {
@@ -150,7 +156,7 @@ namespace {
 
 	class tunable_adhoc_controller_factory : public robot_controller_factory {
 		public:
-			tunable_adhoc_controller_factory() : robot_controller_factory("Ad Hoc =/") {
+			tunable_adhoc_controller_factory() : robot_controller_factory("Ad Hoc =D") {
 			}
 
 			robot_controller::ptr create_controller(player::ptr plr, bool, unsigned int) const {
