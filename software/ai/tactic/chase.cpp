824f5bb567e125424a10271adcb3138b6bc68d63
JasonChang <jasonchang@localhost>
2010-06-10 07:16:24 +0000

    made it so that chase powers through to the ball when close, rather than stopping right when it reaches the ball

--- software/ai/tactic/chase.cpp
+++ software/ai/tactic/chase.cpp
@@ -5,10 +5,12 @@
 
 namespace {
 #warning magic constants
-	//const double VEL_FACTOR = 0.5;
-	//const double ACL_FACTOR = 0.125;
-	const double VEL_FACTOR = 0;
-	const double ACL_FACTOR = 0;
+        const double VEL_FACTOR = 0;//origionally 0.5 from byrons code
+        const double ACL_FACTOR = 0;//origionally 0.125 from byrons code
+        const double STILL_BALL_AMOUNT = 0.005;
+        //if two unit vectors dot to give this value then they are the same direction
+        const double SAME_DIRECTION = 0.98;
+        const double OVERSHOOT_AMOUNT = 0.01;
 }
 
 chase::chase(player::ptr player, world::ptr world) : tactic(player), the_world(world), navi(player, world) {
@@ -16,12 +18,20 @@ chase::chase(player::ptr player, world::ptr world) : tactic(player), the_world(w
 
 void chase::tick() {
 	const ball::ptr the_ball(the_world->ball());
-//	std::cout << (the_ball->position()+the_ball->est_velocity()/2) << std::endl;	
-//	std::cout << the_ball->est_velocity() << std::endl;
-	//move_tactic->set_position(the_ball->position()+the_ball->est_velocity()/2);
 	// predict ball position based on velocity and accleration
  	//NOTE THAT MOVING BALL MANUALLY WITH CURSOR CAN GIVE VERY LARGE VELOCITY AND ACCELERATION
-	navi.set_position(the_ball->position() + VEL_FACTOR * the_ball->est_velocity() + ACL_FACTOR * the_ball->est_acceleration());
+	point pos = the_ball->position() + VEL_FACTOR * the_ball->est_velocity() + ACL_FACTOR * the_ball->est_acceleration();
+
+	point player_ball_direction = (the_ball->position()-the_player->position()).norm();
+	point ball_direction = (the_ball->est_velocity()).norm();
+
+	//if we are heading parallel to ball velocity or the ball is still plan to overshoot the ball by a little bit 
+	//so that we can power through,beat,etc the opponent to the ball
+	if( (the_ball->est_velocity()).len() <= STILL_BALL_AMOUNT || player_ball_direction.dot(ball_direction)>SAME_DIRECTION ){
+	  pos += player_ball_direction.norm()*OVERSHOOT_AMOUNT;
+	}
+
+	navi.set_position(pos);
 	navi.set_flags(flags);
 	navi.tick();
 }
