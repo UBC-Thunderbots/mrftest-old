cc089aefccecedb0d7d60b4acdf2ffc2b7d437cd
simonsyd <simonsyd@localhost>
2010-06-11 01:47:22 +0000

    Passer should turn towards the receiver.

--- software/ai/tactic/pass.cpp
+++ software/ai/tactic/pass.cpp
@@ -3,6 +3,8 @@
 #include "ai/tactic/move.h"
 #include "ai/util.h"
 
+#include <iostream>
+
 pass::pass(player::ptr player, world::ptr world, player::ptr receiver) : tactic(player), the_world(world), the_receiver(receiver) {
 }
 
@@ -10,14 +12,20 @@ void pass::tick() {
 	bool should_pass = ai_util::can_pass(the_world, the_receiver) && the_receiver->est_velocity().len() < ai_util::VEL_CLOSE;
 
 	if (should_pass) {
+		// std::cout << " pass: let's shoot " << std::endl;
 		kick::ptr tactic(new kick(the_player, the_world));
 		tactic->set_target(the_receiver->position());
 		tactic->tick();
 	} else {
+		// std::cout << " pass: move to receiver " << std::endl;
 		move::ptr tactic(new move(the_player, the_world));
+		// ALSO FACE TOWARDS RECEIVER
 		tactic->set_position(the_receiver->position());
+		if (ai_util::has_ball(the_player)) {
+			const point diff = the_receiver->position() - the_player->position();
+			tactic->set_orientation(diff.orientation());
+		}
 		tactic->tick();
 	}
 }
-
 
