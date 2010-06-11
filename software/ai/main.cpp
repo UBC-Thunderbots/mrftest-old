b1f9f50071bbbe432602f0482407c5172b0d08f1
chead <chead@localhost>
2010-06-11 00:18:43 +0000

    Implement custom team building.

--- software/ai/main.cpp
+++ software/ai/main.cpp
@@ -1,17 +1,132 @@
 #include "ai/window.h"
 #include "ai/world/world.h"
+#include "uicomponents/abstract_list_model.h"
 #include "util/clocksource_timerfd.h"
 #include "util/config.h"
 #include "util/timestep.h"
 #include "xbee/client/drive.h"
 #include "xbee/client/lowlevel.h"
 #include <algorithm>
+#include <cstdlib>
 #include <iostream>
 #include <vector>
 #include <stdint.h>
 #include <gtkmm.h>
 
 namespace {
+	/**
+	 * A list model that allows the user to mark each robot as friendly or
+	 * enemy.
+	 */
+	class team_customizer_model : public Glib::Object, public abstract_list_model {
+		public:
+			/**
+			 * A column indicating the state of the robot.
+			 */
+			Gtk::TreeModelColumn<bool> friendly_column;
+
+			/**
+			 * A column showing the name of the robot.
+			 */
+			Gtk::TreeModelColumn<Glib::ustring> name_column;
+
+			/**
+			 * Constructs a new model.
+			 *
+			 * \param[in] conf the configuration file to show.
+			 *
+			 * \return the new model.
+			 */
+			static Glib::RefPtr<team_customizer_model> create(const config &conf) {
+				Glib::RefPtr<team_customizer_model> p(new team_customizer_model(conf));
+				return p;
+			}
+
+			/**
+			 * \return a vector containing \c true if a robot is friendly or \c
+			 * false if it is enemy.
+			 */
+			const std::vector<bool> &friendly() const {
+				return friendly_;
+			}
+
+		private:
+			const config &conf;
+			std::vector<bool> friendly_;
+
+			team_customizer_model(const config &conf) : Glib::ObjectBase(typeid(team_customizer_model)), conf(conf), friendly_(conf.robots().size(), true) {
+				alm_column_record.add(friendly_column);
+				alm_column_record.add(name_column);
+			}
+
+			unsigned int alm_rows() const {
+				return conf.robots().size();
+			}
+
+			void alm_get_value(unsigned int row, unsigned int col, Glib::ValueBase &value) const {
+				if (col == static_cast<unsigned int>(friendly_column.index())) {
+					Glib::Value<bool> v;
+					v.init(friendly_column.type());
+					v.set(friendly_[row]);
+					value.init(friendly_column.type());
+					value = v;
+				} else if (col == static_cast<unsigned int>(name_column.index())) {
+					Glib::Value<Glib::ustring> v;
+					v.init(name_column.type());
+					v.set(conf.robots()[row].name);
+					value.init(name_column.type());
+					value = v;
+				} else {
+					std::abort();
+				}
+			}
+
+			void alm_set_value(unsigned int row, unsigned int col, const Glib::ValueBase &value) {
+				if (col == static_cast<unsigned int>(friendly_column.index())) {
+					Glib::Value<bool> v;
+					v.init(friendly_column.type());
+					Glib::ValueBase &vb(v);
+					vb = value;
+					friendly_[row] = v.get();
+				}
+			}
+	};
+
+	class custom_team_builder : public Gtk::Window {
+		public:
+			custom_team_builder(config &conf) : conf(conf), model(team_customizer_model::create(conf)) {
+				set_title("Thunderbots AI");
+				set_default_size(200, 200);
+
+				Gtk::TreeView *view = Gtk::manage(new Gtk::TreeView(model));
+				view->get_selection()->set_mode(Gtk::SELECTION_SINGLE);
+				view->append_column_editable("Friendly", model->friendly_column);
+				view->append_column("Name", model->name_column);
+				Gtk::ScrolledWindow *scroller = Gtk::manage(new Gtk::ScrolledWindow);
+				scroller->add(*view);
+				scroller->set_shadow_type(Gtk::SHADOW_IN);
+				add(*scroller);
+
+				show_all();
+			}
+
+		private:
+			config &conf;
+			Glib::RefPtr<team_customizer_model> model;
+
+			bool on_delete_event(GdkEventAny *) {
+				std::vector<uint64_t> to_remove;
+				const std::vector<bool> &friendly(model->friendly());
+				for (unsigned int i = 0; i < conf.robots().size(); ++i) {
+					if (!friendly[i]) {
+						to_remove.push_back(conf.robots()[i].address);
+					}
+				}
+				std::for_each(to_remove.begin(), to_remove.end(), sigc::mem_fun(conf.robots(), &config::robot_set::remove));
+				return false;
+			}
+	};
+
 	int main_impl(int argc, char **argv) {
 		Glib::OptionContext option_context;
 		option_context.set_summary("Runs the Thunderbots control process.");
@@ -76,9 +191,8 @@ namespace {
 			std::for_each(to_remove.begin(), to_remove.end(), sigc::mem_fun(conf.robots(), &config::robot_set::remove));
 			refbox_yellow = yellow;
 		} else if (custom) {
-#warning WRITE CODE HERE
-			std::cerr << "Custom team building is not supported yet.\n";
-			return 1;
+			custom_team_builder builder(conf);
+			Gtk::Main::run(builder);
 		} else {
 			std::cerr << "WTF happened?\n";
 			return 1;
