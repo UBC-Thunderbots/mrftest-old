30fc4849a4b786cf1b894e7f68ad94a6d8768c0d
simonsyd <simonsyd@localhost>
2010-06-12 03:46:46 +0000

    Passing.

--- software/ai/tactic/pass.cpp
+++ software/ai/tactic/pass.cpp
@@ -1,4 +1,5 @@
 #include "ai/tactic/pass.h"
+#include "ai/tactic/chase.h"
 #include "ai/tactic/kick.h"
 #include "ai/tactic/move.h"
 #include "ai/util.h"
@@ -9,23 +10,28 @@ pass::pass(player::ptr player, world::ptr world, player::ptr receiver) : tactic(
 }
 
 void pass::tick() {
+	if (!ai_util::has_ball(the_player) || !the_player->sense_ball()) {
+		chase chase_tactic(the_player, the_world);
+		chase_tactic.set_flags(flags);
+		chase_tactic.tick();
+		return;
+	}
+
 	bool should_pass = ai_util::can_pass(the_world, the_receiver) && the_receiver->est_velocity().len() < ai_util::VEL_CLOSE;
 
 	if (should_pass) {
 		// std::cout << " pass: let's shoot " << std::endl;
-		kick::ptr tactic(new kick(the_player, the_world));
-		tactic->set_target(the_receiver->position());
-		tactic->tick();
+		kick kick_tactic(the_player, the_world);
+		kick_tactic.set_target(the_receiver->position());
+		kick_tactic.tick();
 	} else {
 		// std::cout << " pass: move to receiver " << std::endl;
-		move::ptr tactic(new move(the_player, the_world));
+		move move_tactic(the_player, the_world);
 		// ALSO FACE TOWARDS RECEIVER
-		tactic->set_position(the_receiver->position());
-		if (ai_util::has_ball(the_player)) {
-			const point diff = the_receiver->position() - the_player->position();
-			tactic->set_orientation(diff.orientation());
-		}
-		tactic->tick();
+		move_tactic.set_position(the_receiver->position());
+		const point diff = the_receiver->position() - the_player->position();
+		move_tactic.set_orientation(diff.orientation());
+		move_tactic.tick();
 	}
 }
 
