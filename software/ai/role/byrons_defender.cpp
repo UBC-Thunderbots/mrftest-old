4e817f3c4e8354adb3776c54314c108aeea6b7d2
byronknoll <byronknoll@localhost>
2010-06-19 09:55:29 +0000

    shoot.

--- software/ai/role/byrons_defender.cpp
+++ software/ai/role/byrons_defender.cpp
@@ -1,5 +1,6 @@
 #include "ai/role/byrons_defender.h"
 #include "ai/tactic/move.h"
+#include "ai/tactic/shoot.h"
 #include "ai/util.h"
 #include "util/algorithm.h"
 #include "geom/util.h"
@@ -13,21 +14,27 @@ void byrons_defender::tick() {
 	unsigned int flags = ai_flags::calc_flags(the_world->playtype());
 	
 	for (size_t index = 0; index < the_robots.size(); ++index) {
-		move tactic(the_robots[index], the_world);
 		point ball = the_world->ball()->position();
-		if (the_robots.size() == 2) {
-			if (index == 0) {
-				if (the_robots[0]->position().y > the_robots[1]->position().y) ball.y+=0.4;
-				else ball.y-=0.4;
-			} else {
-				if (the_robots[0]->position().y < the_robots[1]->position().y) ball.y+=0.4;
-				else ball.y-=0.4;
+		if ((the_robots[index]->position() - ball).len() > 0.4) {
+			move tactic(the_robots[index], the_world);
+			if (the_robots.size() == 2) {
+				if (index == 0) {
+					if (the_robots[0]->position().y > the_robots[1]->position().y) ball.y+=0.4;
+					else ball.y-=0.4;
+				} else {
+					if (the_robots[0]->position().y < the_robots[1]->position().y) ball.y+=0.4;
+					else ball.y-=0.4;
+				}
 			}
+			point half = (ball + the_world->field().friendly_goal()) * 0.5;
+			tactic.set_position(point(half.x, half.y));
+			tactic.set_flags(flags);
+			tactic.tick();
+		} else {
+			shoot tactic(the_robots[index], the_world);
+			tactic.set_flags(flags);
+			tactic.tick();
 		}
-		point half = (ball + the_world->field().friendly_goal()) * 0.5;
-		tactic.set_position(point(half.x, half.y));
-		tactic.set_flags(flags);
-		tactic.tick();
 	}
 }
 
