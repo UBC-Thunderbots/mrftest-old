aaec2fe2e6c34d7401a55f829f13083a15c870f1
cedricl <cedricl@localhost>
2010-06-19 10:20:06 +0000

    better_strategy now assigns 3 offenders if team has ball, and always assigns player with ball to be an offender

--- software/ai/strategy/better_strategy.cpp
+++ software/ai/strategy/better_strategy.cpp
@@ -10,7 +10,7 @@
 #include "ai/role/penalty_friendly.h"
 #include "ai/role/penalty_enemy.h"
 #include "ai/role/victory_dance.h"
-
+#include "ai/util.h"
 
 #include <iostream>
 
@@ -46,20 +46,14 @@ void better_strategy::in_play_assignment() {
 	roles.clear();
 	if (the_team.size() == 0) return;
 
-	goalie::ptr goalie_role(new goalie(the_world));
-	roles.push_back(role::ptr(goalie_role));
-	std::vector<player::ptr> goalie;
-	goalie.push_back(the_team.get_player(0));
-	goalie_role->set_robots(goalie);
-
-#warning TODO
-
 	defensive2::ptr defensive_role(new defensive2(the_world));
 	offensive::ptr offensive_role(new offensive(the_world));
 	roles.push_back(role::ptr(defensive_role));
 	roles.push_back(role::ptr(offensive_role));
 	std::vector<player::ptr> defenders;
 	std::vector<player::ptr> offenders;
+
+	defenders.push_back(the_team.get_player(0));
 
 	if (the_team.size() >= 2)
 		offenders.push_back(the_team.get_player(1));
@@ -70,14 +64,25 @@ void better_strategy::in_play_assignment() {
 	if (the_team.size() >= 4)
 		offenders.push_back(the_team.get_player(3));
 
-	if (the_team.size() >= 5)
-		defenders.push_back(the_team.get_player(4));
+	// 3 offenders, 1 defender if we have the ball
+	// otherwise 2 offenders, 2 defenders
+	if (the_team.size() >= 5){
+		if (ai_util::friendly_posses_ball(the_world))
+			offenders.push_back(the_team.get_player(4));
+		else
+			defenders.push_back(the_team.get_player(4));
+	}
 	
+	// If we have ball and ball is on non-goalie defender, switch this defender to offender
+	// and switch robot 4 back to defender
+	if (defenders.size() >= 2 && ai_util::posses_ball(the_world,defenders[1]))
+		std::swap(defenders[1],offenders[offenders.size()-1]);
 	// extra players become offenders
 	for (size_t i = 5; i < the_team.size(); ++i)
 		offenders.push_back(the_team.get_player(i));
 
 	defensive_role->set_robots(defenders);
+	defensive_role->set_goalie(the_team.get_player(0));
 	offensive_role->set_robots(offenders);
 }
 
@@ -89,12 +94,6 @@ player::ptr better_strategy::minus_one_assignment() {
 
 	if (the_team.size() == 1) return the_team.get_player(0);
 
-	goalie::ptr goalie_role(new goalie(the_world));
-	roles.push_back(role::ptr(goalie_role));
-	std::vector<player::ptr> goalie;
-	goalie.push_back(the_team.get_player(0));
-	goalie_role->set_robots(goalie);
-
 	// other players just sort by distance
 
 	defensive2::ptr defensive_role(new defensive2(the_world));
@@ -104,20 +103,28 @@ player::ptr better_strategy::minus_one_assignment() {
 	std::vector<player::ptr> defenders;
 	std::vector<player::ptr> offenders;
 
+	defenders.push_back(the_team.get_player(0));
 	if (the_team.size() >= 3)
 		defenders.push_back(the_team.get_player(2));
 
 	if (the_team.size() >= 4)
 		offenders.push_back(the_team.get_player(3));
 
-	if (the_team.size() >= 5)
-		defenders.push_back(the_team.get_player(4));
+	// 3 offenders, 1 defender if we have the ball
+	// otherwise 2 offenders, 2 defenders
+	if (the_team.size() >= 5){
+		if (ai_util::friendly_posses_ball(the_world))
+			offenders.push_back(the_team.get_player(4));
+		else
+			defenders.push_back(the_team.get_player(4));
+	}
 	
 	// extra players become offenders
 	for (size_t i = 5; i < the_team.size(); ++i)
 		offenders.push_back(the_team.get_player(i));
 
 	defensive_role->set_robots(defenders);
+	defensive_role->set_goalie(the_team.get_player(0));
 	offensive_role->set_robots(offenders);
 
 	return the_team.get_player(1);
