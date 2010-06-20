5907eaa7ba6bd8616308718e7337c3fddab38c15
JasonChang <jasonchang@localhost>
2010-06-20 03:54:03 +0000

    made robot navigator prevent goalie from entering it's own net area

--- software/ai/navigator/robot_navigator.cpp
+++ software/ai/navigator/robot_navigator.cpp
@@ -189,6 +189,14 @@ point robot_navigator::get_inbounds_point(point dst){
 	wantdest = clip_circle( the_field.enemy_goal_boundary().first, robot::MAX_RADIUS , wantdest);
 	wantdest = clip_circle( the_field.enemy_goal_boundary().second, robot::MAX_RADIUS , wantdest);
 
+	point dir = wantdest - the_player->position();
+	if(dir.x<0.0){
+	  if(the_player->position().x >= std::min(the_field.friendly_goal_boundary().first.y, the_field.friendly_goal_boundary().second.y) &&
+	     the_player->position().x <=  std::max(the_field.friendly_goal_boundary().first.y, the_field.friendly_goal_boundary().second.y)){
+	    wantdest.x = std::max(the_field.friendly_goal_boundary().first.x + robot::MAX_RADIUS, wantdest.x);
+	  }
+	}
+
 	return wantdest;
 }
 
