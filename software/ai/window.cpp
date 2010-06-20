4501802e2e2790254067bc87b799fca7e5bcf63a
chead <chead@localhost>
2010-06-20 13:10:27 +0000

    Make the robots table and annunciator panel grow to have enough space to be usefully visible.

--- software/ai/window.cpp
+++ software/ai/window.cpp
@@ -271,7 +271,7 @@ ai_window::ai_window(ai &ai, bool show_vis) : the_ai(ai), strategy_controls(0), 
 	strategy_vbox.pack_start(strategy_chooser, Gtk::PACK_SHRINK);
 	put_strategy_controls();
 	strategy_frame->add(strategy_vbox);
-	vbox->pack_start(*strategy_frame, Gtk::PACK_EXPAND_WIDGET);
+	vbox->pack_start(*strategy_frame, Gtk::PACK_SHRINK);
 
 	Gtk::Frame *rc_frame = Gtk::manage(new Gtk::Frame("Robot Controller"));
 	rc_chooser.append_text("<Select RC>");
@@ -287,14 +287,14 @@ ai_window::ai_window(ai &ai, bool show_vis) : the_ai(ai), strategy_controls(0), 
 	rc_chooser.signal_changed().connect(sigc::mem_fun(this, &ai_window::on_rc_changed));
 	rc_vbox.pack_start(rc_chooser, Gtk::PACK_SHRINK);
 	rc_frame->add(rc_vbox);
-	vbox->pack_start(*rc_frame, Gtk::PACK_EXPAND_WIDGET);
+	vbox->pack_start(*rc_frame, Gtk::PACK_SHRINK);
 
 	vis_button.set_label("Visualizer");
 	vis_button.set_active(show_vis);
 	vis_button.signal_toggled().connect(sigc::mem_fun(this, &ai_window::on_vis_toggled));
 	vbox->pack_start(vis_button, Gtk::PACK_SHRINK);
 
-	vbox->pack_start(*Gtk::manage(new annunciator), Gtk::PACK_SHRINK);
+	vbox->pack_start(*Gtk::manage(new annunciator), Gtk::PACK_EXPAND_WIDGET);
 
 	notebook->append_page(*vbox, "Main");
 
