0cbdbac5248cab26358ac492968a758119da532a
simonsyd <simonsyd@localhost>
2010-06-23 07:22:02 +0000

    Goalie now guards the same side of the goal as where the ball is.

--- software/ai/role/defensive2.cpp
+++ software/ai/role/defensive2.cpp
@@ -174,9 +174,9 @@ void defensive2::tick() {
 		goalie->set_state(typeid(*this), state);
 	}
 	if (ballpos.y > robot::MAX_RADIUS * 2) {
-		state->top = false;
-	} else if (ballpos.y < -robot::MAX_RADIUS * 2) {
 		state->top = true;
+	} else if (ballpos.y < -robot::MAX_RADIUS * 2) {
+		state->top = false;
 	}
 
 	std::vector<player::ptr> defenders;
