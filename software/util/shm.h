4df43f2092f06945db0d517bc3e192a2f8596c5d
chead <chead@localhost>
2010-08-04 06:30:20 +0000

    Doxygenize.

--- software/util/shm.h
+++ software/util/shm.h
@@ -6,64 +6,70 @@
 #include <cstddef>
 #include <stdint.h>
 
-//
-// A block of shared memory backed by a file.
-//
+/**
+ * A block of shared memory backed by a file.
+ */
 class RawShmBlock : public NonCopyable {
 	public:
-		//
-		// Creates a new shared memory block in a new file.
-		//
-		RawShmBlock(std::size_t);
+		/**
+		 * Creates a new shared memory block in a new file.
+		 *
+		 * \param[in] sz the size of the block, in bytes.
+		 */
+		RawShmBlock(std::size_t sz);
 
-		//
-		// Opens an existing shared memory block backed by an open file.
-		//
-		RawShmBlock(FileDescriptor, std::size_t);
+		/**
+		 * Opens an existing shared memory block backed by an open file.
+		 *
+		 * \param[in] fd the descriptor of the open file.
+		 *
+		 * \param[in] sz the size of the block, in bytes.
+		 */
+		RawShmBlock(FileDescriptor fd, std::size_t sz);
 
-		//
-		// Destroys a shared memory block.
-		//
+		/**
+		 * Destroys a shared memory block.
+		 */
 		~RawShmBlock();
 
-		//
-		// Returns the size of the shared memory block.
-		//
+		/**
+		 * \return the size of the shared memory block.
+		 */
 		std::size_t size() const {
 			return size_;
 		}
 
-		//
-		// Returns a pointer to the first byte of the block.
-		//
+		/**
+		 * \return a pointer to the first byte of the block.
+		 */
 		operator void *() {
 			return data_;
 		}
 
-		//
-		// Returns a pointer to the first byte of the block.
-		//
+		/**
+		 * \returns a pointer to the first byte of the block.
+		 */
 		operator const void *() const {
 			return data_;
 		}
 
-		//
-		// Returns a pointer to the first byte of the block.
-		//
+		/**
+		 * \return a pointer to the first byte of the block.
+		 */
 		void *get() {
 			return data_;
 		}
 
-		//
-		// Returns a pointer to the first byte of the block.
-		//
+		/**
+		 * \return a pointer to the first byte of the block.
+		 */
 		const void *get() const {
 			return data_;
 		}
 
-		//
-		// Returns the file descriptor holding the shared memory block.
-		//
+		/**
+		 * \return the file descriptor holding the shared memory block.
+		 */
 		int fd() const {
 			return fd_;
 		}
@@ -74,55 +80,59 @@ class RawShmBlock : public NonCopyable {
 		uint8_t *data_;
 };
 
-//
-// A shared memory block containing an object of a particular type.
-//
+/**
+ * A shared memory block containing an object of a particular type.
+ *
+ * \tparam T the type of object held in the block.
+ */
 template<typename T>
 class ShmBlock : public NonCopyable {
 	public:
-		//
-		// Creates a new shared memory block in a new file.
-		//
+		/**
+		 * Creates a new shared memory block in a new file.
+		 */
 		ShmBlock() : raw(sizeof(T)) {
 		}
 
-		//
-		// Opens an existing shared memory block backed by an open file.
-		//
+		/**
+		 * Opens an existing shared memory block backed by an open file.
+		 *
+		 * \param fd the descriptor of the open file.
+		 */
 		ShmBlock(FileDescriptor fd) : raw(fd, sizeof(T)) {
 		}
 
-		//
-		// Allows access to the underlying object.
-		//
+		/**
+		 * \return a pointer to the underlying object.
+		 */
 		operator T *() {
 			return static_cast<T *>(raw.get());
 		}
 
-		// 
-		// Allows access to the underlying object.
-		//
+		/** 
+		 * \return a pointer to the underlying object.
+		 */
 		operator const T *() const {
 			return static_cast<const T *>(raw.get());
 		}
 
-		//
-		// Allows access to the underlying object.
-		//
+		/**
+		 * \return a pointer to the underlying object.
+		 */
 		T *operator->() {
 			return static_cast<T *>(raw.get());
 		}
 
-		// 
-		// Allows access to the underlying object.
-		//
+		/** 
+		 * \return a pointer to the underlying object.
+		 */
 		const T *operator->() const {
 			return static_cast<const T *>(raw.get());
 		}
 
-		//
-		// Returns the file descriptor holding the shared memory block.
-		//
+		/**
+		 * \return the file descriptor holding the shared memory block.
+		 */
 		int fd() const {
 			return raw.fd();
 		}
