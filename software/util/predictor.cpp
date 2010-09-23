46a10cae712a315694cea2ee3ae133f009a0627c
chead <chead@localhost>
2010-09-23 07:23:33 +0000

    Implement an object that performs generic one-dimensional linear-least-squares-based prediction.

---
+++ software/util/predictor.cpp
@@ -0,0 +1,143 @@
+#include "util/predictor.h"
+#include "geom/angle.h"
+#include "leastsquares/leastsquares.h"
+#include "util/time.h"
+#include "util/timestep.h"
+#include <cmath>
+
+namespace {
+	const int MAX_DEGREE = 3;
+	const int NUM_OLD_POSITIONS = 140;
+}
+
+Predictor::Predictor(bool angle) : angle(angle), initialized(false), lock_delta(0.0) {
+	// Record current time.
+	timespec_now(last_datum_timestamp);
+	lock_timestamp = last_datum_timestamp;
+
+	// Fill history data with zeroes.
+	vhistory.setlength(NUM_OLD_POSITIONS);
+	dhistory.setlength(NUM_OLD_POSITIONS);
+	weights.setlength(NUM_OLD_POSITIONS);
+	fmatrix.setlength(NUM_OLD_POSITIONS, MAX_DEGREE + 1);
+	for (int i = 0; i < NUM_OLD_POSITIONS; i++){
+		vhistory(i) = 0.0;
+		dhistory(i) = 1.0 / TIMESTEPS_PER_SECOND;
+	}
+
+	// Allocate space for the approximants.
+	approxv.setlength(MAX_DEGREE + 1);
+}
+
+Predictor::~Predictor() {
+}
+
+double Predictor::value(double delta, unsigned int deriv) const {
+	delta += lock_delta;
+	double v = 0;
+	for (int i = MAX_DEGREE; i >= static_cast<int>(deriv); --i) {
+		v = v * delta + approxv(i);
+	}
+	if (angle && !deriv) {
+		v = angle_mod(v);
+	}
+	return v;
+}
+
+double Predictor::value(const timespec &ts, unsigned int deriv) const {
+	timespec delta;
+	timespec_sub(ts, lock_timestamp, delta);
+	return value(timespec_to_double(delta), deriv);
+}
+
+void Predictor::lock_time(const timespec &ts) {
+	lock_timestamp = ts;
+	timespec diff;
+	timespec_sub(ts, last_datum_timestamp, diff);
+	lock_delta = timespec_to_double(diff);
+}
+
+void Predictor::add_datum(double value, const timespec &ts) {
+	// If this is our first datum, do a clear instead.
+	if (!initialized) {
+		// Remember that we're initialized.
+		initialized = true;
+
+		// Mark the current time as the most recent datum stamp.
+		last_datum_timestamp = ts;
+
+		// Fill the current value into all history cells.
+		// This means that the object appears to be stationary.
+		for (int i = 0; i < NUM_OLD_POSITIONS; ++i) {
+			vhistory(i) = value;
+			dhistory(i) = 1.0 / TIMESTEPS_PER_SECOND;
+		}
+
+		// Update the predictions.
+		update();
+
+		// Don't do any of the rest.
+		return;
+	}
+
+	// Compute delta time and update stamp.
+	timespec diff;
+	timespec_sub(ts, last_datum_timestamp, diff);
+	last_datum_timestamp = ts;
+	double delta_time = timespec_to_double(diff);
+
+	// In the angle case, move the new value close to the previous value.
+	if (angle) {
+		while (std::fabs(value - vhistory(0)) > M_PI + 1e-9) {
+			if (value > vhistory(0)) {
+				value -= 2 * M_PI;
+			} else {
+				value += 2 * M_PI;
+			}
+		}
+	}
+
+	// Add new data to history list.
+	for (int i = NUM_OLD_POSITIONS - 1; i; i--) {
+		vhistory(i) = vhistory(i - 1);
+		dhistory(i) = dhistory(i - 1);
+	}
+	vhistory(0) = value;
+	dhistory(0) = delta_time;
+
+	// Update the predictions.
+	update();
+}
+
+void Predictor::clear() {
+	initialized = false;
+}
+
+void Predictor::update() {
+	// Build the matrices.
+	double dd = 0; // dd is always nonpositive
+	for (int i = 0; i < NUM_OLD_POSITIONS; i++) {
+		if (i)
+			dd -= dhistory(i - 1);
+		// Weights are 1 / (number of AI time steps from the present + 1).
+		// Data further in the past will affect our predictions less.
+		weights(i) = 1.0 / (1 - dd * TIMESTEPS_PER_SECOND);
+		fmatrix(i, 0) = 1;
+		for (unsigned int j = 1; j < MAX_DEGREE + 1; j++)
+			fmatrix(i, j) = fmatrix(i, j - 1) * dd;
+	}
+
+	// If this is an angle predictor and the values are getting too big, reduce them to prevent floating point precision loss.
+	if (angle) {
+		while (std::fabs(vhistory(0)) > 64 * M_PI) {
+			double shift = vhistory(0) > 0 ? -64 * M_PI : 64 * M_PI;
+			for (int i = 0; i < NUM_OLD_POSITIONS; i++) {
+				vhistory(i) += shift;
+			}
+		}
+	}
+
+	// Perform the regressions.
+	buildgeneralleastsquares(vhistory, weights, fmatrix, NUM_OLD_POSITIONS, MAX_DEGREE + 1, approxv);
+}
+
---
+++ software/util/predictor.h
@@ -0,0 +1,87 @@
+#ifndef AI_WORLD_PREDICTABLE_H
+#define AI_WORLD_PREDICTABLE_H
+
+#include "geom/point.h"
+#include "leastsquares/ap.h"
+#include "util/time.h"
+
+/**
+ * Accumulates data points over time and predicts past, current, and future values and derivatives.
+ */
+class Predictor {
+	public:
+		/**
+		 * Constructs a new Predictor.
+		 *
+		 * \param[in] angle \c true to construct a Predictor suitable for operating on angles
+		 * (for example, including anti-windup code), or \c false to construct a Predictor suitable for operating on linear quantities.
+		 */
+		Predictor(bool angle);
+
+		/**
+		 * Destroys a Predictor.
+		 */
+		~Predictor();
+
+		/**
+		 * Gets the predicted value some length of time into the future (or past).
+		 *
+		 * \param[in] delta the number of seconds forward or backward to predict, relative to the current time.
+		 *
+		 * \param[in] deriv the derivative level to take (\c 0 for position, \c 1 for velocity, \c 2 for acceleration, etc.).
+		 *
+		 * \return the value.
+		 */
+		double value(double delta = 0.0, unsigned int deriv = 0) const __attribute__((warn_unused_result));
+
+		/**
+		 * Gets the predicted value at some fixed point in time.
+		 *
+		 * \param[in] ts the time at which to predict the value.
+		 *
+		 * \param[in] deriv the derivative level to take (\c 0 for position, \c 1 for velocity, \c 2 for acceleration, etc.).
+		 *
+		 * \return the value.
+		 */
+		double value(const timespec &ts, unsigned int deriv = 0) const __attribute__((warn_unused_result));
+
+		/**
+		 * Locks in a timestamp to consider as the current time.
+		 *
+		 * \param[in] ts the timestamp.
+		 */
+		void lock_time(const timespec &ts);
+
+		/**
+		 * Pushes a new sample into the prediction engine.
+		 *
+		 * \param[in] value the value to add.
+		 *
+		 * \param[in] ts the timestamp at which the value was sampled.
+		 */
+		void add_datum(double value, const timespec &ts);
+
+		/**
+		 * Clears the accumulated history of the predictor.
+		 * This means that on the next addition of a new datum, the predictor will estimate zero for all derivatives.
+		 * Until the addition of a new datum, the predictor will not change its output.
+		 * This is useful when coordinate transformation changes lead to large step changes in value, such as when switching field ends.
+		 */
+		void clear();
+
+	private:
+		const bool angle;
+		bool initialized;
+		ap::real_1d_array vhistory;
+		ap::real_1d_array dhistory; // history vector of time steps
+		ap::real_1d_array weights;
+		ap::real_2d_array fmatrix;
+		ap::real_1d_array approxv;
+		timespec last_datum_timestamp, lock_timestamp;
+		double lock_delta;
+
+		void update();
+};
+
+#endif
+
