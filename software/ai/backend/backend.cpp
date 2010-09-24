e43038ec80bac24c18821843824aa15902518eb9
chead <chead@localhost>
2010-09-24 06:12:21 +0000

    Handle null StrategyFactory in setter.

--- software/ai/backend/backend.cpp
+++ software/ai/backend/backend.cpp
@@ -4,7 +4,11 @@ using AI::BE::Backend;
 using AI::BE::BackendFactory;
 
 void Backend::strategy(AI::HL::StrategyFactory *s) {
-	strategy_ = s->create_strategy(*this);
+	if (s) {
+		strategy_ = s->create_strategy(*this);
+	} else {
+		strategy_ = AI::HL::Strategy::Ptr();
+	}
 }
 
 Backend::Backend() : defending_end_(WEST), friendly_colour_(YELLOW), playtype_(AI::Common::PlayType::HALT), playtype_override_(AI::Common::PlayType::COUNT), ball_filter_(0), strategy_(AI::HL::Strategy::Ptr()) {
