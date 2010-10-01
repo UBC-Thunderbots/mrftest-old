d29c058327737776fc9d0d6ede6c23c73462ec7b
bchan <bchan@localhost>
2010-10-01 01:53:13 +0000

    Added base file for helper class penaltyEnemy

---
+++ software/ai/hl/penalty_enemy.cpp
@@ -0,0 +1,39 @@
+#include "ai/hl/penalty_enemy.h"
+#include "ai/hl/strategy.h"
+//#include "ai/tactic/move.h"
+//#include "ai/tactic/patrol.h"
+#include <iostream>
+
+using AI::HL::PenaltyEnemy;
+using namespace AI::HL::W;
+
+
+
+const double PenaltyEnemy::PENALTY_MARK_LENGTH = 0.45;
+
+const double PenaltyEnemy::RESTRICTED_ZONE_LENGTH = 0.85;
+
+/*The constructor*/
+PenaltyEnemy::PenaltyEnemy(World &w) : world(w) {
+	const Field &f = (world.field());
+
+	/*Set robot positions*/
+	ready_positions[0] = Point(-0.5 * f.length() + RESTRICTED_ZONE_LENGTH + Robot::MAX_RADIUS, 5 * Robot::MAX_RADIUS);
+	ready_positions[1] = Point(-0.5 * f.length() + RESTRICTED_ZONE_LENGTH + Robot::MAX_RADIUS, 2 * Robot::MAX_RADIUS);
+	ready_positions[2] = Point(-0.5 * f.length() + RESTRICTED_ZONE_LENGTH + Robot::MAX_RADIUS, -2 * Robot::MAX_RADIUS);
+	ready_positions[3] = Point(-0.5 * f.length() + RESTRICTED_ZONE_LENGTH + Robot::MAX_RADIUS, -5 * Robot::MAX_RADIUS);
+}
+
+
+void PenaltyEnemy::tick() {
+/*
+	unsigned int flags = ai_flags::calc_flags(the_world->playtype());
+	for (unsigned int i = 0; i < the_robots.size(); ++i) {
+		move tactic(the_robots[i], the_world);
+		tactic.set_position(standing_positions[i]);
+		tactic.set_flags(flags);
+		tactic.tick();
+*/
+}
+
+
---
+++ software/ai/hl/penalty_enemy.h
@@ -0,0 +1,58 @@
+#ifndef AI_HL_PENALTY_ENEMY_H
+#define AI_HL_PENALTY_ENEMY_H
+
+#include "ai/hl/world.h"
+#include "ai/hl/strategy.h"
+#include <vector>
+
+namespace AI {
+	namespace HL {
+		/**
+		 * Gets the robots to go to their penalty_enemy positions.
+		 */
+		class PenaltyEnemy {
+			public:
+				/**
+				 * A pointer to a penalty_enemy role.
+				 */
+				typedef RefPtr<PenaltyEnemy> ptr;
+
+				/**
+				 * Constructs a new penalty_enemy role.
+				 */
+				PenaltyEnemy(W::World &w);
+
+				/**
+				* Runs the AI for one time tick.
+				*/
+				void tick();
+
+
+			protected:
+				W::World &world;
+
+				/**
+				 * The distance between the penalty mark and the mid point of the two goal posts as described in the rules.
+				 */
+				const static double PENALTY_MARK_LENGTH;
+
+				/**
+				 * The distance between the baseline and the line behind which other robots may stand.
+				 */
+				const static double RESTRICTED_ZONE_LENGTH;
+
+				/**
+				 * Maximum number of robots that can be assigned to this role.
+				 */
+				const static unsigned int NUMBER_OF_READY_POSITIONS = 4;
+
+				/**
+				 * The positions that the robots should move to for this role.
+				 */
+				Point ready_positions[NUMBER_OF_READY_POSITIONS];
+		};
+	}
+}
+
+#endif
+
