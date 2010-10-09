92c22a13be8b04f38dce17cea5cf7540e1a4bf12
chead <chead@localhost>
2010-10-09 07:13:08 +0000

    Add a utility function to add two timespecs.

--- software/util/time.h
+++ software/util/time.h
@@ -27,6 +27,24 @@ namespace {
 	}
 
 	/**
+	 * Adds a pair of timespecs.
+	 *
+	 * \param[in] ts1 the first timespec.
+	 *
+	 * \param[in] ts2 the second timespec.
+	 *
+	 * \param[out] result a location at which to store the value of \p ts1 + \p ts2.
+	 */
+	void timespec_add(const timespec &ts1, const timespec &ts2, timespec &result) {
+		result.tv_sec = ts1.tv_sec + ts2.tv_sec;
+		result.tv_nsec = ts1.tv_nsec + ts2.tv_nsec;
+		if (result.tv_nsec >= 1000000000L) {
+			++result.tv_sec;
+			result.tv_nsec -= 1000000000L;
+		}
+	}
+
+	/**
 	 * Subtracts a pair of timespecs.
 	 *
 	 * \param[in] ts1 the first timespec.
