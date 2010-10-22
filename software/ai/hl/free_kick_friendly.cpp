00bcb9bbd5d72582849b9aabcb93fdad4d33582c
tere11 <tere11@localhost>
2010-10-22 01:45:24 +0000

    added for real free kick helpers
    -Terence

---
+++ software/ai/hl/free_kick_friendly.cpp
@@ -0,0 +1,63 @@
+#include "ai/hl/free_kick_friendly.h"
+#include "ai/hl/tactics.h"
+#include "ai/hl/util.h"
+#include "geom/util.h"
+#include "uicomponents/param.h"
+#include "util/algorithm.h"
+#include "util/dprint.h"
+
+#include <vector>
+
+using AI::HL::DirectFreeKickFriendly;
+using AI::HL::IndirectFreeKickFriendly;
+using namespace AI::HL::W;
+
+DirectFreeKickFriendly::DirectFreeKickFriendly(World &w) : world(w) {
+}
+
+void DirectFreeKickFriendly::tick() {
+#warning Under Construction
+	//const FriendlyTeam &friendly = world.friendly_team();
+	std::vector<Player::Ptr> friendly = AI::HL::Util::get_players(world.friendly_team());
+	
+	if (!kicker.is()) {
+		LOG_ERROR("no goalie");
+		return;
+	}
+
+	std::vector<Player::Ptr> friends;
+	for (size_t i = 0; i < friendly.size(); ++i)
+		if (friendly[i] != kicker)
+			friends.push_back(friendly[i]);
+
+	std::pair<Point, double> bestshot = AI::HL::Util::calc_best_shot(world, kicker);
+
+	AI::HL::Tactics::shoot(world, kicker, AI::Flags::FLAG_CLIP_PLAY_AREA);
+
+}
+
+IndirectFreeKickFriendly::IndirectFreeKickFriendly(World &w) : world(w) {	
+}
+
+void IndirectFreeKickFriendly::tick() {
+
+#warning Under Construction
+	//const FriendlyTeam &friendly = world.friendly_team();
+	std::vector<Player::Ptr> friendly = AI::HL::Util::get_players(world.friendly_team());
+
+	if (!kicker.is()) {
+		LOG_ERROR("no goalie");
+		return;
+	}
+
+	std::vector<Player::Ptr> friends;
+	for (size_t i = 0; i < friendly.size(); ++i)
+		if (friendly[i] != kicker)
+			friends.push_back(friendly[i]);
+
+	// find someone to pass to
+	
+	LOG_INFO("forced kicking");
+	AI::HL::Tactics::shoot(world, kicker, AI::Flags::FLAG_CLIP_PLAY_AREA);
+	
+}
---
+++ software/ai/hl/free_kick_friendly.h
@@ -0,0 +1,84 @@
+#ifndef AI_HL_FREE_KICK_FRIENDLY_H
+#define AI_HL_FREE_KICK_FRIENDLY_H
+
+#include "ai/hl/world.h"
+#include <vector>
+
+namespace AI {
+	namespace HL {
+		/**
+		 * Gets the robots to go to their penalty_enemy positions.
+		 */
+		class DirectFreeKickFriendly {
+			public:
+				/**
+				 * A pointer to a free_kick_friendly role.
+				 */
+				typedef RefPtr<DirectFreeKickFriendly> ptr;
+
+				/**
+				 * Constructs a new free_kick_friendly role.
+				 *
+				 * \param[in] w the world.
+				 */
+				DirectFreeKickFriendly(W::World &w);
+
+				/**
+				 * Sets the kicker
+				 *
+				 * \param[in] k kicker.
+				 */
+				void set_players(W::Player::Ptr &k) {
+					kicker = k;
+				}
+
+				/**
+				 * Runs the AI for one time tick.
+				 */
+				void tick();
+
+			protected:
+				W::World &world;
+
+				W::Player::Ptr kicker;
+			
+		};
+
+		class IndirectFreeKickFriendly {
+						public:
+				/**
+				 * A pointer to a free_kick_friendly role.
+				 */
+				typedef RefPtr<IndirectFreeKickFriendly> ptr;
+
+				/**
+				 * Constructs a new free_kick_friendly role.
+				 *
+				 * \param[in] w the world.
+				 */
+				IndirectFreeKickFriendly(W::World &w);
+
+				/**
+				 * Sets the kicker
+				 *
+				 * \param[in] k kicker.
+				 */
+				void set_players(W::Player::Ptr &k) {
+					kicker = k;
+				}
+
+				/**
+				 * Runs the AI for one time tick.
+				 */
+				void tick();
+
+			protected:
+				W::World &world;
+
+				W::Player::Ptr kicker;
+			
+		};
+	}
+}
+
+#endif
