3388c3714d3d253d637261a9945b90256e929229
tere11 <tere11@localhost>
2010-10-27 08:23:07 +0000

    added more implementation for free kick enemy strategy
    -Terence

--- software/ai/hl/free_kick_enemy_strategy.cpp
+++ software/ai/hl/free_kick_enemy_strategy.cpp
@@ -15,7 +15,7 @@ using namespace AI::HL::W;
 namespace {
 	
 	/**
-	 * Manages the robots during a stoppage in place (that is, when the game is in PlayType::STOP).
+	 * Manages the robots during direct and indirect free kicks.
 	 */
 	class FreeKickEnemyStrategy : public Strategy {
 		public:
@@ -31,11 +31,19 @@ namespace {
 		private:
 			FreeKickEnemyStrategy(World &world);
 			~FreeKickEnemyStrategy();
+			void on_player_added(std::size_t);
+			void on_player_removing(std::size_t);
 
 			void execute_indirect_free_kick_enemy();
 			void execute_direct_free_kick_enemy();
 
 			void prepare();
+
+			void run_assignment();
+
+			// the players invovled
+			std::vector<Player::Ptr> defenders;
+			std::vector<Player::Ptr> offenders;
 
 			AI::HL::Defender defender;
 			AI::HL::Offender offender;
@@ -73,16 +81,16 @@ namespace {
 		prepare();
 	}
 
-
-// a goal may not be scored directly from the kick
 	void FreeKickEnemyStrategy::execute_indirect_free_kick_enemy() {
 		prepare();
 	}
 
-
+	/**
+	 * Ticks the strategy
+         *
+	 * Don't need to assign a kicker as in friendly. It is also "ok" for players to ram the enemy as long as they block the ball.
+	 */
 	void FreeKickEnemyStrategy::prepare() {
-
-		// should have one kicker and the rest as defenders or offenders for friendly freekick
 
 		if (world.friendly_team().size() == 0) {
 			return;
@@ -93,43 +101,62 @@ namespace {
 			return;
 		}
 
-		// run assignment and tick
-		if (world.playtype() == PlayType::EXECUTE_DIRECT_FREE_KICK_FRIENDLY) {
-
-			
-
-
-		} else if (world.playtype() == PlayType::EXECUTE_INDIRECT_FREE_KICK_FRIENDLY) {
-			Player::Ptr kicker = players[players.size()-1];
-			players.pop_back();
-
-			// 500 mm from the ball
-		} else if (world.playtype() == PlayType::EXECUTE_INDIRECT_FREE_KICK_ENEMY || world.playtype() == PlayType::EXECUTE_DIRECT_FREE_KICK_ENEMY) {
-			std::size_t preferred_offender_number = std::max(1, static_cast<int>(world.friendly_team().size()) - 3);
-
-			// decrease the number of offenders by 1, if there are no offenders, set it to one
-			preferred_offender_number--;
-			if (preferred_offender_number < 1) {
-				preferred_offender_number = 1;
-			}
-
-			// preferred_defender_number includes goalie
-			std::size_t preferred_defender_number = world.friendly_team().size() - preferred_offender_number;
-
-			std::vector<Player::Ptr> defenders; // excludes goalie
-			std::vector<Player::Ptr> offenders;
-			// start from 1, to exclude goalie
-			for (std::size_t i = 1; i < players.size(); ++i) {
-				if (i < preferred_defender_number) {
-					defenders.push_back(players[i]);
-				} else {
-					offenders.push_back(players[i]);
-				}
-			}
+		defender.tick();  // might want to do something with flags in order to "ram" enemy
+		offender.tick();
+		
+		if (world.playtype() == PlayType::EXECUTE_INDIRECT_FREE_KICK_ENEMY || world.playtype() == PlayType::EXECUTE_DIRECT_FREE_KICK_ENEMY) {
+			// TODO: something sensible
 
 		} else {
-			LOG_ERROR("freeKick: unhandled playtype");
+			LOG_ERROR("freeKickEnemy: unhandled playtype");
 		}
+	}
+
+	void FreeKickEnemyStrategy::run_assignment() {
+		if (world.friendly_team().size() == 0) {
+			LOG_WARN("no players");
+			return;
+		}
+
+		// it is easier to change players every tick?
+		std::vector<Player::Ptr> players = AI::HL::Util::get_players(world.friendly_team());
+
+		// sort players by distance to own goal
+		if (players.size() > 1) {
+			std::sort(players.begin() + 1, players.end(), AI::HL::Util::CmpDist<Player::Ptr>(world.field().friendly_goal()));
+		}
+
+		// defenders
+		Player::Ptr goalie = players[0];
+
+		std::size_t ndefenders = 1; // includes goalie
+
+		switch (players.size()) {
+			case 5:
+			case 4:
+				++ndefenders;
+			case 3:
+			case 2:
+				break;
+		}
+
+		// clear up
+		defenders.clear();
+		offenders.clear();
+
+		// start from 1, to exclude goalie
+		for (std::size_t i = 1; i < players.size(); ++i) {
+			if (i < ndefenders) {
+				defenders.push_back(players[i]);
+			} else {
+				offenders.push_back(players[i]);
+			}
+		}
+
+		LOG_INFO(Glib::ustring::compose("player reassignment %1 defenders, %2 offenders", ndefenders, offenders.size()));
+
+		defender.set_players(defenders, goalie);
+		offender.set_players(offenders);
 	}
 
 	Strategy::Ptr FreeKickEnemyStrategy::create(World &world) {
@@ -137,7 +164,19 @@ namespace {
 		return p;
 	}
 
-	FreeKickEnemyStrategy::FreeKickEnemyStrategy(World &world) : Strategy(world), defender(world), offender(world) {}
+	FreeKickEnemyStrategy::FreeKickEnemyStrategy(World &world) : Strategy(world), defender(world), offender(world) {
+		world.friendly_team().signal_robot_added().connect(sigc::mem_fun(this, &FreeKickEnemyStrategy::on_player_added));
+		world.friendly_team().signal_robot_removing().connect(sigc::mem_fun(this, &FreeKickEnemyStrategy::on_player_removing));
+		run_assignment();
+	}
+
+	void FreeKickEnemyStrategy::on_player_added(std::size_t) {
+		run_assignment();
+	}
+
+	void FreeKickEnemyStrategy::on_player_removing(std::size_t) {
+		run_assignment();
+	}
 
 	FreeKickEnemyStrategy::~FreeKickEnemyStrategy() {
 	}
