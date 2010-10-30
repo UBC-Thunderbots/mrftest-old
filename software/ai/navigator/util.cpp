751e1318886788c59a98eed9240eaeb88c88d755
JasonChang <jasonchang@localhost>
2010-10-30 21:10:12 +0000

    added a navigator util class

---
+++ software/ai/navigator/util.cpp
@@ -0,0 +1,10 @@
+#include "ai/navigator/util.h"
+
+bool AI::Nav::Util::valid_dst(Point dst, AI::Nav::W::World &world, AI::Nav::W::Player::Ptr player){
+	return true;
+}
+
+bool AI::Nav::Util::valid_path(Point cur, Point dst, AI::Nav::W::World &world, AI::Nav::W::Player::Ptr player){
+	return true;
+}
+		
---
+++ software/ai/navigator/util.h
@@ -0,0 +1,31 @@
+#ifndef AI_NAVIGATOR_UTIL_H
+#define AI_NAVIGATOR_UTIL_H
+
+#include <vector>
+#include "geom/util.h"
+#include "ai/flags.h"
+#include "geom/point.h"
+#include "ai/navigator/world.h"
+
+namespace AI {
+	namespace Nav {	
+		namespace Util{
+		
+		/**
+		*
+		*/
+		bool valid_dst(Point dst, AI::Nav::W::World &world, AI::Nav::W::Player::Ptr player);
+		
+		/**
+		*
+		*/ 
+		bool valid_path(Point cur, Point dst, AI::Nav::W::World &world, AI::Nav::W::Player::Ptr player);
+		
+		
+		
+		}
+		
+	}
+}
+
+#endif
