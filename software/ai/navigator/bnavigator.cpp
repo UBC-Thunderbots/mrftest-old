d2bfb7db6d7665c1d9b37288880907d69a55de62
chead <chead@localhost>
2010-11-06 19:33:57 +0000

    Uncrustify.

--- software/ai/navigator/bnavigator.cpp
+++ software/ai/navigator/bnavigator.cpp
@@ -1,12 +1,12 @@
 #include "ai/hl/util.h"
 #include "ai/navigator/navigator.h"
-#include "util/time.h"
-#include <iostream>
-#include "math.h"
-#include <algorithm>
 #include "util/dprint.h"
+#include "util/time.h"
+#include <algorithm>
+#include <cmath>
 #include <glibmm.h>
-		
+#include <iostream>
+
 using AI::Nav::Navigator;
 using AI::Nav::NavigatorFactory;
 using namespace AI::Nav::W;
@@ -74,7 +74,7 @@ namespace {
 		timespec ts;
 		unsigned int flags;
 
-		for( unsigned int i=0; i<fteam.size(); i++ ) {
+		for (unsigned int i = 0; i < fteam.size(); i++) {
 			player = fteam.get(i);
 			currp = player->position();
 			destp = player->destination().first;
@@ -85,23 +85,33 @@ namespace {
 
 			nexto = normalisedAngle(currp, nextp);
 
-			if((destp-nextp).len()>(destp-currp).len()) {
+			if ((destp - nextp).len() > (destp - currp).len()) {
 				nextp = destp;
 				nexto = desto;
 			}
 
-			if((flags & FLAG_CLIP_PLAY_AREA) == FLAG_CLIP_PLAY_AREA) {
-				if(nextp.x > field.length()/2) nextp.x = field.length()/2;
-				if(nextp.x < -field.length()/2) nextp.x = -field.length()/2;
-				if(nextp.y > field.width()/2) nextp.y = field.width()/2;
-				if(nextp.y < -field.width()/2) nextp.y = -field.width()/2;
+			if ((flags & FLAG_CLIP_PLAY_AREA) == FLAG_CLIP_PLAY_AREA) {
+				if (nextp.x > field.length() / 2) {
+					nextp.x = field.length() / 2;
+				}
+				if (nextp.x < -field.length() / 2) {
+					nextp.x = -field.length() / 2;
+				}
+				if (nextp.y > field.width() / 2) {
+					nextp.y = field.width() / 2;
+				}
+				if (nextp.y < -field.width() / 2) {
+					nextp.y = -field.width() / 2;
+				}
 			}
 
-			if((flags & FLAG_STAY_OWN_HALF) == FLAG_STAY_OWN_HALF) {
-				if(nextp.y > 0) nextp.y = 0;
+			if ((flags & FLAG_STAY_OWN_HALF) == FLAG_STAY_OWN_HALF) {
+				if (nextp.y > 0) {
+					nextp.y = 0;
+				}
 			}
 
-			if((flags & FLAG_AVOID_FRIENDLY_DEFENSE) == FLAG_AVOID_FRIENDLY_DEFENSE) {
+			if ((flags & FLAG_AVOID_FRIENDLY_DEFENSE) == FLAG_AVOID_FRIENDLY_DEFENSE) {
 				const double defense_stretch = world.field().defense_area_stretch();
 				const double defense_radius = world.field().defense_area_radius();
 				const double field_length = world.field().length();
@@ -109,25 +119,24 @@ namespace {
 				const Point pole2 = Point(-field_length, -defense_stretch / 2 - defense_radius);
 				double dist1 = (nextp - pole1).len();
 				double dist2 = (nextp - pole2).len();
-				
+
 				if (nextp.x > -field_length / 2 && nextp.x < -field_length / 2 + defense_radius && nextp.y > -defense_stretch / 2 && nextp.y < defense_stretch / 2) {
 					nextp.x = -field_length / 2 + defense_radius;
-					if(abs(-defense_stretch/2-nextp.y)>abs(defense_stretch/2-nextp.y)) {
-						nextp.y = -defense_stretch/2;
+					if (std::abs(-defense_stretch / 2 - nextp.y) > std::abs(defense_stretch / 2 - nextp.y)) {
+						nextp.y = -defense_stretch / 2;
 					} else {
-						nextp.y = defense_stretch/2;
+						nextp.y = defense_stretch / 2;
 					}
 				}
-				
+
 				if (dist1 < defense_radius || dist2 < defense_radius) {
-				
 				}
-		}
+			}
 
 
 
 			ts = world.monotonic_time();
-			ts.tv_sec += (nextp-currp).len() / player->avelocity();
+			ts.tv_sec += (nextp - currp).len() / player->avelocity();
 
 			path.push_back(std::make_pair(std::make_pair(nextp, nexto), ts));
 			player->path(path);
@@ -136,21 +145,23 @@ namespace {
 	}
 
 	double BNavigator::normalisedAngle(Point x, Point y) {
-		double angle = (M_PI/2) - atan2(y.x-x.x, y.y-x.y);
-		if( angle > M_PI ) angle -= 2*M_PI;
-		if( angle < -M_PI ) angle += 2*M_PI;
+		double angle = (M_PI / 2) - std::atan2(y.x - x.x, y.y - x.y);
+		if (angle > M_PI) {
+			angle -= 2 * M_PI;
+		}
+		if (angle < -M_PI) {
+			angle += 2 * M_PI;
+		}
 		return angle;
 	}
 
 	Point BNavigator::calcArrival(Point x, Point y) {
-		Point offset = y-x;
+		Point offset = y - x;
 		double distance = offset.len();
-		double ramped_speed = MAX_SPEED*(distance/SLOW_DIST);
+		double ramped_speed = MAX_SPEED * (distance / SLOW_DIST);
 		double clipped_speed = std::min(ramped_speed, MAX_SPEED);
-		Point desired = (clipped_speed/distance)*offset;
+		Point desired = (clipped_speed / distance) * offset;
 		return desired;
 	}
-
-
 }
 
