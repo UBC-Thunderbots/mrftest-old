ddb404d7a64e26e4dfebe945d983253e2aec08d9
ebemunk <ebemunk@localhost>
2010-11-06 18:16:49 +0000

    fixed flag check logic, added support for FLAG_STAY_OWN_HALF, working on FLAG_AVOID_X_DEFENSE support

--- software/ai/navigator/bnavigator.cpp
+++ software/ai/navigator/bnavigator.cpp
@@ -6,7 +6,7 @@
 #include <algorithm>
 #include "util/dprint.h"
 #include <glibmm.h>
-
+		
 using AI::Nav::Navigator;
 using AI::Nav::NavigatorFactory;
 using namespace AI::Nav::W;
@@ -22,9 +22,8 @@ namespace {
 			static Navigator::Ptr create(World &world);
 			void tick();
 
-			double distance(Point x, Point y);
 			double normalisedAngle(Point x, Point y);
-			Point calcArrival(Point x, Point y, double avelocity);
+			Point calcArrival(Point x, Point y);
 
 		private:
 			BNavigator(World &world);
@@ -69,65 +68,66 @@ namespace {
 		const Field &field = world.field();
 		FriendlyTeam &fteam = world.friendly_team();
 		Player::Ptr player;
-		std::vector<std::pair<std::pair<Point, double>, timespec> > path;
+		std::vector<std::pair<std::pair<Point, double>, timespec>> path;
 		Point currp, destp, nextp;
 		double curro, desto, nexto;
 		timespec ts;
 		unsigned int flags;
 
-		for (unsigned int i = 0; i < fteam.size(); i++) {
+		for( unsigned int i=0; i<fteam.size(); i++ ) {
 			player = fteam.get(i);
 			currp = player->position();
 			destp = player->destination().first;
 			curro = player->orientation();
 			desto = player->destination().second;
 			flags = player->flags();
-			nextp = calcArrival(currp, destp, player->avelocity());
-
-			//LOG_INFO(Glib::ustring::compose("Player: %1 Flags: %2",i,flags));
+			nextp = calcArrival(currp, destp);
 
 			nexto = normalisedAngle(currp, nextp);
-			if (distance(nextp, destp) > distance(currp, destp)) {
+
+			if((destp-nextp).len()>(destp-currp).len()) {
 				nextp = destp;
 				nexto = desto;
 			}
 
-
-
-
-			if (flags & FLAG_CLIP_PLAY_AREA == FLAG_CLIP_PLAY_AREA) {
-				if (nextp.x > field.length() / 2) {
-					nextp.x = field.length() / 2;
-				}
-				if (nextp.x < -field.length() / 2) {
-					nextp.x = -field.length() / 2;
-				}
-				if (nextp.y > field.width() / 2) {
-					nextp.y = field.width() / 2;
-				}
-				if (nextp.y < -field.width() / 2) {
-					nextp.y = -field.width() / 2;
-				}
+			if((flags & FLAG_CLIP_PLAY_AREA) == FLAG_CLIP_PLAY_AREA) {
+				if(nextp.x > field.length()/2) nextp.x = field.length()/2;
+				if(nextp.x < -field.length()/2) nextp.x = -field.length()/2;
+				if(nextp.y > field.width()/2) nextp.y = field.width()/2;
+				if(nextp.y < -field.width()/2) nextp.y = -field.width()/2;
 			}
 
-/*
-        const double defense_stretch = world.field().defense_area_stretch();
-    const double defense_radius = world.field().defense_area_radius();
-    const double field_length = world.field().length();
-    const Point pole1 = Point(-field_length, defense_stretch / 2 + defense_radius);
-    const Point pole2 = Point(-field_length, -defense_stretch / 2 - defense_radius);
-    double dist1 = (currp - pole1).len();
-    double dist2 = (currp - pole2).len();
-    if (nextp.x > -field_length / 2 && nextp.x < -field_length / 2 + defense_radius && nextp.y > -defense_stretch / 2 && nextp.y < defense_stretch / 2) {
-                    nextp.x = -;
-    }
-    if (dist1 < defense_radius || dist2 < defense_radius) {
-        continue;
-    }
- */
+			if((flags & FLAG_STAY_OWN_HALF) == FLAG_STAY_OWN_HALF) {
+				if(nextp.y > 0) nextp.y = 0;
+			}
+
+			if((flags & FLAG_AVOID_FRIENDLY_DEFENSE) == FLAG_AVOID_FRIENDLY_DEFENSE) {
+				const double defense_stretch = world.field().defense_area_stretch();
+				const double defense_radius = world.field().defense_area_radius();
+				const double field_length = world.field().length();
+				const Point pole1 = Point(-field_length, defense_stretch / 2 + defense_radius);
+				const Point pole2 = Point(-field_length, -defense_stretch / 2 - defense_radius);
+				double dist1 = (nextp - pole1).len();
+				double dist2 = (nextp - pole2).len();
+				
+				if (nextp.x > -field_length / 2 && nextp.x < -field_length / 2 + defense_radius && nextp.y > -defense_stretch / 2 && nextp.y < defense_stretch / 2) {
+					nextp.x = -field_length / 2 + defense_radius;
+					if(abs(-defense_stretch/2-nextp.y)>abs(defense_stretch/2-nextp.y)) {
+						nextp.y = -defense_stretch/2;
+					} else {
+						nextp.y = defense_stretch/2;
+					}
+				}
+				
+				if (dist1 < defense_radius || dist2 < defense_radius) {
+				
+				}
+		}
+
+
 
 			ts = world.monotonic_time();
-			ts.tv_sec += distance(currp, nextp) / player->avelocity();
+			ts.tv_sec += (nextp-currp).len() / player->avelocity();
 
 			path.push_back(std::make_pair(std::make_pair(nextp, nexto), ts));
 			player->path(path);
@@ -135,32 +135,22 @@ namespace {
 		}
 	}
 
-	double BNavigator::distance(Point x, Point y) {
-		return std::sqrt(std::pow((x.x - y.x), 2) + std::pow(x.y - y.y, 2));
-	}
-
 	double BNavigator::normalisedAngle(Point x, Point y) {
-		double angle = (M_PI / 2) - std::atan2(y.x - x.x, y.y - x.y);
-		if (angle > M_PI) {
-			angle -= 2 * M_PI;
-		}
-		if (angle < -M_PI) {
-			angle += 2 * M_PI;
-		}
+		double angle = (M_PI/2) - atan2(y.x-x.x, y.y-x.y);
+		if( angle > M_PI ) angle -= 2*M_PI;
+		if( angle < -M_PI ) angle += 2*M_PI;
 		return angle;
 	}
 
-	Point BNavigator::calcArrival(Point x, Point y, double avelocity) {
-		Point offset = y - x;
+	Point BNavigator::calcArrival(Point x, Point y) {
+		Point offset = y-x;
 		double distance = offset.len();
-		double ramped_speed = MAX_SPEED * (distance / SLOW_DIST);
+		double ramped_speed = MAX_SPEED*(distance/SLOW_DIST);
 		double clipped_speed = std::min(ramped_speed, MAX_SPEED);
-		Point desired = (clipped_speed / distance) * offset;
-		Point steering;
-		steering.x = desired.x - avelocity;
-		steering.y = desired.y - avelocity;
-		offset = x + steering;
-		return offset;
+		Point desired = (clipped_speed/distance)*offset;
+		return desired;
 	}
+
+
 }
 
