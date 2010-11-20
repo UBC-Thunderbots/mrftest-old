e57c1c1a7a38ea4e36cfc863b609c2f5e1a55b96
byronknoll <byronknoll@localhost>
2010-11-20 23:50:37 +0000

    Added lazy navigator (in progress).

---
+++ software/ai/navigator/lazy_navigator.cpp
@@ -0,0 +1,82 @@
+#include "ai/hl/util.h"
+#include "ai/navigator/navigator.h"
+#include "util/time.h"
+
+using AI::Nav::Navigator;
+using AI::Nav::NavigatorFactory;
+using namespace AI::Nav::W;
+
+namespace {
+	/**
+	 * Lazy Navigator
+	 * A work in progress.
+	 */
+	class LazyNavigator : public Navigator {
+		public:
+			NavigatorFactory &factory() const;
+			static Navigator::Ptr create(World &world);
+			void tick();
+
+		private:
+			LazyNavigator(World &world);
+			~LazyNavigator();
+	};
+
+	class LazyNavigatorFactory : public NavigatorFactory {
+		public:
+			Navigator::Ptr create_navigator(World &world) const;
+			LazyNavigatorFactory();
+			~LazyNavigatorFactory();
+	};
+
+	LazyNavigatorFactory simple_nav_factory;
+
+	NavigatorFactory &LazyNavigator::factory() const {
+		return simple_nav_factory;
+	}
+
+	Navigator::Ptr LazyNavigator::create(World &world) {
+		const Navigator::Ptr p(new LazyNavigator(world));
+		return p;
+	}
+
+	LazyNavigator::LazyNavigator(World &world) : Navigator(world) {
+	}
+
+	LazyNavigator::~LazyNavigator() {
+	}
+
+	LazyNavigatorFactory::LazyNavigatorFactory() : NavigatorFactory("Lazy Navigator") {
+	}
+
+	LazyNavigatorFactory::~LazyNavigatorFactory() {
+	}
+
+	Navigator::Ptr LazyNavigatorFactory::create_navigator(World &world) const {
+		return LazyNavigator::create(world);
+	}
+
+	void LazyNavigator::tick() {
+		const Field &field = world.field();
+		FriendlyTeam &fteam = world.friendly_team();
+
+		Player::Ptr player;
+		std::vector<std::pair<std::pair<Point, double>, timespec> > path;
+
+		Point currentPosition, destinationPosition;
+		double currentOrientation, destinationOrientation;
+
+		for (unsigned int i = 0; i < fteam.size(); i++) {
+			path.clear();
+			player = fteam.get(i);
+			currentPosition = player->position();
+			currentOrientation = player->orientation();
+			destinationPosition = player->destination().first;
+			destinationOrientation = player->destination().second;
+
+			path.push_back(std::make_pair(std::make_pair(destinationPosition, destinationOrientation), world.monotonic_time()));
+			player->path(path);
+		}
+	}
+}
+
