a2c08260c0c477114fe47e321984f3c5998a9f44
simonsyd <simonsyd@localhost>
2010-12-20 05:05:53 +0000

    Experimental module using cacheable.

---
+++ software/ai/hl/stp/module/shoot.cpp
@@ -0,0 +1,19 @@
+#include "ai/hl/stp/module/shoot.h"
+#include "ai/hl/util.h"
+
+using namespace AI::HL::STP;
+using AI::HL::STP::Module::ShootStats;
+using AI::HL::STP::Module::EvaluateShoot;
+
+ShootStats EvaluateShoot::compute(AI::HL::W::World& world, AI::HL::W::Player::Ptr player) const {
+	ShootStats stats;
+
+	std::pair<Point, double> shot = AI::HL::Util::calc_best_shot(world, player);
+
+	stats.target = shot.first;
+	stats.angle = shot.second;
+	stats.good = (shot.second > AI::HL::Util::shoot_accuracy);
+
+	return stats;
+}
+
---
+++ software/ai/hl/stp/module/shoot.h
@@ -0,0 +1,28 @@
+#ifndef AI_HL_STP_MODULE_SHOOT_H
+#define AI_HL_STP_MODULE_SHOOT_H
+
+#include "ai/hl/world.h"
+#include "util/cacheable.h"
+
+namespace AI {
+	namespace HL {
+		namespace STP {
+			namespace Module {
+
+				struct ShootStats {
+					Point target;
+					double angle;
+					bool good;
+				};
+
+				class EvaluateShoot : public Cacheable<ShootStats, CacheableNonKeyArgs<AI::HL::W::World&>, CacheableKeyArgs<AI::HL::W::Player::Ptr> > {
+					ShootStats compute(AI::HL::W::World& world, AI::HL::W::Player::Ptr player) const;
+				};
+
+			}
+		}
+	}
+}
+
+#endif
+
