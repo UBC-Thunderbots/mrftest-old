8c99dc608cac133136ed7b11296de69b4108b99b
chead <chead@localhost>
2011-01-22 00:45:02 +0000

    Fix bug with state transport endpoint max packet size.

--- firmware/dongle/descriptors.c
+++ firmware/dongle/descriptors.c
@@ -76,7 +76,7 @@ __code const uint8_t CONFIGURATION_DESCRIPTOR_TAIL[] = {
 		/* bDescriptorType */ USB_DESCRIPTOR_ENDPOINT,
 		/* bEndpointAddress */ 0x04,
 		/* bmAttributes */ 0x03,
-		/* wMaxPacketSize */ 16, 0,
+		/* wMaxPacketSize */ 64, 0,
 		/* bInterval */ 1,
 
 		/* Endpoint Descriptor */
