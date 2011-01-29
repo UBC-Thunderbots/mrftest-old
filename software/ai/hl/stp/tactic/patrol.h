3ccaf38b4ed8ff3083b8202e8d117303a015c4d7
tere11 <tere11@localhost>
2011-01-29 20:50:10 +0000

    patrol tactic for stp
    -Terence

--- software/ai/hl/stp/tactic/patrol.h
+++ software/ai/hl/stp/tactic/patrol.h
@@ -1,28 +1,38 @@
 #ifndef AI_HL_STP_TACTIC_PATROL_H
 #define AI_HL_STP_TACTIC_PATROL_H
 
+#include "ai/hl/world.h"
+#include "ai/hl/stp/tactic/tactic.h"
+
 namespace AI {
 	namespace HL {
 		namespace STP {
+			namespace Tactic {
 			/**
 			 * Used for going back and forth between 2 destinations.
 			 * A play must store this tactic in order to use this correctly.
 			 */
-			class Patrol : public Tactic {
-				public:
-					typedef RefPtr<Patrol> Ptr;
+				class Patrol : public Tactic {
+					public:
 
-					Patrol(AI::HL::W::World &world);
+						Patrol(AI::HL::W::World &world);
 
-					void set_waypoints(Point w1, Point w2);
+						void set_waypoints(Point w1, Point w2);
 
-					double score(AI::HL::W::Player::Ptr player);
+						double score(AI::HL::W::Player::Ptr player) const;
+						
+						void set_flags(const unsigned int f) {
+							flags = f;
+						}
 
-					void tick(AI::HL::W::Player::Ptr player);
+						void tick(AI::HL::W::Player::Ptr player);
 
-				protected:
-					Point p1, p2;
-			};
+					protected:
+						Point p1, p2;
+						bool goto_target1;
+						unsigned int flags;
+				};
+			}
 		}
 	}
 }
