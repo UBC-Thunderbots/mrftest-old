6415043a010f02f6bf99e3c8c1f03670037addfe
tere11 <tere11@localhost>
2011-02-19 21:23:11 +0000

    added stp free kick enemy
    -Terence

--- software/ai/hl/stp/play/free_kick_enemy.cpp
+++ software/ai/hl/stp/play/free_kick_enemy.cpp
@@ -3,6 +3,7 @@
 #include "ai/hl/stp/play/play.h"
 #include "ai/hl/stp/tactic/shoot.h"
 #include "ai/hl/stp/tactic/defend.h"
+#include "ai/hl/stp/tactic/offend.h"
 #include "ai/hl/stp/tactic/move_wait_playtype.h"
 #include "ai/hl/stp/tactic/move.h"
 #include "ai/hl/util.h"
@@ -15,8 +16,6 @@ using namespace AI::HL::W;
 namespace Predicates = AI::HL::STP::Predicates;
 
 namespace {
-	const double PENALTY_MARK_LENGTH = 0.45;
-	const double RESTRICTED_ZONE_LENGTH = 0.85;
 	/**
 	 * Condition:
 	 * - Playtype Prepare Penalty Enemy
@@ -37,7 +36,7 @@ namespace {
 			const PlayFactory &factory() const;
 	};
 
-	PlayFactoryImpl<PenaltyEnemy> factory_instance("Penalty Enemy");
+	PlayFactoryImpl<PenaltyEnemy> factory_instance("Free Kick Enemy");
 
 	const PlayFactory &PenaltyEnemy::factory() const {
 		return factory_instance;
@@ -50,11 +49,11 @@ namespace {
 	}
 
 	bool PenaltyEnemy::applicable() const {
-		return Predicates::playtype(world, PlayType::PREPARE_PENALTY_ENEMY) && Predicates::our_team_size_at_least(world, 1);
+		return (Predicates::playtype(world, PlayType::EXECUTE_DIRECT_FREE_KICK_ENEMY) || Predicates::playtype(world, PlayType::EXECUTE_INDIRECT_FREE_KICK_ENEMY)) && Predicates::our_team_size_at_least(world, 1);
 	}
 
 	bool PenaltyEnemy::done() const {
-		return !(Predicates::playtype(world, PlayType::EXECUTE_PENALTY_ENEMY) || Predicates::playtype(world, PlayType::PREPARE_PENALTY_ENEMY)) ;
+		return !(Predicates::playtype(world, PlayType::EXECUTE_DIRECT_FREE_KICK_ENEMY) || Predicates::playtype(world, PlayType::EXECUTE_INDIRECT_FREE_KICK_ENEMY)) ;
 	}
 
 	bool PenaltyEnemy::fail() const {
@@ -63,26 +62,24 @@ namespace {
 
 	void PenaltyEnemy::assign(std::vector<Tactic::Ptr> &goalie_role, std::vector<Tactic::Ptr>(&roles)[4]) {
 		// std::Player::Ptr goalie = world.Enemy_team().get(0);
-
 		// GOALIE
-		goalie_role.push_back(defend_solo_goalie(world));
+		goalie_role.push_back(defend_duo_goalie(world));
 
 		// ROLE 1
-		// move to penalty position 1
-		roles[0].push_back(move(world, Point(-0.5 * world.field().length() + RESTRICTED_ZONE_LENGTH + Robot::MAX_RADIUS, 5 * Robot::MAX_RADIUS)));
-		//roles[0].push_back(shoot(world));
+		// defend
+		roles[0].push_back(defend_duo_defender(world));
 		
 		// ROLE 2
-		// move to penalty position 2
-		roles[1].push_back(move(world, Point(-0.5 * world.field().length() + RESTRICTED_ZONE_LENGTH + Robot::MAX_RADIUS, 2 * Robot::MAX_RADIUS)));
-
+		// defend
+		roles[1].push_back(defend_duo_defender(world));
+		
 		// ROLE 3
-		// move to penalty position 3
-		roles[2].push_back(move(world, Point(-0.5 * world.field().length() + RESTRICTED_ZONE_LENGTH + Robot::MAX_RADIUS, -2 * Robot::MAX_RADIUS)));
-
+		// offend
+		roles[2].push_back(offend(world));
+		
 		// ROLE 4
-		// move to penalty position 4
-		roles[3].push_back(move(world, Point(-0.5 * world.field().length() + RESTRICTED_ZONE_LENGTH + Robot::MAX_RADIUS, -5 * Robot::MAX_RADIUS)));
+		// offend
+		roles[3].push_back(offend(world));
 	}
 }
 
