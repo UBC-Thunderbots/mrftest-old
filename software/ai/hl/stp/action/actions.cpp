7b9aec2145e225c3f164c6c8ea9aa016be854cf5
simonsyd <simonsyd@localhost>
2011-02-26 20:12:48 +0000

    actions for STP

---
+++ software/ai/hl/stp/action/actions.cpp
@@ -0,0 +1,152 @@
+#include "ai/flags.h"
+#include "ai/hl/stp/action/actions.h"
+#include "ai/hl/util.h"
+#include "geom/util.h"
+#include "uicomponents/param.h"
+#include "util/dprint.h"
+
+using namespace AI::HL::STP;
+
+namespace {
+	DoubleParam lone_goalie_dist("Lone goalie distance to goal post (m)", 0.30, 0.05, 1.0);
+}
+
+void AI::HL::STP::Actions::chase(const World &world, Player::Ptr player, const unsigned int flags) {
+	player->move(world.ball().position(), (world.ball().position() - player->position()).orientation(), flags, AI::Flags::MOVE_CATCH, AI::Flags::PRIO_HIGH);
+}
+
+void AI::HL::STP::Actions::shoot(const World &world, Player::Ptr player, const unsigned int flags, const bool force) {
+	std::pair<Point, double> target = AI::HL::Util::calc_best_shot(world, player);
+
+	if (!player->has_ball()) {
+		if (target.second == 0) {
+			chase(world, player, flags);
+		} else {
+			player->move(world.ball().position(), (world.field().enemy_goal() - player->position()).orientation(), flags, AI::Flags::MOVE_CATCH, AI::Flags::PRIO_HIGH);
+		}
+		return;
+	}
+
+	if (target.second == 0) { // blocked
+		if (force) {
+			// TODO: perhaps do a reduced radius calculation
+			target.first = world.field().enemy_goal();
+			shoot(world, player, flags, target.first);
+		} else { // just aim at the enemy goal
+			player->move(player->position(), (world.field().enemy_goal() - player->position()).orientation(), flags, AI::Flags::MOVE_DRIBBLE, AI::Flags::PRIO_HIGH);
+		}
+	} else {
+		// call the other shoot function with the specified target
+		AI::HL::STP::Actions::shoot(world, player, flags, target.first);
+	}
+}
+
+void AI::HL::STP::Actions::shoot(const World &world, Player::Ptr player, const unsigned int flags, const Point target, const double kick_power) {
+	const double ori_target = (target - player->position()).orientation();
+
+	if (!player->has_ball()) {
+		// chase(world, player, flags);
+		player->move(world.ball().position(), ori_target, flags, AI::Flags::MOVE_CATCH, AI::Flags::PRIO_HIGH);
+		return;
+	}
+
+	// const double ori_diff = std::fabs(player->orientation() - ori_target);
+
+	// aim
+	player->move(player->position(), ori_target, flags, AI::Flags::MOVE_DRIBBLE, AI::Flags::PRIO_HIGH);
+
+	// ignoring accuracy, comment this out for now so that we'll shoot more
+	// if (ori_diff > AI::HL::STP::Util::shoot_accuracy * M_PI / 180.0) { // aim
+	// return;
+	// }
+
+	// shoot!
+	if (player->chicker_ready_time() == 0) {
+		player->kick(kick_power);
+	}
+}
+
+void AI::HL::STP::Actions::repel(const World &world, Player::Ptr player, const unsigned int flags) {
+	// set to RAM_BALL instead of using chase
+	if (!player->has_ball()) {
+		player->move(world.ball().position(), (world.ball().position() - player->position()).orientation(), flags, AI::Flags::MOVE_RAM_BALL, AI::Flags::PRIO_HIGH);
+		return;
+	}
+
+	// just shoot as long as it's not in backwards direction
+	if (player->orientation() < M_PI / 2 && player->orientation() > -M_PI / 2) {
+		if (player->chicker_ready_time() == 0) {
+			player->kick(1.0);
+		}
+	}
+
+	player->move(world.ball().position(), (world.ball().position() - player->position()).orientation(), flags, AI::Flags::MOVE_RAM_BALL, AI::Flags::PRIO_HIGH);
+
+	// all enemies are obstacles
+	/*
+	   std::vector<Point> obstacles;
+	   EnemyTeam &enemy = world.enemy_team();
+	   for (std::size_t i = 0; i < enemy.size(); ++i) {
+	   obstacles.push_back(enemy.get(i)->position());
+	   }
+
+	   const Field &f = world.field();
+
+	   // vertical line at the enemy goal area
+	   // basically u want the ball to be somewhere there
+	   const Point p1 = Point(f.length() / 2.0, -f.width() / 2.0);
+	   const Point p2 = Point(f.length() / 2.0, f.width() / 2.0);
+	   std::pair<Point, double> target = angle_sweep_circles(player->position(), p1, p2, obstacles, Robot::MAX_RADIUS);
+
+	   AI::HL::STP::Actions::shoot(world, player, flags, target.first);
+	 */
+}
+
+void AI::HL::STP::Actions::free_move(World &world, Player::Ptr player, const Point p) {
+	// no flags
+	player->move(p, (world.ball().position() - player->position()).orientation(), 0, AI::Flags::MOVE_NORMAL, AI::Flags::PRIO_LOW);
+}
+
+void AI::HL::STP::Actions::lone_goalie(const World &world, Player::Ptr player) {
+	// if ball is inside the defense area, must repel!
+	if (AI::HL::Util::point_in_friendly_defense(world.field(), world.ball().position())) {
+		repel(world, player, 0);
+		return;
+	}
+
+	const Point default_pos = Point(-0.45 * world.field().length(), 0);
+	const Point centre_of_goal = world.field().friendly_goal();
+	Point target = world.ball().position() - centre_of_goal;
+	target = target * (lone_goalie_dist / target.len());
+	target += centre_of_goal;
+	player->move(target, (world.ball().position() - player->position()).orientation(), 0, AI::Flags::MOVE_NORMAL, AI::Flags::PRIO_MEDIUM);
+}
+
+void AI::HL::STP::Actions::block(World &world, Player::Ptr player, const unsigned int flags, Robot::Ptr robot) {
+#warning can be better
+	player->move(robot->position(), (world.ball().position() - player->position()).orientation(), flags, AI::Flags::MOVE_NORMAL, AI::Flags::PRIO_MEDIUM);
+}
+
+void AI::HL::STP::Actions::pass(World &world, Player::Ptr passer, Player::Ptr passee, const unsigned int flags) {
+	if (!passee.is()) {
+		LOG_ERROR("There is no passee");
+		return;
+	}
+
+	// this has to be done first
+	if (!passer->has_ball()) {
+		chase(world, passer, flags);
+		return;
+	}
+
+	// TODO: if the target is blocked, aim towards the target
+	if (!AI::HL::Util::can_receive(world, passee)) {
+		const double ori_target = (passee->position() - passer->position()).orientation();
+		passer->move(passer->position(), ori_target, flags, AI::Flags::MOVE_DRIBBLE, AI::Flags::PRIO_HIGH);
+		return;
+	}
+
+	// don't pass too fast, but also not too slow
+	AI::HL::STP::Actions::shoot(world, passer, flags, passee->position(), 6.0);
+}
+
---
+++ software/ai/hl/stp/action/actions.h
@@ -0,0 +1,77 @@
+#ifndef AI_HL_STP_ACTION_ACTIONS_H
+#define AI_HL_STP_ACTION_ACTIONS_H
+
+#include "ai/hl/stp/world.h"
+
+namespace AI {
+	namespace HL {
+		namespace STP {
+			namespace Actions {
+				/**
+				 * Chases after the ball as fast as possible.
+				 */
+				void chase(const World &world, Player::Ptr player, const unsigned int flags);
+
+				/**
+				 * If the player posses the ball,
+				 * aims at an open angle at the enemy goal,
+				 * and shoots the ball.
+				 *
+				 * If the player does not have the ball, chases after it.
+				 *
+				 * \param[in] force forces the player to shoot,
+				 * even if the goal is completely blocked.
+				 */
+				void shoot(const World &world, Player::Ptr player, const unsigned int flags, const bool force = false);
+
+				/**
+				 * If the player posses the ball,
+				 * aims at the target and shoots the ball.
+				 *
+				 * If the player does not have the ball, chases after it.
+				 *
+				 * \param[in] target the location to shoot the ball to.
+				 */
+				void shoot(const World &world, Player::Ptr player, const unsigned int flags, const Point target, const double kick_power = 10.0);
+
+				/**
+				 * I can't think of a good name for this function.
+				 *
+				 * Anyways, tries to move the ball as far away from friendly goal.
+				 * If ball is not in possesion, chase after the ball.
+				 * Otherwise, shoot the ball in the furthest possible direction.
+				 * Useful for defenders.
+				 *
+				 * \param[in] flags movement flags for the robot (most likely you want to disable AI::Flags::FLAG_AVOID_FRIENDLY_DEFENSE).
+				 */
+				void repel(const World &world, Player::Ptr player, const unsigned int flags);
+
+				/**
+				 * Convenient function.
+				 * Move the players when NOT in any ACTIVE play.
+				 * For example, during stop, preparing for kickoff, preparing for free kick.
+				 */
+				void free_move(World &world, Player::Ptr player, const Point p);
+
+				/**
+				 * A single goalie and NO ONE ELSE defending the field.
+				 */
+				void lone_goalie(const World &world, Player::Ptr player);
+
+				/**
+				 * Blocks against a single enemy from shooting.
+				 */
+				void block(World &world, Player::Ptr player, const unsigned int flags, Robot::Ptr robot);
+
+				/**
+				 * Passer shoots ball to passee.
+				 * Checks if passer has ball and passee can receive ball.
+				 */
+				void pass(World &world, Player::Ptr passer, Player::Ptr passee, const unsigned int flags);
+			}
+		}
+	}
+}
+
+#endif
+
