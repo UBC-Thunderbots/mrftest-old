eb0a8359a91de79dcec5b1d97dd7b9c3f4a322c7
simonsyd <simonsyd@localhost>
2011-02-27 10:03:36 +0000

    shorten code

--- software/ai/hl/stp/predicates.h
+++ software/ai/hl/stp/predicates.h
@@ -1,7 +1,7 @@
 #ifndef AI_HL_STP_PREDICATES
 #define AI_HL_STP_PREDICATES
 
-#include "ai/hl/world.h"
+#include "ai/hl/stp/world.h"
 #include <functional>
 
 namespace AI {
@@ -13,35 +13,35 @@ namespace AI {
 			 * A play is more READABLE if its conditions are written as functions.
 			 */
 			namespace Predicates {
-				bool goal(const AI::HL::W::World &);
+				bool goal(const World &);
 
-				bool playtype(const AI::HL::W::World &world, const AI::HL::W::PlayType::PlayType playtype);
+				bool playtype(const World &world, const PlayType::PlayType playtype);
 
-				bool our_ball(const AI::HL::W::World &world);
+				bool our_ball(const World &world);
 
-				bool their_ball(const AI::HL::W::World &world);
+				bool their_ball(const World &world);
 
-				bool none_ball(const AI::HL::W::World &world);
+				bool none_ball(const World &world);
 
-				bool our_team_size_at_least(const AI::HL::W::World &world, const unsigned int n);
+				bool our_team_size_at_least(const World &world, const unsigned int n);
 
-				bool their_team_size_at_least(const AI::HL::W::World &world, const unsigned int n);
+				bool their_team_size_at_least(const World &world, const unsigned int n);
 
-				bool their_team_size_at_most(const AI::HL::W::World &world, const unsigned int n);
+				bool their_team_size_at_most(const World &world, const unsigned int n);
 
-				bool ball_x_less_than(const AI::HL::W::World &world, const double x);
+				bool ball_x_less_than(const World &world, const double x);
 
-				bool ball_x_greater_than(const AI::HL::W::World &world, const double x);
+				bool ball_x_greater_than(const World &world, const double x);
 
-				bool ball_on_our_side(const AI::HL::W::World &world);
+				bool ball_on_our_side(const World &world);
 
-				bool ball_on_their_side(const AI::HL::W::World &world);
+				bool ball_on_their_side(const World &world);
 				
-				bool ball_in_our_corner(const AI::HL::W::World &world);
+				bool ball_in_our_corner(const World &world);
 				
-				bool ball_in_their_corner(const AI::HL::W::World &world);
+				bool ball_in_their_corner(const World &world);
 				
-				bool ball_midfield(const AI::HL::W::World &world);
+				bool ball_midfield(const World &world);
 			}
 		}
 	}
