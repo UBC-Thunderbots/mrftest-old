07e9921889974652c86c7815a1de8d86ce9f6a72
tere11 <tere11@localhost>
2011-03-19 23:53:23 +0000

    completed enemy evaluation (needs to do more error checking) -Terence

--- software/ai/hl/stp/evaluation/enemy.cpp
+++ software/ai/hl/stp/evaluation/enemy.cpp
@@ -12,10 +12,23 @@ using AI::HL::STP::Evaluation::EnemyThreat;
 
 namespace {
 
-	//bool enemy_blocked(const World &world, const Robot::Ptr enemy){}
+	std::pair<Point, double> calc_enemy_best_shot_target(const World &world, const Point &target_pos, const Robot::Ptr enemy, const double radius = 1.0) {
+		std::vector<Point> obstacles;
+		const FriendlyTeam &friendly = world.friendly_team();
+		for (std::size_t i = 0; i < friendly.size(); ++i) {
+			obstacles.push_back(friendly.get(i)->position());
+		}
+		const EnemyTeam &enemies = world.enemy_team();
+		for (std::size_t i = 0; i < enemies.size(); ++i) {
+			const Robot::Ptr erob = enemies.get(i);
+			if (erob == enemy) {
+				continue;
+			}
+			obstacles.push_back(erob->position());
+		}
+		return AI::HL::Util::calc_best_shot_target(target_pos, obstacles, enemy->position(), radius);
+	}
 	
-
-	#warning TODO: maybe the source to a point instead of defaulting to ball.
 	bool enemy_can_receive(const World &world, const Robot::Ptr enemy) {
 		const Ball &ball = world.ball();
 		if ((ball.position() - enemy->position()).lensq() < POS_CLOSE) {
@@ -61,23 +74,39 @@ namespace {
 		return true;
 	}
 
-	
+	bool available_enemy_pass(const World &world, const Robot::Ptr passer, const Robot::Ptr passee) {
+		// assuming a good enemy team
+		double minangle = 5;
+		double maxdist = world.field().length()/2;
+		
+		if (!enemy_can_receive(world, passee)) {
+			return false;
+		}
+		
+		const double dist = (passee->position() - world.ball().position()).len();
+		const double angle = calc_enemy_best_shot_target(world, passee->position(), passer).second;
+		
+		return  angle >= minangle && dist < maxdist;
+	}
+
 }
 
 EnemyThreat eval_enemy(const World &world, Robot::Ptr robot) {
 
-	#warning Under Construction
+	// TODO: Check for errors
 	EnemyThreat enemy_threat;
 
 	enemy_threat.threat_dist = std::min((robot->position() - world.ball().position()).len() , (robot->position() - world.field().friendly_goal()).len());
 	
+	enemy_threat.blocked = enemy_can_receive(world, robot);
+	
 	std::vector<AI::HL::W::Robot::Ptr> enemies = AI::HL::Util::get_robots(world.enemy_team());
 	
 	std::sort(enemies.begin(), enemies.end(), AI::HL::Util::CmpDist<Robot::Ptr>(robot->position()));
-
+	
 	// don't count this robot
 	for (std::size_t i = 1; i < enemies.size(); ++i) {
-		if (enemy_can_receive(world, enemies[i])){
+		if (available_enemy_pass(world, robot, enemies[i])){
 			enemy_threat.pass_enemies.push_back(enemies[i]);
 		} 
 	}	
--- software/ai/hl/stp/evaluation/enemy.h
+++ software/ai/hl/stp/evaluation/enemy.h
@@ -21,11 +21,6 @@ namespace AI {
 					 * blocked by our player or not to our goal
 					 */
 					bool blocked;
-					
-					/**
-					 * blocked by our player or not to pass
-					 */
-					bool blocked_pass;
 
 					/**
 					 * Other enemies that the enemy can pass to sorted by distance.
@@ -40,15 +35,6 @@ namespace AI {
 				EnemyThreat eval_enemy(const AI::HL::W::World &world);
 				
 				
-				bool enemy_blocked(const AI::HL::W::World &world, const AI::HL::W::Robot::Ptr enemy);
-				
-				/**
-			 	* Checks if the enemy can get the ball now.
-				 *
-				 * \return \c false if some players is blocking line of sight of ball from \p enemy, if \p enemy is not facing the ball, or if some condition is invalid;
-				 * or \c true if \p enemy can receive the ball.
-			 	*/
-				bool enemy_can_receive(const AI::HL::W::World &world, const AI::HL::W::Robot::Ptr enemy);
 				
 			}
 		}
