3b5b6bc767d801b7113df414877a37457e9f2935
simonsyd <simonsyd@localhost>
2011-04-09 19:54:05 +0000

    stp test chase

---
+++ software/ai/hl/stp/test_chase.cpp
@@ -0,0 +1,56 @@
+#include "ai/hl/hl.h"
+#include "ai/hl/stp/action/actions.h"
+#include "ai/hl/stp/tactic/idle.h"
+#include "util/dprint.h"
+
+#include <cassert>
+#include <gtkmm.h>
+
+using namespace AI::HL;
+using namespace AI::HL::STP;
+using namespace AI::HL::W;
+
+namespace {
+	class TestChaseFactory : public HighLevelFactory {
+		public:
+			TestChaseFactory() : HighLevelFactory("Test STP Chase") {
+			}
+
+			HighLevel::Ptr create_high_level(World &world) const;
+	};
+
+	TestChaseFactory factory_instance;
+
+	class TestChase : public HighLevel {
+		public:
+			TestChase(World& world) : world(world) {
+			}
+
+		private:
+			World& world;
+
+			TestChaseFactory &factory() const {
+				return factory_instance;
+			}
+
+			Gtk::Widget *ui_controls() {
+				return NULL;
+			}
+
+			void draw_overlay(Cairo::RefPtr<Cairo::Context> ctx) {
+			}
+
+			void tick() {
+				FriendlyTeam& friendly = world.friendly_team();
+				if (friendly.size() == 0) return;
+
+				Action::chase(world, friendly.get(0), 0);
+			}
+	};
+
+	HighLevel::Ptr TestChaseFactory::create_high_level(World &world) const {
+		HighLevel::Ptr p(new TestChase(world));
+		return p;
+	}
+}
+
