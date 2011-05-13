9256130f7884249d36b230a1b2b2bb0ebf8108ae
tere11 <tere11@localhost>
2011-05-13 01:35:30 +0000

    removed duplicate goalie code

--- software/ai/hl/stp/action/goalie.cpp
+++ software/ai/hl/stp/action/goalie.cpp
@@ -14,7 +14,7 @@ using AI::HL::STP::Evaluation::BallThreat;
 
 namespace {
 	DoubleParam lone_goalie_dist("Lone Goalie: distance to goal post (m)", "STP/Action/Goalie" , 0.30, 0.05, 1.0);
-	// DoubleParam ball_dangerous_speed("Goalie Action: threatening ball speed", "STP/Action/Goalie", 0.1, 0.1, 10.0); 
+	
 }
 
 void AI::HL::STP::Action::lone_goalie(const World &world, Player::Ptr player) {
@@ -41,8 +41,6 @@ void AI::HL::STP::Action::goalie_move(const World &world, Player::Ptr player, Po
 		repel(world, player);
 		return;
 	}
-
-	// use ball threat evaluation instead?
 	
 	// check if ball is heading towards our goal
 	if (Evaluation::ball_on_net(world)){
@@ -58,28 +56,5 @@ void AI::HL::STP::Action::goalie_move(const World &world, Player::Ptr player, Po
 		player->prio(AI::Flags::MovePrio::MEDIUM);
 	}
 
-	/*
-	// Check if ball is threatening to our goal
-	Point ballvel = world.ball().velocity();
-	Point ballpos = world.ball().position();
-	Point goalpos;
-	if (ballvel.len() > ball_dangerous_speed && ballvel.x < -1e-6) {
-
-		goalpos = line_intersect(ballpos, ballpos + ballvel, 
-				Point(-world.field().length()/2.0+0.2, 1.0),
-				Point(-world.field().length()/2.0+0.2, -1.0));
-
-		if (std::fabs(goalpos.y) < world.field().goal_width()/2.0) {
-			player->move(goalpos, (world.ball().position() - player->position()).orientation(), Point());
-			player->type(AI::Flags::MoveType::RAM_BALL);
-			player->prio(AI::Flags::MovePrio::HIGH);
-			return;
-		}
-	}
-
-	player->move(dest, (world.ball().position() - player->position()).orientation(), Point());
-	player->type(AI::Flags::MoveType::NORMAL);
-	player->prio(AI::Flags::MovePrio::MEDIUM);
-	*/
 }
 
