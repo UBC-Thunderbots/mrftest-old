daa86f0431cae345abb310498ce9197e8a273246
chead <chead@localhost>
2011-05-14 18:56:41 +0000

    Provide macros to make plays smaller to write.

--- software/ai/hl/stp/play/just_shoot.cpp
+++ software/ai/hl/stp/play/just_shoot.cpp
@@ -1,84 +1,29 @@
-#include "ai/hl/stp/predicates.h"
-#include "ai/hl/stp/play/play.h"
-#include "ai/hl/stp/tactic/offend.h"
-#include "ai/hl/stp/tactic/defend.h"
-#include "ai/hl/stp/tactic/shoot.h"
-#include "ai/hl/util.h"
-#include "util/dprint.h"
-#include <glibmm.h>
+#include "ai/hl/stp/play/simple_play.h"
 
-using namespace AI::HL::STP::Play;
-using namespace AI::HL::STP::Tactic;
-using namespace AI::HL::W;
-namespace Predicates = AI::HL::STP::Predicates;
+BEGIN_PLAY(JustShoot)
+INVARIANT(playtype(world, PlayType::PLAY) && our_team_size_at_least(world, 3) && baller_can_shoot(world))
+APPLICABLE(our_ball(world))
+DONE(goal(world))
+FAIL(their_ball(world))
+BEGIN_ASSIGN()
+	// GOALIE
+	goalie_role.push_back(defend_duo_goalie(world));
 
-namespace {
-	/**
-	 * Condition:
-	 * - at least 3 players
-	 * - ball under team possesion
-	 *
-	 * Objective:
-	 * - shoot the ball to enemy goal
-	 */
-	class JustShoot : public Play {
-		public:
-			JustShoot(const AI::HL::W::World &world);
+	// ROLE 1
+	// shoot
+	roles[0].push_back(shoot(world));
 
-		private:
-			bool invariant() const;
-			bool applicable() const;
-			bool done() const;
-			bool fail() const;
-			void assign(std::vector<Tactic::Ptr> &goalie_role, std::vector<Tactic::Ptr>(&roles)[4]);
-			const PlayFactory &factory() const;
-	};
+	// ROLE 2
+	// defend
+	roles[1].push_back(defend_duo_defender(world));
 
-	PlayFactoryImpl<JustShoot> factory_instance("Just Shoot");
+	// ROLE 3 (optional)
+	// offensive support
+	roles[2].push_back(offend(world));
 
-	const PlayFactory &JustShoot::factory() const {
-		return factory_instance;
-	}
-
-	JustShoot::JustShoot(const World &world) : Play(world) {
-	}
-
-	bool JustShoot::invariant() const {
-		return Predicates::playtype(world, AI::Common::PlayType::PLAY) && Predicates::our_team_size_at_least(world, 3) && Predicates::baller_can_shoot(world);
-	}
-
-	bool JustShoot::applicable() const {
-		return Predicates::our_ball(world);
-	}
-
-	bool JustShoot::done() const {
-		return Predicates::goal(world);
-	}
-
-	bool JustShoot::fail() const {
-		return Predicates::their_ball(world);
-	}
-
-	void JustShoot::assign(std::vector<Tactic::Ptr> &goalie_role, std::vector<Tactic::Ptr>(&roles)[4]) {
-
-		// GOALIE
-		goalie_role.push_back(defend_duo_goalie(world));
-
-		// ROLE 1
-		// shoot
-		roles[0].push_back(shoot(world));
-
-		// ROLE 2
-		// defend
-		roles[1].push_back(defend_duo_defender(world));
-
-		// ROLE 3 (optional)
-		// offensive support
-		roles[2].push_back(offend(world));
-
-		// ROLE 4 (optional)
-		// offensive support
-		roles[3].push_back(offend(world));
-	}
-}
+	// ROLE 4 (optional)
+	// offensive support
+	roles[3].push_back(offend(world));
+END_ASSIGN()
+END_PLAY()
 
---
+++ software/ai/hl/stp/play/simple_play.h
@@ -0,0 +1,61 @@
+#ifndef AI_HL_STP_PLAY_SIMPLE_PLAY_H
+#define AI_HL_STP_PLAY_SIMPLE_PLAY_H
+
+#include "ai/hl/util.h"
+#include "ai/hl/world.h"
+#include "ai/hl/stp/predicates.h"
+#include "ai/hl/stp/play/play.h"
+#include "ai/hl/stp/tactic/defend.h"
+#include "ai/hl/stp/tactic/offend.h"
+#include "ai/hl/stp/tactic/shoot.h"
+#include <glibmm.h>
+
+using AI::Common::PlayType;
+using namespace AI::HL::STP::Predicates;
+using namespace AI::HL::STP::Tactic;
+
+#define BEGIN_PLAY(cls) \
+	namespace { \
+		class cls; \
+		AI::HL::STP::Play::PlayFactoryImpl<cls> factory_instance(#cls); \
+		class cls : public AI::HL::STP::Play::Play { \
+			public: \
+				cls(const AI::HL::W::World &world) : AI::HL::STP::Play::Play(world) { \
+				} \
+\
+				AI::HL::STP::Play::PlayFactory &factory() const { \
+					return factory_instance; \
+				}
+
+#define INVARIANT(expr) \
+				bool invariant() const { \
+					return expr; \
+				}
+
+#define APPLICABLE(expr) \
+				bool applicable() const { \
+					return expr; \
+				}
+
+#define DONE(expr) \
+				bool done() const { \
+					return expr; \
+				}
+
+#define FAIL(expr) \
+				bool fail() const { \
+					return expr; \
+				}
+
+#define BEGIN_ASSIGN() \
+				void assign(std::vector<Tactic::Ptr> &goalie_role, std::vector<Tactic::Ptr> (&roles)[4]) {
+
+#define END_ASSIGN() \
+				}
+
+#define END_PLAY() \
+		}; \
+	}
+
+#endif
+
