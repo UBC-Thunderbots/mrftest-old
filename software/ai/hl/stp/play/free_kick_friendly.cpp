f16589d17bd6e883abba7ed8c1020957cb4bdf90
mparizeau <mparizeau@localhost>
2011-05-21 22:58:59 +0000

    Convert free kick friendly plays to the simple play macros

--- software/ai/hl/stp/play/free_kick_friendly.cpp
+++ software/ai/hl/stp/play/free_kick_friendly.cpp
@@ -1,86 +1,37 @@
-#include "ai/hl/stp/evaluation/defense.h"
-#include "ai/hl/stp/predicates.h"
-#include "ai/hl/stp/play/play.h"
-#include "ai/hl/stp/tactic/shoot.h"
-#include "ai/hl/stp/tactic/defend.h"
-#include "ai/hl/stp/tactic/offend.h"
-#include "ai/hl/stp/tactic/wait_playtype.h"
-#include "ai/hl/stp/tactic/move.h"
-#include "ai/hl/util.h"
-#include "util/dprint.h"
-#include <glibmm.h>
+#include "ai/hl/stp/play/simple_play.h"
 
-using namespace AI::HL::STP::Play;
-using namespace AI::HL::STP::Tactic;
-using namespace AI::HL::W;
 namespace Predicates = AI::HL::STP::Predicates;
 
-namespace {
-	/**
-	 * Condition:
-	 * - Playtype Free Kick Friendly
-	 *
-	 * Objective:
-	 * - Handle Friendly Free Kick
-	 */
-	class FreeKickFriendly : public Play {
-		public:
-			FreeKickFriendly(const World &world);
+/**
+ * Condition:
+ * - Playtype Free Kick Friendly
+ *
+ * Objective:
+ * - Handle Friendly Free Kick
+ */
+BEGIN_PLAY(FreeKickFriendly)
+INVARIANT((Predicates::playtype(world, AI::Common::PlayType::EXECUTE_DIRECT_FREE_KICK_FRIENDLY) || Predicates::playtype(world, AI::Common::PlayType::EXECUTE_INDIRECT_FREE_KICK_FRIENDLY)) && Predicates::our_team_size_at_least(world, 3) && !Predicates::baller_can_pass(world))
+APPLICABLE(true)
+DONE(false)
+FAIL(false)
+BEGIN_ASSIGN()
+	// GOALIE
+	goalie_role.push_back(defend_duo_goalie(world));
 
-		private:
-			bool invariant() const;
-			bool applicable() const;
-			bool done() const;
-			bool fail() const;
-			void assign(std::vector<Tactic::Ptr> &goalie_role, std::vector<Tactic::Ptr>(&roles)[4]);
-			const PlayFactory &factory() const;
-	};
+	// ROLE 1
+	// kicker
+	roles[0].push_back(shoot(world));
 
-	PlayFactoryImpl<FreeKickFriendly> factory_instance("Free Kick Friendly");
+	// ROLE 2
+	// defend
+	roles[1].push_back(defend_duo_defender(world));
 
-	const PlayFactory &FreeKickFriendly::factory() const {
-		return factory_instance;
-	}
+	// ROLE 3
+	// offend
+	roles[2].push_back(offend(world));
 
-	FreeKickFriendly::FreeKickFriendly(const World &world) : Play(world) {
-	}
-
-	bool FreeKickFriendly::invariant() const {
-		return (Predicates::playtype(world, AI::Common::PlayType::EXECUTE_DIRECT_FREE_KICK_FRIENDLY) || Predicates::playtype(world, AI::Common::PlayType::EXECUTE_INDIRECT_FREE_KICK_FRIENDLY)) && Predicates::our_team_size_at_least(world, 3) && !Predicates::baller_can_pass(world);
-	}
-
-	bool FreeKickFriendly::applicable() const {
-		return true;
-	}
-
-	bool FreeKickFriendly::done() const {
-		return false;
-	}
-
-	bool FreeKickFriendly::fail() const {
-		return false;
-	}
-
-	void FreeKickFriendly::assign(std::vector<Tactic::Ptr> &goalie_role, std::vector<Tactic::Ptr>(&roles)[4]) {
-		
-		// GOALIE
-		goalie_role.push_back(defend_duo_goalie(world));
-
-		// ROLE 1
-		// kicker
-		roles[0].push_back(shoot(world));
-		
-		// ROLE 2
-		// defend
-		roles[1].push_back(defend_duo_defender(world));
-		
-		// ROLE 3
-		// offend
-		roles[2].push_back(offend(world));
-		
-		// ROLE 4
-		// offend
-		roles[3].push_back(offend(world));
-	}
-}
-
+	// ROLE 4
+	// offend
+	roles[3].push_back(offend(world));
+END_ASSIGN()
+END_PLAY()
--- software/ai/hl/stp/play/free_kick_friendly_pass.cpp
+++ software/ai/hl/stp/play/free_kick_friendly_pass.cpp
@@ -1,88 +1,32 @@
-#include "ai/hl/stp/evaluation/defense.h"
-#include "ai/hl/stp/predicates.h"
-#include "ai/hl/stp/play/play.h"
-#include "ai/hl/stp/tactic/shoot.h"
-#include "ai/hl/stp/tactic/defend.h"
-#include "ai/hl/stp/tactic/offend.h"
-#include "ai/hl/stp/tactic/wait_playtype.h"
 #include "ai/hl/stp/tactic/pass.h"
-#include "ai/hl/util.h"
-#include "util/dprint.h"
-#include <glibmm.h>
+#include "ai/hl/stp/play/simple_play.h"
 
-using namespace AI::HL::STP::Play;
-using namespace AI::HL::STP::Tactic;
-using namespace AI::HL::W;
 namespace Predicates = AI::HL::STP::Predicates;
 
-namespace {
-	/**
-	 * Condition:
-	 * - Playtype Free Kick Friendly
-	 * - Baller can't shoot at enemy ball but can pass to some other player
-	 *
-	 * Objective:
-	 * - Handle Friendly Free Kick
-	 */
-	class FreeKickFriendlyPass : public Play {
-		public:
-			FreeKickFriendlyPass(const World &world);
+BEGIN_PLAY(FreeKickFriendlyPass)
+INVARIANT((Predicates::playtype(world, AI::Common::PlayType::EXECUTE_DIRECT_FREE_KICK_FRIENDLY) || Predicates::playtype(world, AI::Common::PlayType::EXECUTE_INDIRECT_FREE_KICK_FRIENDLY)) && Predicates::our_team_size_at_least(world, 3) && !Predicates::baller_can_shoot(world) && Predicates::baller_can_pass(world))
+APPLICABLE(true)
+DONE(false)
+FAIL(false)
+BEGIN_ASSIGN()
+	// GOALIE
+	goalie_role.push_back(defend_duo_goalie(world));
 
-		private:
-			bool invariant() const;
-			bool applicable() const;
-			bool done() const;
-			bool fail() const;
-			void assign(std::vector<Tactic::Ptr> &goalie_role, std::vector<Tactic::Ptr>(&roles)[4]);
-			const PlayFactory &factory() const;
-	};
+	// ROLE 1
+	// passer
+	roles[0].push_back(passer_shoot(world));
+	roles[0].push_back(offend(world));
 
-	PlayFactoryImpl<FreeKickFriendlyPass> factory_instance("Free Kick Friendly Pass");
+	// ROLE 2
+	// passee
+	roles[1].push_back(passee_move(world));
 
-	const PlayFactory &FreeKickFriendlyPass::factory() const {
-		return factory_instance;
-	}
+	// ROLE 3
+	// defend
+	roles[2].push_back(defend_duo_defender(world));
 
-	FreeKickFriendlyPass::FreeKickFriendlyPass(const World &world) : Play(world) {
-	}
-
-	bool FreeKickFriendlyPass::invariant() const {
-		return (Predicates::playtype(world, AI::Common::PlayType::EXECUTE_DIRECT_FREE_KICK_FRIENDLY) || Predicates::playtype(world, AI::Common::PlayType::EXECUTE_INDIRECT_FREE_KICK_FRIENDLY)) && Predicates::our_team_size_at_least(world, 3) && !Predicates::baller_can_shoot(world) && Predicates::baller_can_pass(world);
-	}
-
-	bool FreeKickFriendlyPass::applicable() const {
-		return true;
-	}
-
-	bool FreeKickFriendlyPass::done() const {
-		return false;
-	}
-
-	bool FreeKickFriendlyPass::fail() const {
-		return false;
-	}
-
-	void FreeKickFriendlyPass::assign(std::vector<Tactic::Ptr> &goalie_role, std::vector<Tactic::Ptr>(&roles)[4]) {
-		
-		// GOALIE
-		goalie_role.push_back(defend_duo_goalie(world));
-
-		// ROLE 1
-		// passer
-		roles[0].push_back(passer_shoot(world));
-		roles[0].push_back(offend(world));
-
-		// ROLE 2
-		// passee
-		roles[1].push_back(passee_move(world));
-
-		// ROLE 3
-		// defend
-		roles[2].push_back(defend_duo_defender(world));
-
-		// ROLE 4
-		// offend
-		roles[3].push_back(offend(world));
-	}
-}
-
+	// ROLE 4
+	// offend
+	roles[3].push_back(offend(world));
+END_ASSIGN()
+END_PLAY()
