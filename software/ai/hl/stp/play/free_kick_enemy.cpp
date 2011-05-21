0e217ff9506f0a6ab25973c045e3650784ffee5f
mparizeau <mparizeau@localhost>
2011-05-21 22:44:02 +0000

    Convert free kick enemy to the simple play macros

--- software/ai/hl/stp/play/free_kick_enemy.cpp
+++ software/ai/hl/stp/play/free_kick_enemy.cpp
@@ -1,86 +1,38 @@
-#include "ai/hl/stp/evaluation/defense.h"
-#include "ai/hl/stp/predicates.h"
-#include "ai/hl/stp/play/play.h"
-#include "ai/hl/stp/tactic/shoot.h"
-#include "ai/hl/stp/tactic/defend.h"
-#include "ai/hl/stp/tactic/offend.h"
 #include "ai/hl/stp/tactic/wait_playtype.h"
-#include "ai/hl/stp/tactic/move.h"
-#include "ai/hl/util.h"
-#include "util/dprint.h"
-#include <glibmm.h>
+#include "ai/hl/stp/play/simple_play.h"
 
-using namespace AI::HL::STP::Play;
-using namespace AI::HL::STP::Tactic;
-using namespace AI::HL::W;
 namespace Predicates = AI::HL::STP::Predicates;
 
-namespace {
-	/**
-	 * Condition:
-	 * - Playtype Free Kick Enemy
-	 *
-	 * Objective:
-	 * - Handle Enemy Free Kick
-	 */
-	class FreeKickEnemy : public Play {
-		public:
-			FreeKickEnemy(const World &world);
+/**
+ * Condition:
+ * - Playtype Free Kick Enemy
+ *
+ * Objective:
+ * - Handle Enemy Free Kick
+ */
+BEGIN_PLAY(FreeKickEnemy)
+INVARIANT((Predicates::playtype(world, AI::Common::PlayType::EXECUTE_DIRECT_FREE_KICK_ENEMY) || Predicates::playtype(world, AI::Common::PlayType::EXECUTE_INDIRECT_FREE_KICK_ENEMY)) && Predicates::our_team_size_at_least(world, 2))
+APPLICABLE(true)
+DONE(false)
+FAIL(false)
+BEGIN_ASSIGN()
+	// GOALIE
+	goalie_role.push_back(wait_playtype(world, defend_duo_goalie(world), AI::Common::PlayType::PLAY));
 
-		private:
-			bool invariant() const;
-			bool applicable() const;
-			bool done() const;
-			bool fail() const;
-			void assign(std::vector<Tactic::Ptr> &goalie_role, std::vector<Tactic::Ptr>(&roles)[4]);
-			const PlayFactory &factory() const;
-	};
+	// ROLE 1
+	// defend
+	roles[0].push_back(defend_duo_defender(world));
 
-	PlayFactoryImpl<FreeKickEnemy> factory_instance("Free Kick Enemy");
+	// ROLE 2
+	// defend
+	roles[1].push_back(defend_duo_extra(world));
 
-	const PlayFactory &FreeKickEnemy::factory() const {
-		return factory_instance;
-	}
+	// ROLE 3
+	// offend
+	roles[2].push_back(offend(world));
 
-	FreeKickEnemy::FreeKickEnemy(const World &world) : Play(world) {
-	}
-
-	bool FreeKickEnemy::invariant() const {
-		return (Predicates::playtype(world, AI::Common::PlayType::EXECUTE_DIRECT_FREE_KICK_ENEMY) || Predicates::playtype(world, AI::Common::PlayType::EXECUTE_INDIRECT_FREE_KICK_ENEMY)) && Predicates::our_team_size_at_least(world, 2);
-	}
-
-	bool FreeKickEnemy::applicable() const {
-		return true;
-	}
-
-	bool FreeKickEnemy::done() const {
-		return false;
-	}
-
-	bool FreeKickEnemy::fail() const {
-		return false;
-	}
-
-	void FreeKickEnemy::assign(std::vector<Tactic::Ptr> &goalie_role, std::vector<Tactic::Ptr>(&roles)[4]) {
-		// std::Player::Ptr goalie = world.Enemy_team().get(0);
-		// GOALIE
-		goalie_role.push_back(wait_playtype(world, defend_duo_goalie(world), AI::Common::PlayType::PLAY));
-
-		// ROLE 1
-		// defend
-		roles[0].push_back(defend_duo_defender(world));
-
-		// ROLE 2
-		// defend
-		roles[1].push_back(defend_duo_extra(world));
-
-		// ROLE 3
-		// offend
-		roles[2].push_back(offend(world));
-
-		// ROLE 4
-		// offend
-		roles[3].push_back(offend(world));
-	}
-}
-
+	// ROLE 4
+	// offend
+	roles[3].push_back(offend(world));
+END_ASSIGN()
+END_PLAY()
