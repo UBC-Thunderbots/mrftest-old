82b54330250bf674afa24c1486b9801151230203
mparizeau <mparizeau@localhost>
2011-05-21 22:31:27 +0000

    Convert frantic defense to the simple play macros

--- software/ai/hl/stp/play/frantic_defense.cpp
+++ software/ai/hl/stp/play/frantic_defense.cpp
@@ -1,89 +1,43 @@
-#include "ai/hl/stp/play/play.h"
-#include "ai/hl/stp/predicates.h"
-#include "ai/hl/stp/tactic/chase.h"
-#include "ai/hl/stp/tactic/defend_solo.h"
 #include "ai/hl/stp/tactic/repel.h"
 #include "ai/hl/stp/tactic/block.h"
-#include "ai/hl/util.h"
-#include "util/dprint.h"
-#include <glibmm.h>
+#include "ai/hl/stp/play/simple_play.h"
 
-using namespace AI::HL::STP::Play;
-using namespace AI::HL::STP::Tactic;
-using namespace AI::HL::W;
 using AI::HL::STP::Enemy;
 namespace Predicates = AI::HL::STP::Predicates;
 
-namespace {
-	/**
-	 * Condition:
-	 * - ball not under team possesion
-	 * - ball not in corner or midfield
-	 * - ball on our side (hence very very close to our goal!)
-	 * - have at least 2 players (one goalie, one defender)
-	 *
-	 * Objective:
-	 * - get the ball away from our goal at all cost!
-	 */
-	class FranticDefense : public Play {
-		public:
-			FranticDefense(const AI::HL::W::World &world);
+/**
+ * Condition:
+ * - ball not under team possesion
+ * - ball not in corner or midfield
+ * - ball on our side (hence very very close to our goal!)
+ * - have at least 2 players (one goalie, one defender)
+ *
+ * Objective:
+ * - get the ball away from our goal at all cost!
+ */
+BEGIN_PLAY(FranticDefense)
+INVARIANT(Predicates::playtype(world, AI::Common::PlayType::PLAY) && Predicates::our_team_size_at_least(world, 2) && Predicates::their_ball(world))
+APPLICABLE(!Predicates::ball_in_our_corner(world) && !Predicates::ball_in_their_corner(world) && !Predicates::ball_midfield(world) && Predicates::ball_on_our_side(world))
+DONE(!Predicates::their_ball(world) || Predicates::ball_in_our_corner(world) || Predicates::ball_in_their_corner(world) || Predicates::ball_midfield(world) || Predicates::ball_on_their_side(world))
+FAIL(false)
+BEGIN_ASSIGN()
+	// GOALIE
+	goalie_role.push_back(defend_solo_goalie(world));
 
-		private:
-			bool invariant() const; 
-			bool applicable() const;
-			bool done() const;
-			bool fail() const;
-			void assign(std::vector<Tactic::Ptr> &goalie_role, std::vector<Tactic::Ptr>(&roles)[4]);
-			const PlayFactory &factory() const;
-	};
+	// ROLE 1
+	// try to repel ball away
+	roles[0].push_back(repel(world));
 
-	PlayFactoryImpl<FranticDefense> factory_instance("Frantic Defense");
+	// ROLE 2
+	// block the enemy baller
+	roles[1].push_back(block(world, Enemy::closest_ball(world, 0)));
 
-	const PlayFactory &FranticDefense::factory() const {
-		return factory_instance;
-	}
+	// ROLE 3 (optional)
+	// block enemy closest to our goal
+	roles[2].push_back(block(world, Enemy::closest_friendly_goal(world, 0)));
 
-	FranticDefense::FranticDefense(const World &world) : Play(world) {
-	}
-
-	bool FranticDefense::invariant() const {
-		return Predicates::playtype(world, AI::Common::PlayType::PLAY) && Predicates::our_team_size_at_least(world, 2) && Predicates::their_ball(world);
-	}
-
-	bool FranticDefense::applicable() const {
-		return !Predicates::ball_in_our_corner(world) && !Predicates::ball_in_their_corner(world) && !Predicates::ball_midfield(world) && Predicates::ball_on_our_side(world);
-	}
-
-	bool FranticDefense::done() const {
-		return !Predicates::their_ball(world) || Predicates::ball_in_our_corner(world) || Predicates::ball_in_their_corner(world) || Predicates::ball_midfield(world) || Predicates::ball_on_their_side(world);
-	}
-
-	bool FranticDefense::fail() const {
-		return false;
-	}
-
-	void FranticDefense::assign(std::vector<Tactic::Ptr> &goalie_role, std::vector<Tactic::Ptr>(&roles)[4]) {
-		// std::Player::Ptr goalie = world.friendly_team().get(0);
-
-		// GOALIE
-		goalie_role.push_back(defend_solo_goalie(world));
-		
-		// ROLE 1
-		// try to repel ball away
-		roles[0].push_back(repel(world));
-
-		// ROLE 2
-		// block the enemy baller
-		roles[1].push_back(block(world, Enemy::closest_ball(world, 0)));
-
-		// ROLE 3 (optional)
-		// block enemy closest to our goal
-		roles[2].push_back(block(world, Enemy::closest_friendly_goal(world, 0)));
-
-		// ROLE 4 (optional)
-		// block enemy closest to our goal
-		roles[3].push_back(block(world, Enemy::closest_friendly_goal(world, 1)));
-	}
-}
-
+	// ROLE 4 (optional)
+	// block enemy closest to our goal
+	roles[3].push_back(block(world, Enemy::closest_friendly_goal(world, 1)));
+END_ASSIGN()
+END_PLAY()
