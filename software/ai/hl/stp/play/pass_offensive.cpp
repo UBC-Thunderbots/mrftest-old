3e6e74c6de807b24cb2f77474d4b8213b562b414
mparizeau <mparizeau@localhost>
2011-05-28 23:16:39 +0000

    Convert penalty enemy to the simple play macros

--- software/ai/hl/stp/play/pass_offensive.cpp
+++ software/ai/hl/stp/play/pass_offensive.cpp
@@ -5,6 +5,14 @@
 using AI::HL::STP::Enemy;
 namespace Predicates = AI::HL::STP::Predicates;
 
+/**
+ * Condition:
+ * - ball under team possesion
+ * - have at least 4 players (one goalie, one passer, one passee, one defender)
+ *
+ * Objective:
+ * - shoot the ball to enemy goal while passing the ball between the passer and passee
+ */
 BEGIN_PLAY(PassOffensive)
 INVARIANT(Predicates::playtype(world, AI::Common::PlayType::PLAY) && Predicates::our_team_size_at_least(world, 3) && Predicates::their_team_size_at_least(world, 1) && !Predicates::baller_can_shoot(world) && Predicates::baller_can_pass(world))
 APPLICABLE(Predicates::our_ball(world) && (Predicates::ball_midfield(world) || Predicates::ball_in_their_corner(world) || Predicates::ball_in_our_corner(world)))
--- software/ai/hl/stp/play/penalty_enemy.cpp
+++ software/ai/hl/stp/play/penalty_enemy.cpp
@@ -1,86 +1,45 @@
-#include "ai/hl/stp/evaluation/defense.h"
-#include "ai/hl/stp/predicates.h"
-#include "ai/hl/stp/play/play.h"
-#include "ai/hl/stp/tactic/penalty_goalie.h"
 #include "ai/hl/stp/tactic/move.h"
-#include "ai/hl/util.h"
-#include "util/dprint.h"
-#include <glibmm.h>
+#include "ai/hl/stp/tactic/penalty_goalie.h"
+#include "ai/hl/stp/play/simple_play.h"
 
-using namespace AI::HL::STP::Play;
-using namespace AI::HL::STP::Tactic;
 using namespace AI::HL::W;
 namespace Predicates = AI::HL::STP::Predicates;
 
 namespace {
 	const double PENALTY_MARK_LENGTH = 0.45;
 	const double RESTRICTED_ZONE_LENGTH = 0.85;
-	/**
-	 * Condition:
-	 * - Playtype Prepare Penalty Enemy
-	 *
-	 * Objective:
-	 * - move to Penalty positions and shoot the ball to enemy goal
-	 */
-	class PenaltyEnemy : public Play {
-		public:
-			PenaltyEnemy(const World &world);
-
-		private:
-			bool invariant() const;
-			bool applicable() const;
-			bool done() const;
-			bool fail() const;
-			void assign(std::vector<Tactic::Ptr> &goalie_role, std::vector<Tactic::Ptr>(&roles)[4]);
-			const PlayFactory &factory() const;
-	};
-
-	PlayFactoryImpl<PenaltyEnemy> factory_instance("Penalty Enemy");
-
-	const PlayFactory &PenaltyEnemy::factory() const {
-		return factory_instance;
-	}
-
-	PenaltyEnemy::PenaltyEnemy(const World &world) : Play(world) {
-	}
-
-	bool PenaltyEnemy::invariant() const {
-		return (Predicates::playtype(world, AI::Common::PlayType::PREPARE_PENALTY_ENEMY) || Predicates::playtype(world, AI::Common::PlayType::EXECUTE_PENALTY_ENEMY)) && Predicates::our_team_size_at_least(world, 1);
-	}
-
-	bool PenaltyEnemy::applicable() const {
-		return true;
-	}
-
-	bool PenaltyEnemy::done() const {
-		return false;
-	}
-
-	bool PenaltyEnemy::fail() const {
-		return false;
-	}
-
-	void PenaltyEnemy::assign(std::vector<Tactic::Ptr> &goalie_role, std::vector<Tactic::Ptr>(&roles)[4]) {
-		// std::Player::Ptr goalie = world.Enemy_team().get(0);
-
-		// GOALIE
-		goalie_role.push_back(penalty_goalie(world));
-
-		// ROLE 1
-		// move to penalty position 1
-		roles[0].push_back(move(world, Point(-0.5 * world.field().length() + RESTRICTED_ZONE_LENGTH + Robot::MAX_RADIUS, 5 * Robot::MAX_RADIUS)));
-
-		// ROLE 2
-		// move to penalty position 2
-		roles[1].push_back(move(world, Point(-0.5 * world.field().length() + RESTRICTED_ZONE_LENGTH + Robot::MAX_RADIUS, 2 * Robot::MAX_RADIUS)));
-
-		// ROLE 3
-		// move to penalty position 3
-		roles[2].push_back(move(world, Point(-0.5 * world.field().length() + RESTRICTED_ZONE_LENGTH + Robot::MAX_RADIUS, -2 * Robot::MAX_RADIUS)));
-
-		// ROLE 4
-		// move to penalty position 4
-		roles[3].push_back(move(world, Point(-0.5 * world.field().length() + RESTRICTED_ZONE_LENGTH + Robot::MAX_RADIUS, -5 * Robot::MAX_RADIUS)));
-	}
 }
 
+/**
+ * Condition:
+ * - Playtype Prepare Penalty Enemy
+ *
+ * Objective:
+ * - move to Penalty positions and shoot the ball to enemy goal
+ */
+BEGIN_PLAY(PenaltyEnemy)
+INVARIANT((Predicates::playtype(world, AI::Common::PlayType::PREPARE_PENALTY_ENEMY) || Predicates::playtype(world, AI::Common::PlayType::EXECUTE_PENALTY_ENEMY)) && Predicates::our_team_size_at_least(world, 1))
+APPLICABLE(true)
+DONE(false)
+FAIL(false)
+BEGIN_ASSIGN()
+// GOALIE
+goalie_role.push_back(penalty_goalie(world));
+
+// ROLE 1
+// move to penalty position 1
+roles[0].push_back(move(world, Point(-0.5 * world.field().length() + RESTRICTED_ZONE_LENGTH + Robot::MAX_RADIUS, 5 * Robot::MAX_RADIUS)));
+
+// ROLE 2
+// move to penalty position 2
+roles[1].push_back(move(world, Point(-0.5 * world.field().length() + RESTRICTED_ZONE_LENGTH + Robot::MAX_RADIUS, 2 * Robot::MAX_RADIUS)));
+
+// ROLE 3
+// move to penalty position 3
+roles[2].push_back(move(world, Point(-0.5 * world.field().length() + RESTRICTED_ZONE_LENGTH + Robot::MAX_RADIUS, -2 * Robot::MAX_RADIUS)));
+
+// ROLE 4
+// move to penalty position 4
+roles[3].push_back(move(world, Point(-0.5 * world.field().length() + RESTRICTED_ZONE_LENGTH + Robot::MAX_RADIUS, -5 * Robot::MAX_RADIUS)));
+END_ASSIGN()
+END_PLAY()
