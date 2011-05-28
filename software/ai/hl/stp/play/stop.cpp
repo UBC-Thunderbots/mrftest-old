fb8a051bcc6df217459bb830aae2979d8cdd2adb
myd <myd@localhost>
2011-05-28 22:19:25 +0000

    refactor stop play

--- software/ai/hl/stp/play/stop.cpp
+++ software/ai/hl/stp/play/stop.cpp
@@ -1,15 +1,11 @@
-#include "ai/hl/stp/play/play.h"
+#include "ai/hl/stp/play/simple_play.h"
 #include "ai/hl/stp/predicates.h"
-#include "ai/hl/stp/tactic/defend.h"
 #include "ai/hl/stp/tactic/move_stop.h"
 #include "ai/hl/stp/tactic/wait_playtype.h"
 
 using namespace AI::HL::STP::Play;
-using namespace AI::HL::STP::Tactic;
 using namespace AI::HL::W;
 namespace Predicates = AI::HL::STP::Predicates;
-
-namespace {
 	/**
 	 * Condition:
 	 * - It is the stop play
@@ -17,55 +13,25 @@ namespace {
 	 * Objective:
 	 * - Handle the stop play
 	 */
-	class Stop : public Play {
-		public:
-			Stop(const World &world);
+BEGIN_PLAY(Stop)
+INVARIANT(Predicates::playtype(world, AI::Common::PlayType::STOP))
+APPLICABLE(true)
+DONE(false)
+FAIL(false)
+BEGIN_ASSIGN()
+goalie_role.push_back(defend_duo_goalie(world));
 
-		private:
-			bool invariant() const;
-			bool applicable() const;
-			bool done() const;
-			bool fail() const;
-			void assign(std::vector<Tactic::Ptr> &goalie_role, std::vector<Tactic::Ptr>(&roles)[4]);
-			const PlayFactory &factory() const;
-	};
+// doesn't matter what the playtype we are waiting for is here, we just need an active tactic
+roles[0].push_back(wait_playtype(world, move_stop(world, 0), AI::Common::PlayType::PLAY));
 
-	PlayFactoryImpl<Stop> factory_instance("Stop");
+roles[1].push_back(move_stop(world, 1));
 
-	const PlayFactory &Stop::factory() const {
-		return factory_instance;
-	}
+roles[2].push_back(move_stop(world, 2));
 
-	Stop::Stop(const World &world) : Play(world) {
-	}
+roles[3].push_back(move_stop(world, 3));
+END_ASSIGN()
+END_PLAY()
 
-	bool Stop::invariant() const {
-		return Predicates::playtype(world, AI::Common::PlayType::STOP);
-	}
 
-	bool Stop::applicable() const {
-		return true;
-	}
 
-	bool Stop::done() const {
-		return false;
-	}
-
-	bool Stop::fail() const {
-		return false;
-	}
-
-	void Stop::assign(std::vector<Tactic::Ptr> &goalie_role, std::vector<Tactic::Ptr>(&roles)[4]) {
-		goalie_role.push_back(defend_duo_goalie(world));
-
-		// doesn't matter what the playtype we are waiting for is here, we just need an active tactic
-		roles[0].push_back(wait_playtype(world, move_stop(world, 0), AI::Common::PlayType::PLAY));
-
-		roles[1].push_back(move_stop(world, 1));
-
-		roles[2].push_back(move_stop(world, 2));
-
-		roles[3].push_back(move_stop(world, 3));
-	}
-}
 
