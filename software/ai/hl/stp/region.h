36c16e74239c48722f69e3a4f4ec4a29805b9a0b
simonsyd <simonsyd@localhost>
2011-05-28 22:51:39 +0000

    update region class

--- software/ai/hl/stp/region.h
+++ software/ai/hl/stp/region.h
@@ -12,7 +12,7 @@ namespace AI {
 			 * The region can make use of dynamic coordinates.
 			 * See STP paper section 5.2.3 (b)
 			 *
-			 * NOTE: class needs to be copyable,
+			 * NOTE: class needs to be COPYABLE,
 			 * hence it is not subclassed.
 			 */
 			class Region {
@@ -41,8 +41,20 @@ namespace AI {
 					 */
 					Point center_velocity() const;
 
+					/**
+					 * Valid only if this region type is a circle.
+					 * Returns the associated radius.
+					 */
 					double radius() const {
 						return radius_;
+					}
+
+					/**
+					 * Valid only if this region type is a rectangle.
+					 * Evaluates the associated rectangle.
+					 */
+					Rect rectangle() const {
+						return Rect(p1.position(), p2.position());
 					}
 
 					/**
@@ -70,6 +82,9 @@ namespace AI {
 					Coordinate p1, p2;
 					double radius_;
 
+					/**
+					 * The actual constructor, hidden from public view.
+					 */
 					Region(Type type, Coordinate p1, Coordinate p2, double radius);
 			};
 		}
