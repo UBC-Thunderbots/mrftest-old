6bc246a8ec4ee47ac22865813a23da845a158922
mparizeau <mparizeau@localhost>
2011-05-28 22:58:25 +0000

    Convert pass offensive to the simple play macros

--- software/ai/hl/stp/play/pass_offensive.cpp
+++ software/ai/hl/stp/play/pass_offensive.cpp
@@ -1,91 +1,34 @@
-#include "ai/hl/stp/play/play.h"
-#include "ai/hl/stp/predicates.h"
-#include "ai/hl/stp/tactic/offend.h"
-#include "ai/hl/stp/tactic/defend.h"
-#include "ai/hl/stp/tactic/shoot.h"
-#include "ai/hl/stp/tactic/pass.h"
 #include "ai/hl/stp/tactic/block.h"
-#include "ai/hl/util.h"
-#include "util/dprint.h"
-#include <glibmm.h>
+#include "ai/hl/stp/tactic/pass.h"
+#include "ai/hl/stp/play/simple_play.h"
 
-#include "ai/hl/stp/evaluation/offense.h"
-
-using namespace AI::HL::STP::Play;
-using namespace AI::HL::STP::Tactic;
-using namespace AI::HL::W;
 using AI::HL::STP::Enemy;
 namespace Predicates = AI::HL::STP::Predicates;
 
-using namespace AI::HL::STP::Evaluation;
+BEGIN_PLAY(PassOffensive)
+INVARIANT(Predicates::playtype(world, AI::Common::PlayType::PLAY) && Predicates::our_team_size_at_least(world, 3) && Predicates::their_team_size_at_least(world, 1) && !Predicates::baller_can_shoot(world) && Predicates::baller_can_pass(world))
+APPLICABLE(Predicates::our_ball(world) && (Predicates::ball_midfield(world) || Predicates::ball_in_their_corner(world) || Predicates::ball_in_our_corner(world)))
+DONE(Predicates::goal(world))
+FAIL(Predicates::their_ball(world))
+BEGIN_ASSIGN()
+// GOALIE
+goalie_role.push_back(defend_duo_goalie(world));
 
-namespace {
-	/**
-	 * Condition:
-	 * - ball under team possesion
-	 * - have at least 4 players (one goalie, one passer, one passee, one defender)
-	 *
-	 * Objective:
-	 * - shoot the ball to enemy goal while passing the ball between the passer and passee
-	 */
-	class PassOffensive : public Play {
-		public:
-			PassOffensive(const AI::HL::W::World &world);
+// ROLE 1
+// passer
+roles[0].push_back(passer_shoot(world));
+roles[0].push_back(offend(world));
 
-		private:
-			bool invariant() const;
-			bool applicable() const;
-			bool done() const;
-			bool fail() const;
-			void assign(std::vector<Tactic::Ptr> &goalie_role, std::vector<Tactic::Ptr>(&roles)[4]);
-			const PlayFactory &factory() const;
-	};
+// ROLE 2
+// passee
+roles[1].push_back(passee_move(world));
 
-	PlayFactoryImpl<PassOffensive> factory_instance("Pass Offensive");
+// ROLE 3
+// defend
+roles[2].push_back(defend_duo_defender(world));
 
-	const PlayFactory &PassOffensive::factory() const {
-		return factory_instance;
-	}
-
-	PassOffensive::PassOffensive(const World &world) : Play(world) {
-	}
-
-	bool PassOffensive::invariant() const {
-		return Predicates::playtype(world, AI::Common::PlayType::PLAY) && Predicates::our_team_size_at_least(world, 3) && Predicates::their_team_size_at_least(world, 1) && !Predicates::baller_can_shoot(world) && Predicates::baller_can_pass(world);
-	}
-
-	bool PassOffensive::applicable() const {
-		return Predicates::our_ball(world) && (Predicates::ball_midfield(world) || Predicates::ball_in_their_corner(world) || Predicates::ball_in_our_corner(world));
-	}
-
-	bool PassOffensive::done() const {
-		return Predicates::goal(world);
-	}
-
-	bool PassOffensive::fail() const {
-		return Predicates::their_ball(world);
-	}
-
-	void PassOffensive::assign(std::vector<Tactic::Ptr> &goalie_role, std::vector<Tactic::Ptr>(&roles)[4]) {
-		// GOALIE
-		goalie_role.push_back(defend_duo_goalie(world));
-
-		// ROLE 1
-		// passer
-		roles[0].push_back(passer_shoot(world));
-		roles[0].push_back(offend(world));
-
-		// ROLE 2
-		// passee
-		roles[1].push_back(passee_move(world));
-
-		// ROLE 3
-		// defend
-		roles[2].push_back(defend_duo_defender(world));
-
-		// ROLE 4
-		// offensive support through blocking closest enemy to ball
-		roles[3].push_back(block(world, Enemy::closest_ball(world, 0)));
-	}
-}
-
+// ROLE 4
+// offensive support through blocking closest enemy to ball
+roles[3].push_back(block(world, Enemy::closest_ball(world, 0)));
+END_ASSIGN()
+END_PLAY()
