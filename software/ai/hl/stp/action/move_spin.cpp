8f5508802ca91cab01ea54adc4a17d8f6e933e35
tere11 <tere11@localhost>
2011-06-01 17:10:44 +0000

    modified move spin

--- software/ai/hl/stp/action/move_spin.cpp
+++ software/ai/hl/stp/action/move_spin.cpp
@@ -1,10 +1,16 @@
 #include "ai/flags.h"
 #include "ai/hl/stp/action/move_spin.h"
 #include "geom/angle.h"
+#include "ai/hl/util.h"
 
 using namespace AI::HL::STP;
 
 void AI::HL::STP::Action::move_spin(Player::Ptr player, const Point dest) {
-	player->move(dest, angle_mod(player->orientation() + 0.2), Point());
+
+	// spin faster when you are close to the destination point
+	if ((player->position()-dest).len() < AI::HL::Util::POS_CLOSE)
+		player->move(dest, angle_mod(player->orientation() + 0.4), Point());
+	else 
+		player->move(dest, angle_mod(player->orientation() + 0.2), Point());
 	player->type(AI::Flags::MoveType::NORMAL);
 }
