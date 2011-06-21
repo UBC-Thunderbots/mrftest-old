181e5214c4e9f153f1ac49e3224f6d2d40d14935
JasonChang <jasonchang@localhost>
2011-06-21 23:33:03 +0000

    some minor changes to test_pass

--- software/ai/hl/stp/test_pass.cpp
+++ software/ai/hl/stp/test_pass.cpp
@@ -26,6 +26,8 @@ namespace {
 	DoubleParam passee_hack_dist("Hack to get reciever to move more quickly to intercept pos by modifying dest (meters)", "STP/test_pass", 0.03, 0.0, 1.0);
 	// make better targets
 
+	BoolParam lower_number_passer("lower numbered bot is passer", "STP/test_pass2", true);
+
 	const Point default_targets[] = {
 		Point(1.2, 0),
 		Point(1.5, 0),
@@ -87,6 +89,10 @@ namespace {
 				std::vector<AI::HL::W::Player::Ptr> players = AI::HL::Util::get_players(world.friendly_team());
 				if (players.size() != 2) {
 					return;
+				}
+
+				if (!lower_number_passer){
+					std::reverse(players.begin(), players.end());
 				}
 
 				if (kicked) {
