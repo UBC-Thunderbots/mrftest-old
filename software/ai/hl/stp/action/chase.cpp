04dd9e72b8433a976788e7f277d1cc9c573a94ec
simonsyd <simonsyd@localhost>
2011-07-05 16:59:45 +0000

    chase always face towards goal

--- software/ai/hl/stp/action/chase.cpp
+++ software/ai/hl/stp/action/chase.cpp
@@ -1,4 +1,5 @@
 #include "ai/hl/stp/action/chase.h"
+#include "ai/hl/stp/evaluation/shoot.h"
 #include "ai/flags.h"
 #include "ai/hl/util.h"
 #include "geom/util.h"
@@ -8,12 +9,25 @@
 using namespace AI::HL::STP;
 
 namespace {
-	const double FAST = 100.0;
+
+	BoolParam chase_target_enemy("Chase always face toward enemy goal", "STP/Chase", true);
 }
 
 void AI::HL::STP::Action::chase(const World &world, Player::Ptr player) {
-	player->move(world.ball().position(), (world.ball().position() - player->position()).orientation(), Point());
-	player->type(AI::Flags::MoveType::CATCH);
+	Evaluation::ShootData shoot_data = Evaluation::evaluate_shoot(world, player);
+
+	Point target = world.field().enemy_goal();
+	if (!shoot_data.blocked) {
+		target = shoot_data.target;
+	}
+
+	if (chase_target_enemy) {
+		player->move(target, 0, Point());
+		player->type(AI::Flags::MoveType::CATCH_PIVOT);
+	} else {
+		player->move(world.ball().position(), (world.ball().position() - player->position()).orientation(), Point());
+		player->type(AI::Flags::MoveType::CATCH);
+	}
 	player->prio(AI::Flags::MovePrio::HIGH);
 }
 
