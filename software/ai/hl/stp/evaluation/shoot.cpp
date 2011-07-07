5acbbc0c1cfafd78e339f389a4c9f9af35c40119
byronknoll <byronknoll@localhost>
2011-07-07 12:25:27 +0000

    removed commented out code. stop checking in commented out code.

--- software/ai/hl/stp/evaluation/shoot.cpp
+++ software/ai/hl/stp/evaluation/shoot.cpp
@@ -6,8 +6,6 @@
 
 using namespace AI::HL::STP;
 
-// ShootData EvaluateShoot::compute(AI::HL::W::World &world, AI::HL::W::Player::Ptr player) const {
-
 namespace {
 	DoubleParam reduced_radius("reduced radius for calculating best shot (robot radius ratio)", "STP/Shoot", 0.8, 0.0, 1.0);
 }
@@ -15,13 +13,8 @@ namespace {
 Evaluation::ShootData Evaluation::evaluate_shoot(const World &world, Player::CPtr player) {
 	ShootData data;
 
-	//std::pair<Point, double> shot = AI::HL::Util::calc_best_shot(world, player);
-	//data.reduced_radius = false;
-
-	//if (shot.second == 0) {
 	auto shot = AI::HL::Util::calc_best_shot(world, player, reduced_radius);
 	data.reduced_radius = true;
-	//}
 
 	double ori = (shot.first - player->position()).orientation();
 	double ori_diff = angle_diff(ori, player->orientation());
@@ -43,42 +36,3 @@ Evaluation::ShootData Evaluation::evaluate_shoot(const World &world, Player::CPt
 
 	return data;
 }
-
-/*
-   Evaluation::ShootData Evaluation::evaluate_shoot_target(const World &world, Player::CPtr player, const Point target) {
-   ShootData data;
-
-   std::pair<Point, double> shot = AI::HL::Util::calc_best_shot_target(world, target, player);
-   data.reduced_radius = false;
-
-   if (shot.second == 0) {
-   shot = AI::HL::Util::calc_best_shot_target(world, target, player, reduced_radius);
-   data.reduced_radius = true;
-   }
-
-   double ori = (shot.first - player->position()).orientation();
-   double ori_diff = angle_diff(ori, player->orientation());
-   data.accuracy_diff = radians2degrees(ori_diff - (shot.second / 2));
-
-   if (player->kicker_directional()) {
-   data.accuracy_diff -= 45;
-   }
-
-   data.target = shot.first;
-   data.angle = shot.second;
-   data.can_shoot = (data.accuracy_diff < -shoot_accuracy);
-   data.blocked = (shot.second == 0);
-
-#warning a fix to other parts of the code for now
-if (data.blocked) {
-data.target = world.field().enemy_goal();
-}
-
-return data;
-}
-
-bool Evaluation::can_shoot_target(const World &world, Player::CPtr player, const Point target, bool pass) {
-AI::HL::Util::calc_best_shot_target(world, target, player, pass).second > AI::HL::Util::shoot_accuracy * M_PI / 180.0;
-}
- */
-
