3b593c6e06e473ccfb3f682ea0f4b365b1e98e54
mparizeau <mparizeau@localhost>
2011-07-07 06:34:47 +0000

    In the stop play move the goalie to the correct location

--- software/ai/hl/stp/play/stop.cpp
+++ software/ai/hl/stp/play/stop.cpp
@@ -20,7 +20,7 @@ APPLICABLE(true)
 DONE(false)
 FAIL(false)
 BEGIN_ASSIGN()
-goalie_role.push_back(move(world, Point(-world.field().length() + Robot::MAX_RADIUS, 0.0)));
+goalie_role.push_back(move(world, Point(world.field().friendly_goal().x + Robot::MAX_RADIUS, 0.0)));
 
 // doesn't matter what the playtype we are waiting for is here, we just need an active tactic
 roles[0].push_back(wait_playtype(world, move_stop(world, 0), AI::Common::PlayType::PLAY));
