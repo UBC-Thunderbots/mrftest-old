a65e310a880775a058a4472c1c1b27addc0e5e7b
chead <chead@localhost>
2011-09-30 08:08:25 +0000

    Clean up Tactic a bit.

--- software/ai/hl/stp/tactic/tactic.cpp
+++ software/ai/hl/stp/tactic/tactic.cpp
@@ -1,24 +1,15 @@
 #include "ai/hl/stp/tactic/tactic.h"
 #include "util/param.h"
 #include "util/dprint.h"
-#include <cassert>
+#include <stdexcept>
 
 using namespace AI::HL::STP::Tactic;
 using namespace AI::HL::W;
 
-
-BoolParam enable_tactic_logging("Enable Tactic Log", "AI", true);
-
-Tactic::Tactic(const World &world, bool active) : world(world), active_(active) {
-	if (enable_tactic_logging) {
-		LOG_DEBUG(description());
-	}
-}
-
 bool Tactic::done() const {
 	// if this fails, then you probably have an active tactic that you forgot to implement done()
 	// check to make sure that the signature of done() is correct, with the const at the back.
-	assert(0);
+	throw std::logic_error("Active tactic's function [bool Tactic::done() const] not overridden.");
 }
 
 bool Tactic::fail() const {
@@ -32,12 +23,10 @@ void Tactic::set_player(Player::Ptr p) {
 	}
 }
 
-Glib::ustring Tactic::description() const {
-	return "no description";
+void Tactic::draw_overlay(Cairo::RefPtr<Cairo::Context>) const {
 }
 
-void Tactic::draw_overlay(Cairo::RefPtr<Cairo::Context>) const {
-	// do nothing..
+Tactic::Tactic(const World &world, bool active) : world(world), active_(active) {
 }
 
 void Tactic::player_changed() {
--- software/ai/hl/stp/tactic/tactic.h
+++ software/ai/hl/stp/tactic/tactic.h
@@ -13,8 +13,9 @@ namespace AI {
 		namespace STP {
 			namespace Tactic {
 				/**
-				 * A tactic is a layer in the STP paradigm.
-				 * See STP paper section 4.1, and the wiki.
+				 * \brief A tactic is a layer in the STP paradigm.
+				 *
+				 * See STP paper section 4.1 and the wiki.
 				 *
 				 * A tactic is an action and has verb names.
 				 *
@@ -28,29 +29,33 @@ namespace AI {
 				 */
 				class Tactic : public ByRef {
 					public:
+						/**
+						 * \brief A pointer to a Tactic.
+						 */
 						typedef RefPtr<Tactic> Ptr;
 
 						/**
-						 * An active tactic must override this,
-						 * and provide a condition when this tactic is completed.
+						 * \brief An active tactic must override this and provide a condition when this tactic is completed.
 						 */
 						virtual bool done() const;
 
 						/**
-						 * A tactic can fail if something really bad happens.
+						 * \brief A tactic can fail if something really bad happens.
+						 *
 						 * Only usable by active tactic.
 						 */
 						virtual bool fail() const;
 
 						/**
-						 * Checks if the current tactic is an active tactic.
+						 * \brief Checks if the current tactic is an active tactic.
 						 */
 						bool active() const {
 							return active_;
 						}
 
 						/**
-						 * Selects a player from the set.
+						 * \brief Selects a player from the set.
+						 *
 						 * A non-goalie tactic must implement this function.
 						 *
 						 * \param[in] players a set of players to choose from
@@ -60,24 +65,27 @@ namespace AI {
 						virtual Player::Ptr select(const std::set<Player::Ptr> &players) const = 0;
 
 						/**
-						 * Changes the player associated with this tactic.
+						 * \brief Changes the player associated with this tactic.
 						 */
 						void set_player(Player::Ptr p);
 
 						/**
-						 * The main execution of this tactic.
+						 * \brief The main execution of this tactic.
+						 *
 						 * This function runs every tick.
 						 * A subclass must implement this function.
 						 */
 						virtual void execute() = 0;
 
 						/**
-						 * An optional string description of this tactic.
+						 * \brief Returns a string description of this tactic.
+						 *
+						 * \return a description of the tactic.
 						 */
-						virtual Glib::ustring description() const;
+						virtual Glib::ustring description() const = 0;
 
 						/**
-						 * An optional function to draw extra stuff on the overlay.
+						 * \brief An optional function to draw extra stuff on the overlay.
 						 */
 						virtual void draw_overlay(Cairo::RefPtr<Cairo::Context> context) const;
 
@@ -86,14 +94,14 @@ namespace AI {
 						Player::Ptr player;
 
 						/**
-						 * Constructor for tactic.
+						 * \brief Constructor for tactic.
 						 *
 						 * \param [in] active indicates if this is an active tactic.
 						 */
 						Tactic(const World &world, bool active = false);
 
 						/**
-						 * Triggerred when the player associated changes.
+						 * \brief Triggerred when the player associated changes.
 						 */
 						virtual void player_changed();
 
