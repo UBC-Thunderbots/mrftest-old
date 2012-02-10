9b1dc7abb5134ae25cf622afa270a2c627bc4e1a
chead <chead@localhost>
2012-02-10 01:45:34 +0000

    Fix (?) GCC 4.6 issue.

--- software/util/param.cpp
+++ software/util/param.cpp
@@ -331,7 +331,7 @@ unsigned int NumericParam::fractional_digits() const {
 	}
 }
 
-NumericParam::NumericParam(const char *name, const char *location, double def, double min, double max, bool integer) : Param(name, location), def(def), min(min), max(max), integer(integer), adjustment_(0) {
+NumericParam::NumericParam(const char *name, const char *location, double def, double min, double max, bool integer) : Param(name, location), def(def), min(min), max(max), integer(integer), adjustment_(static_cast<Gtk::Adjustment *>(0)) {
 	if (!(min <= def && def <= max)) {
 		throw std::invalid_argument(Glib::locale_from_utf8(Glib::ustring::compose("Parameter default value for %1/%2 out of valid range.", location, name)));
 	}
