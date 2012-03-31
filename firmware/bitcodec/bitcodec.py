f1633aada451060020cf3930e32ced0c25cadbd3
chead <chead@localhost>
2012-03-31 22:39:57 +0000

    Split a two-variable with statement into two with statements to make it work with archaic Python.

--- firmware/bitcodec/bitcodec.py
+++ firmware/bitcodec/bitcodec.py
@@ -196,146 +196,147 @@ def run():
 	
 	# Generate output.
 	try:
-		with open(cfilename, "wt") as cfile, open(hfilename, "wt") as hfile:
-			hfile.write("/* This file was generated from {}. Do not edit it directly. */\n".format(deffilename))
-			hfile.write("\n")
-			include_guard = outbasename.upper().replace("../", "").replace("/", "_")
-			hfile.write("#ifndef {}_H\n".format(include_guard))
-			hfile.write("#define {}_H\n".format(include_guard))
-			hfile.write("\n")
-			hfile.write("#include <stdbool.h>\n")
-			hfile.write("#include <stdint.h>\n")
-			hfile.write("\n")
-			hfile.write("/**\n")
-			hfile.write(" * \\brief The unpacked representation of a {}\n".format(structname))
-			hfile.write(" */\n")
-			hfile.write("typedef struct {\n")
-			for f in fields:
-				hfile.write("\t{} {};\n".format(f.type(), f.name()))
-			hfile.write("} " + structname + "_t;\n")
-			hfile.write("\n")
-			hfile.write("enum {\n")
-			hfile.write("\t/**\n")
-			hfile.write("\t * \\brief The number of bytes in the packed representation of a {}.\n".format(structname))
-			hfile.write("\t */\n")
-			hfile.write("\t{}_BYTES = {}\n".format(fnprefix.upper(), structbytes))
-			hfile.write("};\n")
-			hfile.write("\n")
-			hfile.write("/**\n")
-			hfile.write(" * \\brief Initializes a {} to its default values.\n".format(structname))
-			hfile.write(" *\n")
-			hfile.write(" * \\param[out] obj the object to initialize.\n")
-			hfile.write(" */\n")
-			hfile.write("void {}_init(__data {}_t *obj);\n".format(fnprefix, structname))
-			hfile.write("\n")
-			hfile.write("/**\n")
-			hfile.write(" * \\brief Packs a {} into its packed binary representation.\n".format(structname))
-			hfile.write(" *\n")
-			hfile.write(" * \\param[out] buffer the buffer into which to store the packed data.\n")
-			hfile.write(" *\n")
-			hfile.write(" * \\param[in] obj the object to encode.\n")
-			hfile.write(" */\n")
-			hfile.write("void {}_encode(__data void *buffer, __data const {}_t *obj);\n".format(fnprefix, structname))
-			hfile.write("\n")
-			hfile.write("/**\n")
-			hfile.write(" * \\brief Unpacks a {} from its packed binary representation.\n".format(structname))
-			hfile.write(" *\n")
-			hfile.write(" * \\param[out] obj the object into which to store the decoded data.\n")
-			hfile.write(" *\n")
-			hfile.write(" * \\param[in] buffer the packed data to decode.\n")
-			hfile.write(" */\n")
-			hfile.write("void {}_decode(__data {}_t *obj, __data const void *buffer);\n".format(fnprefix, structname))
-			hfile.write("\n")
-			hfile.write("#endif\n")
-			hfile.write("\n")
-
-			cfile.write("/* This file was generated from {}. Do not edit it directly. */\n".format(deffilename))
-			cfile.write("\n")
-			cfile.write('#include "{}"\n'.format(hfilename))
-			cfile.write("\n")
-			for f in fields:
-				f.genpreamble(cfile)
-			cfile.write("void {}_init(__data {}_t *obj) {{\n".format(fnprefix, structname))
-			for f in fields:
-				cfile.write("\tobj->{} = {};\n".format(f.name(), f.default()))
-			cfile.write("}\n")
-			cfile.write("\n")
-			cfile.write("void {}_encode(__data void *buffer, __data const {}_t *obj) {{\n".format(fnprefix, structname))
-			cfile.write("\t__data uint8_t *p = buffer;\n")
-			cfile.write("\n")
-			for i in range(0, structbytes):
-				cfile.write("\tp[{}] = (uint8_t) (".format(i))
-				found = False
+		with open(cfilename, "wt") as cfile:
+			with open(hfilename, "wt") as hfile:
+				hfile.write("/* This file was generated from {}. Do not edit it directly. */\n".format(deffilename))
+				hfile.write("\n")
+				include_guard = outbasename.upper().replace("../", "").replace("/", "_")
+				hfile.write("#ifndef {}_H\n".format(include_guard))
+				hfile.write("#define {}_H\n".format(include_guard))
+				hfile.write("\n")
+				hfile.write("#include <stdbool.h>\n")
+				hfile.write("#include <stdint.h>\n")
+				hfile.write("\n")
+				hfile.write("/**\n")
+				hfile.write(" * \\brief The unpacked representation of a {}\n".format(structname))
+				hfile.write(" */\n")
+				hfile.write("typedef struct {\n")
 				for f in fields:
-					if (8 * i < f.offset() + f.length()) and (8 * i + 7 >= f.offset()):
-						if found:
-							cfile.write(" | ")
-						found = True
-						bits_before_this_byte = max(0, 8 * i - f.offset())
-						bits_this_byte_before = max(0, f.offset() - 8 * i)
-						bits_this_byte = min(8 - bits_this_byte_before, f.length() - bits_before_this_byte)
-						bits_after_this_byte = f.length() - bits_before_this_byte - bits_this_byte
-						bits_this_byte_after = 8 - bits_this_byte_before - bits_this_byte
-						shift_distance = bits_after_this_byte - bits_this_byte_after
-						mask = ((1 << (bits_this_byte + bits_this_byte_after)) - 1) & ~((1 << bits_this_byte_after) - 1)
-						if shift_distance > 0:
-							if mask == 0xFF or (not f.spuriousbits() and bits_before_this_byte == 0):
-								cfile.write("((uint8_t) ({} >> {}))".format(f.uval("obj->"), shift_distance))
-							else:
-								cfile.write("((uint8_t) (((uint8_t) ({} >> {})) & (uint8_t) 0x{:02X}))".format(f.uval("obj->"), shift_distance, mask))
-						elif shift_distance < 0:
-							if mask & 0x80 or not f.spuriousbits():
-								cfile.write("((uint8_t) (((uint8_t) {}) << {}))".format(f.uval("obj->"), -shift_distance))
-							else:
-								cfile.write("((uint8_t) ((((uint8_t) {}) << {}) & ((uint8_t) 0x{:02X})))".format(f.uval("obj->"), -shift_distance, mask))
-						else:
-							if mask & 0x80 or not f.spuriousbits():
-								assert mask == 0xFF or not f.spuriousbits()
-								cfile.write("((uint8_t) {})".format(f.uval("obj->")))
-							else:
-								cfile.write("(((uint8_t) {}) & ((uint8_t) 0x{:02X}))".format(f.uval("obj->"), mask))
-				if not found:
-					cfile.write("0")
-				cfile.write(");\n")
-			cfile.write("}\n")
-			cfile.write("\n")
-			cfile.write("void {}_decode(__data {}_t *obj, __data const void *buffer) {{\n".format(fnprefix, structname))
-			cfile.write("\t__data const uint8_t *p = buffer;\n")
-			cfile.write("\n")
-			for f in fields:
-				cfile.write("\tobj->{} = {}(({}) (".format(f.name(), f.postdecodefn(), f.utype()))
-				first = True
+					hfile.write("\t{} {};\n".format(f.type(), f.name()))
+				hfile.write("} " + structname + "_t;\n")
+				hfile.write("\n")
+				hfile.write("enum {\n")
+				hfile.write("\t/**\n")
+				hfile.write("\t * \\brief The number of bytes in the packed representation of a {}.\n".format(structname))
+				hfile.write("\t */\n")
+				hfile.write("\t{}_BYTES = {}\n".format(fnprefix.upper(), structbytes))
+				hfile.write("};\n")
+				hfile.write("\n")
+				hfile.write("/**\n")
+				hfile.write(" * \\brief Initializes a {} to its default values.\n".format(structname))
+				hfile.write(" *\n")
+				hfile.write(" * \\param[out] obj the object to initialize.\n")
+				hfile.write(" */\n")
+				hfile.write("void {}_init(__data {}_t *obj);\n".format(fnprefix, structname))
+				hfile.write("\n")
+				hfile.write("/**\n")
+				hfile.write(" * \\brief Packs a {} into its packed binary representation.\n".format(structname))
+				hfile.write(" *\n")
+				hfile.write(" * \\param[out] buffer the buffer into which to store the packed data.\n")
+				hfile.write(" *\n")
+				hfile.write(" * \\param[in] obj the object to encode.\n")
+				hfile.write(" */\n")
+				hfile.write("void {}_encode(__data void *buffer, __data const {}_t *obj);\n".format(fnprefix, structname))
+				hfile.write("\n")
+				hfile.write("/**\n")
+				hfile.write(" * \\brief Unpacks a {} from its packed binary representation.\n".format(structname))
+				hfile.write(" *\n")
+				hfile.write(" * \\param[out] obj the object into which to store the decoded data.\n")
+				hfile.write(" *\n")
+				hfile.write(" * \\param[in] buffer the packed data to decode.\n")
+				hfile.write(" */\n")
+				hfile.write("void {}_decode(__data {}_t *obj, __data const void *buffer);\n".format(fnprefix, structname))
+				hfile.write("\n")
+				hfile.write("#endif\n")
+				hfile.write("\n")
+
+				cfile.write("/* This file was generated from {}. Do not edit it directly. */\n".format(deffilename))
+				cfile.write("\n")
+				cfile.write('#include "{}"\n'.format(hfilename))
+				cfile.write("\n")
+				for f in fields:
+					f.genpreamble(cfile)
+				cfile.write("void {}_init(__data {}_t *obj) {{\n".format(fnprefix, structname))
+				for f in fields:
+					cfile.write("\tobj->{} = {};\n".format(f.name(), f.default()))
+				cfile.write("}\n")
+				cfile.write("\n")
+				cfile.write("void {}_encode(__data void *buffer, __data const {}_t *obj) {{\n".format(fnprefix, structname))
+				cfile.write("\t__data uint8_t *p = buffer;\n")
+				cfile.write("\n")
 				for i in range(0, structbytes):
-					if (8 * i < f.offset() + f.length()) and (8 * i + 7 >= f.offset()):
-						if not first:
-							cfile.write(" | ")
-						first = False
-						bits_before_this_byte = max(0, 8 * i - f.offset())
-						bits_this_byte_before = max(0, f.offset() - 8 * i)
-						bits_this_byte = min(8 - bits_this_byte_before, f.length() - bits_before_this_byte)
-						bits_after_this_byte = f.length() - bits_before_this_byte - bits_this_byte
-						bits_this_byte_after = 8 - bits_this_byte_before - bits_this_byte
-						shift_distance = bits_after_this_byte - bits_this_byte_after
-						mask = ((1 << (bits_this_byte + bits_this_byte_after)) - 1) & ~((1 << bits_this_byte_after) - 1)
-						if shift_distance > 0:
-							if mask == 0xFF:
-								cfile.write("(({}) ((({}) p[{}]) << {}))".format(f.utype(), f.utype(), i, shift_distance))
+					cfile.write("\tp[{}] = (uint8_t) (".format(i))
+					found = False
+					for f in fields:
+						if (8 * i < f.offset() + f.length()) and (8 * i + 7 >= f.offset()):
+							if found:
+								cfile.write(" | ")
+							found = True
+							bits_before_this_byte = max(0, 8 * i - f.offset())
+							bits_this_byte_before = max(0, f.offset() - 8 * i)
+							bits_this_byte = min(8 - bits_this_byte_before, f.length() - bits_before_this_byte)
+							bits_after_this_byte = f.length() - bits_before_this_byte - bits_this_byte
+							bits_this_byte_after = 8 - bits_this_byte_before - bits_this_byte
+							shift_distance = bits_after_this_byte - bits_this_byte_after
+							mask = ((1 << (bits_this_byte + bits_this_byte_after)) - 1) & ~((1 << bits_this_byte_after) - 1)
+							if shift_distance > 0:
+								if mask == 0xFF or (not f.spuriousbits() and bits_before_this_byte == 0):
+									cfile.write("((uint8_t) ({} >> {}))".format(f.uval("obj->"), shift_distance))
+								else:
+									cfile.write("((uint8_t) (((uint8_t) ({} >> {})) & (uint8_t) 0x{:02X}))".format(f.uval("obj->"), shift_distance, mask))
+							elif shift_distance < 0:
+								if mask & 0x80 or not f.spuriousbits():
+									cfile.write("((uint8_t) (((uint8_t) {}) << {}))".format(f.uval("obj->"), -shift_distance))
+								else:
+									cfile.write("((uint8_t) ((((uint8_t) {}) << {}) & ((uint8_t) 0x{:02X})))".format(f.uval("obj->"), -shift_distance, mask))
 							else:
-								cfile.write("(({}) ((({}) (p[{}] & (uint8_t) 0x{:02X})) << {}))".format(f.utype(), f.utype(), i, mask, shift_distance))
-						elif shift_distance < 0:
-							if mask & 0x80:
-								cfile.write("((uint8_t) (p[{}] >> {}))".format(i, -shift_distance))
+								if mask & 0x80 or not f.spuriousbits():
+									assert mask == 0xFF or not f.spuriousbits()
+									cfile.write("((uint8_t) {})".format(f.uval("obj->")))
+								else:
+									cfile.write("(((uint8_t) {}) & ((uint8_t) 0x{:02X}))".format(f.uval("obj->"), mask))
+					if not found:
+						cfile.write("0")
+					cfile.write(");\n")
+				cfile.write("}\n")
+				cfile.write("\n")
+				cfile.write("void {}_decode(__data {}_t *obj, __data const void *buffer) {{\n".format(fnprefix, structname))
+				cfile.write("\t__data const uint8_t *p = buffer;\n")
+				cfile.write("\n")
+				for f in fields:
+					cfile.write("\tobj->{} = {}(({}) (".format(f.name(), f.postdecodefn(), f.utype()))
+					first = True
+					for i in range(0, structbytes):
+						if (8 * i < f.offset() + f.length()) and (8 * i + 7 >= f.offset()):
+							if not first:
+								cfile.write(" | ")
+							first = False
+							bits_before_this_byte = max(0, 8 * i - f.offset())
+							bits_this_byte_before = max(0, f.offset() - 8 * i)
+							bits_this_byte = min(8 - bits_this_byte_before, f.length() - bits_before_this_byte)
+							bits_after_this_byte = f.length() - bits_before_this_byte - bits_this_byte
+							bits_this_byte_after = 8 - bits_this_byte_before - bits_this_byte
+							shift_distance = bits_after_this_byte - bits_this_byte_after
+							mask = ((1 << (bits_this_byte + bits_this_byte_after)) - 1) & ~((1 << bits_this_byte_after) - 1)
+							if shift_distance > 0:
+								if mask == 0xFF:
+									cfile.write("(({}) ((({}) p[{}]) << {}))".format(f.utype(), f.utype(), i, shift_distance))
+								else:
+									cfile.write("(({}) ((({}) (p[{}] & (uint8_t) 0x{:02X})) << {}))".format(f.utype(), f.utype(), i, mask, shift_distance))
+							elif shift_distance < 0:
+								if mask & 0x80:
+									cfile.write("((uint8_t) (p[{}] >> {}))".format(i, -shift_distance))
+								else:
+									cfile.write("((uint8_t) (((uint8_t) (p[{}] & (uint8_t) 0x{:02X})) >> {}))".format(i, mask, -shift_distance))
 							else:
-								cfile.write("((uint8_t) (((uint8_t) (p[{}] & (uint8_t) 0x{:02X})) >> {}))".format(i, mask, -shift_distance))
-						else:
-							if mask & 0x80:
-								assert mask == 0xFF
-								cfile.write("p[{}]".format(i))
-							else:
-								cfile.write("((uint8_t) (p[{}] & (uint8_t) 0x{:02X}))".format(i, mask))
-				cfile.write("));\n")
-			cfile.write("}\n")
-			cfile.write("\n")
+								if mask & 0x80:
+									assert mask == 0xFF
+									cfile.write("p[{}]".format(i))
+								else:
+									cfile.write("((uint8_t) (p[{}] & (uint8_t) 0x{:02X}))".format(i, mask))
+					cfile.write("));\n")
+				cfile.write("}\n")
+				cfile.write("\n")
 	except:
 		# On error, remove output files.
 		os.remove(cfilename)
