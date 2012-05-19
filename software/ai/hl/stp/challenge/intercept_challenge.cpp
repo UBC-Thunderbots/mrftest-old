e6ff5bc4de931634d76e525690bd8ea0a442316c
jhj2984 <jhj2984@localhost>
2012-05-19 21:29:57 +0000

    changes to intercept_challenge.cpp

--- software/ai/hl/stp/challenge/intercept_challenge.cpp
+++ software/ai/hl/stp/challenge/intercept_challenge.cpp
@@ -2,7 +2,11 @@
 #include "ai/flags.h"
 #include "ai/hl/stp/world.h"
 #include "ai/hl/stp/action/intercept.h"
+#include "ai/hl/stp/action/ram.h"
 #include "ai/hl/stp/predicates.h"
+#include "ai/hl/stp/evaluation/enemy.h"
+#include "ai/hl/stp/evaluation/pass.h"
+#include "ai/hl/stp/evaluation/ball.h"
 
 using namespace AI::HL;
 using namespace AI::HL::W;
@@ -11,39 +15,45 @@ using namespace AI::HL::STP;
 using namespace AI::HL::STP::Predicates;
 
 namespace {
-	class InterceptChallenge : public HighLevel {
-		public:
-			InterceptChallenge(World &world) : world(world) {
-			}
+class InterceptChallenge: public HighLevel {
+public:
+	InterceptChallenge(World &world) :
+			world(world) {
+	}
 
-		private:
-			World &world;
+private:
+	World &world;
 
-			HighLevelFactory &factory() const;
+	HighLevelFactory &factory() const;
 
-			Gtk::Widget *ui_controls() {
-				return 0;
-			}
+	Gtk::Widget *ui_controls() {
+		return 0;
+	}
 
-			void tick() {
-				FriendlyTeam &friendly = world.friendly_team();
-				if (friendly.size() != 2) {
-					return;
-				}
+	void tick() {
+		FriendlyTeam &friendly = world.friendly_team();
+		if (friendly.size() != 2) {
+			return;
+		}
 
-				if (AI::HL::STP::Predicates::our_ball(world)){
-					// do something
-				}
+		Player::Ptr player = friendly.get(0);
+		player->autokick(AI::HL::STP::BALL_MAX_SPEED);
 
-				Player::Ptr player = friendly.get(0);
-				player->autokick(AI::HL::STP::BALL_MAX_SPEED);
-				Action::intercept(player, world.field().enemy_goal());
+		Player::Ptr player2 = friendly.get(1);
+		player2->autokick(AI::HL::STP::BALL_MAX_SPEED);
 
-				Player::Ptr player2 = friendly.get(1);
-				player2->autokick(AI::HL::STP::BALL_MAX_SPEED);
-				Action::intercept(player2, world.field().enemy_goal());
-			}
-	};
+		if (AI::HL::STP::Predicates::their_ball(world)) {
+			//const Robot::Ptr baller = Evaluation::calc_enemy_baller(world);
+
+
+		} else if (!AI::HL::STP::Predicates::their_ball(world)
+				&& !AI::HL::STP::Predicates::our_ball(world)) {
+			Action::ram(world, player);
+			Action::ram(world, player2);
+		}
+
+	}
+};
 }
 
 HIGH_LEVEL_REGISTER(InterceptChallenge)
