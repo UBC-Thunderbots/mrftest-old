0c996b328334f36d1782cfaa8a540ffa2ca66ec0
chead <chead@localhost>
2012-06-01 01:41:36 +0000

    Fix screwup

--- firmware/dongle/getabi.sh
+++ firmware/dongle/getabi.sh
@@ -3,7 +3,7 @@ if which arm-elf-gcc &>/dev/null; then
 	echo arm-elf
 	exit 0
 elif which arm-linux-gnueabi-gcc &>/dev/null; then
-	echo arm-elf-linux-gnueabi
+	echo arm-linux-gnueabi
 	exit 0
 else
 	echo "No ARM toolchain found" >&2
