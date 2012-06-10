c84dd54a9ca06f735dffac980234fcd9fb1068f5
chead <chead@localhost>
2012-06-10 04:14:49 +0000

    Add MRF24J40 library

---
+++ firmware/main/mrf.c
@@ -0,0 +1,115 @@
+#include "io.h"
+#include "mrf.h"
+#include "sleep.h"
+
+static inline void sleep_50ns(void) {
+	asm volatile("nop");
+	asm volatile("nop");
+}
+
+uint8_t mrf_read_short(mrf_reg_short_t reg) {
+	outb(MRF_CTL, 0x04); // Assert chip select
+	sleep_50ns();
+	outb(MRF_DATA, reg << 1); // Send first byte (read command plus address)
+	while (inb(MRF_CTL) & 0x01); // Wait for bus idle
+	outb(MRF_DATA, 0x00); // Send second byte (dummy for receive)
+	while (inb(MRF_CTL) & 0x01); // Wait for bus idle
+	sleep_50ns();
+	outb(MRF_CTL, 0x06); // Deassert chip select
+	return inb(MRF_DATA); // Return read data
+}
+
+void mrf_write_short(mrf_reg_short_t reg, uint8_t value) {
+	outb(MRF_CTL, 0x04); // Assert chip select
+	sleep_50ns();
+	outb(MRF_DATA, (reg << 1) | 0x01); // Send first byte (write command plus address)
+	while (inb(MRF_CTL) & 0x01); // Wait for bus idle
+	outb(MRF_DATA, value); // Send second byte (value)
+	while (inb(MRF_CTL) & 0x01); // Wait for bus idle
+	sleep_50ns();
+	outb(MRF_CTL, 0x06); // Deassert chip select
+}
+
+uint8_t mrf_read_long(mrf_reg_long_t reg) {
+	outb(MRF_CTL, 0x04); // Assert chip select
+	sleep_50ns();
+	outb(MRF_DATA, (reg >> 3) | 0x80); // Send first byte (start of address)
+	while (inb(MRF_CTL) & 0x01); // Wait for bus idle
+	outb(MRF_DATA, reg << 5); // Send second byte (rest of address plus read command)
+	while (inb(MRF_CTL) & 0x01); // Wait for bus idle
+	outb(MRF_DATA, 0x00); // Send third byte (dummy for receive)
+	while (inb(MRF_CTL) & 0x01); // Wait for bus idle
+	sleep_50ns();
+	outb(MRF_CTL, 0x06); // Deassert chip select
+	return inb(MRF_DATA); // Return read data
+}
+
+void mrf_write_long(mrf_reg_long_t reg, uint8_t value) {
+	outb(MRF_CTL, 0x04); // Assert chip select
+	sleep_50ns();
+	outb(MRF_DATA, (reg >> 3) | 0x80); // Send first byte (start of address)
+	while (inb(MRF_CTL) & 0x01); // Wait for bus idle
+	outb(MRF_DATA, (reg << 5) | 0x10); // Send second byte (rest of address plus write command)
+	while (inb(MRF_CTL) & 0x01); // Wait for bus idle
+	outb(MRF_DATA, value); // Send third byte (value)
+	while (inb(MRF_CTL) & 0x01); // Wait for bus idle
+	sleep_50ns();
+	outb(MRF_CTL, 0x06); // Deassert chip select
+}
+
+void mrf_common_init(uint8_t channel, bool symbol_rate, uint16_t pan_id, uint64_t mac_address) {
+	mrf_write_short(MRF_REG_SHORT_SOFTRST, 0x07);
+	mrf_write_short(MRF_REG_SHORT_PACON2, 0x98);
+	mrf_write_short(MRF_REG_SHORT_TXSTBL, 0x95);
+	mrf_write_long(MRF_REG_LONG_RFCON0, 0x03);
+	mrf_write_long(MRF_REG_LONG_RFCON1, 0x02);
+	mrf_write_long(MRF_REG_LONG_RFCON2, 0x80);
+	mrf_write_long(MRF_REG_LONG_RFCON6, 0x90);
+	mrf_write_long(MRF_REG_LONG_RFCON7, 0x80);
+	mrf_write_long(MRF_REG_LONG_RFCON8, 0x10);
+	mrf_write_long(MRF_REG_LONG_SLPCON0, 0x03);
+	mrf_write_long(MRF_REG_LONG_SLPCON1, 0x21);
+	mrf_write_short(MRF_REG_SHORT_RXFLUSH, 0x61);
+	mrf_write_short(MRF_REG_SHORT_BBREG2, 0x80);
+	mrf_write_short(MRF_REG_SHORT_CCAEDTH, 0x60);
+	mrf_write_short(MRF_REG_SHORT_BBREG6, 0x40);
+	mrf_write_long(MRF_REG_LONG_RFCON0, ((channel - 0x0B) << 8) | 0x03);
+	mrf_write_short(MRF_REG_SHORT_RFCTL, 0x04);
+	mrf_write_short(MRF_REG_SHORT_RFCTL, 0x00);
+	sleep_short();
+	if (symbol_rate) {
+		mrf_write_short(MRF_REG_SHORT_BBREG0, 0x01);
+		mrf_write_short(MRF_REG_SHORT_BBREG3, 0x34);
+		mrf_write_short(MRF_REG_SHORT_BBREG4, 0x5C);
+		mrf_write_short(MRF_REG_SHORT_SOFTRST, 0x02);
+	}
+	mrf_write_short(MRF_REG_SHORT_PANIDL, pan_id);
+	mrf_write_short(MRF_REG_SHORT_PANIDH, pan_id >> 8);
+	mrf_write_short(MRF_REG_SHORT_EADR0, mac_address);
+	mrf_write_short(MRF_REG_SHORT_EADR1, mac_address >> 8);
+	mrf_write_short(MRF_REG_SHORT_EADR2, mac_address >> 16);
+	mrf_write_short(MRF_REG_SHORT_EADR3, mac_address >> 24);
+	mrf_write_short(MRF_REG_SHORT_EADR4, mac_address >> 32);
+	mrf_write_short(MRF_REG_SHORT_EADR5, mac_address >> 40);
+	mrf_write_short(MRF_REG_SHORT_EADR6, mac_address >> 48);
+	mrf_write_short(MRF_REG_SHORT_EADR7, mac_address >> 56);
+	mrf_analogue_off();
+	mrf_write_short(MRF_REG_SHORT_TRISGPIO, 0x3F);
+}
+
+void mrf_analogue_off(void) {
+	mrf_write_short(MRF_REG_SHORT_GPIO, 0x00);
+	mrf_write_long(MRF_REG_LONG_TESTMODE, 0x08);
+}
+
+void mrf_analogue_rx(void) {
+	mrf_write_short(MRF_REG_SHORT_GPIO, 0x04);
+	mrf_write_long(MRF_REG_LONG_TESTMODE, 0x08);
+}
+
+void mrf_analogue_txrx(void) {
+	mrf_write_short(MRF_REG_SHORT_GPIO, 0x00);
+	mrf_write_long(MRF_REG_LONG_TESTMODE, 0x0F);
+}
+
+
---
+++ firmware/main/mrf.h
@@ -0,0 +1,209 @@
+#ifndef MRF_H
+#define MRF_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "io.h"
+
+/**
+ * \brief The possible short register addresses
+ */
+typedef enum {
+	MRF_REG_SHORT_RXMCR,
+	MRF_REG_SHORT_PANIDL,
+	MRF_REG_SHORT_PANIDH,
+	MRF_REG_SHORT_SADRL,
+	MRF_REG_SHORT_SADRH,
+	MRF_REG_SHORT_EADR0,
+	MRF_REG_SHORT_EADR1,
+	MRF_REG_SHORT_EADR2,
+	MRF_REG_SHORT_EADR3,
+	MRF_REG_SHORT_EADR4,
+	MRF_REG_SHORT_EADR5,
+	MRF_REG_SHORT_EADR6,
+	MRF_REG_SHORT_EADR7,
+	MRF_REG_SHORT_RXFLUSH,
+	MRF_REG_SHORT_ORDER = 0x10,
+	MRF_REG_SHORT_TXMCR,
+	MRF_REG_SHORT_ACKTMOUT,
+	MRF_REG_SHORT_ESLOTG1,
+	MRF_REG_SHORT_SYMTICKL,
+	MRF_REG_SHORT_SYMTICKH,
+	MRF_REG_SHORT_PACON0,
+	MRF_REG_SHORT_PACON1,
+	MRF_REG_SHORT_PACON2,
+	MRF_REG_SHORT_TXBCON0 = 0x1A,
+	MRF_REG_SHORT_TXNCON,
+	MRF_REG_SHORT_TXG1CON,
+	MRF_REG_SHORT_TXG2CON,
+	MRF_REG_SHORT_ESLOTG23,
+	MRF_REG_SHORT_ESLOTG45,
+	MRF_REG_SHORT_ESLOTG67,
+	MRF_REG_SHORT_TXPEND,
+	MRF_REG_SHORT_WAKECON,
+	MRF_REG_SHORT_FRMOFFSET,
+	MRF_REG_SHORT_TXSTAT,
+	MRF_REG_SHORT_TXBCON1,
+	MRF_REG_SHORT_GATECLK,
+	MRF_REG_SHORT_TXTIME,
+	MRF_REG_SHORT_HSYMTMRL,
+	MRF_REG_SHORT_HSYMTMRH,
+	MRF_REG_SHORT_SOFTRST,
+	MRF_REG_SHORT_SECCON0 = 0x2C,
+	MRF_REG_SHORT_SECCON1,
+	MRF_REG_SHORT_TXSTBL,
+	MRF_REG_SHORT_RXSR = 0x30,
+	MRF_REG_SHORT_INTSTAT,
+	MRF_REG_SHORT_INTCON,
+	MRF_REG_SHORT_GPIO,
+	MRF_REG_SHORT_TRISGPIO,
+	MRF_REG_SHORT_SLPACK,
+	MRF_REG_SHORT_RFCTL,
+	MRF_REG_SHORT_SECCR2,
+	MRF_REG_SHORT_BBREG0,
+	MRF_REG_SHORT_BBREG1,
+	MRF_REG_SHORT_BBREG2,
+	MRF_REG_SHORT_BBREG3,
+	MRF_REG_SHORT_BBREG4,
+	MRF_REG_SHORT_BBREG6 = 0x3E,
+	MRF_REG_SHORT_CCAEDTH,
+} mrf_reg_short_t;
+
+typedef enum {
+	MRF_REG_LONG_TXNFIFO = 0x000,
+	MRF_REG_LONG_TXBFIFO = 0x080,
+	MRF_REG_LONG_TXGTS1FIFO = 0x100,
+	MRF_REG_LONG_TXGTS2FIFO = 0x180,
+	MRF_REG_LONG_RFCON0 = 0x200,
+	MRF_REG_LONG_RFCON1,
+	MRF_REG_LONG_RFCON2,
+	MRF_REG_LONG_RFCON3,
+	MRF_REG_LONG_RFCON5 = 0x205,
+	MRF_REG_LONG_RFCON6,
+	MRF_REG_LONG_RFCON7,
+	MRF_REG_LONG_RFCON8,
+	MRF_REG_LONG_SLPCAL0,
+	MRF_REG_LONG_SLPCAL1,
+	MRF_REG_LONG_SLPCAL2,
+	MRF_REG_LONG_RFSTATE = 0x20F,
+	MRF_REG_LONG_RSSI,
+	MRF_REG_LONG_SLPCON0,
+	MRF_REG_LONG_SLPCON1 = 0x220,
+	MRF_REG_LONG_WAKETIMEL = 0x222,
+	MRF_REG_LONG_WAKETIMEH,
+	MRF_REG_LONG_REMCNTL,
+	MRF_REG_LONG_REMCNTH,
+	MRF_REG_LONG_MAINCNT0,
+	MRF_REG_LONG_MAINCNT1,
+	MRF_REG_LONG_MAINCNT2,
+	MRF_REG_LONG_MAINCNT3,
+	MRF_REG_LONG_TESTMODE = 0x22F,
+	MRF_REG_LONG_ASSOEADR0,
+	MRF_REG_LONG_ASSOEADR1,
+	MRF_REG_LONG_ASSOEADR2,
+	MRF_REG_LONG_ASSOEADR3,
+	MRF_REG_LONG_ASSOEADR4,
+	MRF_REG_LONG_ASSOEADR5,
+	MRF_REG_LONG_ASSOEADR6,
+	MRF_REG_LONG_ASSOEADR7,
+	MRF_REG_LONG_ASSOSADR0,
+	MRF_REG_LONG_ASSOSADR1,
+	MRF_REG_LONG_UPNONCE0 = 0x240,
+	MRF_REG_LONG_UPNONCE1,
+	MRF_REG_LONG_UPNONCE2,
+	MRF_REG_LONG_UPNONCE3,
+	MRF_REG_LONG_UPNONCE4,
+	MRF_REG_LONG_UPNONCE5,
+	MRF_REG_LONG_UPNONCE6,
+	MRF_REG_LONG_UPNONCE7,
+	MRF_REG_LONG_UPNONCE8,
+	MRF_REG_LONG_UPNONCE9,
+	MRF_REG_LONG_UPNONCE10,
+	MRF_REG_LONG_UPNONCE11,
+	MRF_REG_LONG_UPNONCE12,
+	MRF_REG_LONG_KEYFIFO = 0x280,
+	MRF_REG_LONG_RXFIFO = 0x300,
+} mrf_reg_long_t;
+
+/**
+ * \brief Initializes the interface to the radio and places the radio in reset
+ */
+static inline void mrf_init(void) {
+	outb(MRF_CTL, 0x02);
+}
+
+/**
+ * \brief Releases the radio from reset
+ */
+static inline void mrf_release_reset(void) {
+	outb(MRF_CTL, 0x06);
+}
+
+/**
+ * \brief Checks the radio's interrupt line
+ *
+ * \return \c true if the interrupt line is high, or \c false if low
+ */
+static inline bool mrf_get_interrupt(void) {
+	return !!(inb(MRF_CTL) & 0x10);
+}
+
+/**
+ * \brief Reads a short-address register
+ *
+ * \param[in] reg the register to read
+ *
+ * \return the register's value
+ */
+uint8_t mrf_read_short(mrf_reg_short_t reg);
+
+/**
+ * \brief Writes a short-address register
+ *
+ * \param[in] reg the register to write
+ *
+ * \param[in] value the value to write
+ */
+void mrf_write_short(mrf_reg_short_t reg, uint8_t value);
+
+/**
+ * \brief Reads a long-address register
+ *
+ * \param[in] reg the register to read
+ *
+ * \return the register's value
+ */
+uint8_t mrf_read_long(mrf_reg_long_t reg);
+
+/**
+ * \brief Writes a long-address register
+ *
+ * \param[in] reg the register to write
+ *
+ * \param[in] value the value to write
+ */
+void mrf_write_long(mrf_reg_long_t reg, uint8_t value);
+
+/**
+ * \brief Performs common initialiation of the radio based on the configuration parameters
+ */
+void mrf_common_init(uint8_t channel, bool symbol_rate, uint16_t pan_id, uint64_t mac_address);
+
+/**
+ * \brief Sets the analogue path on the MRF to consume minimum power and not allow any communication
+ */
+void mrf_analogue_off(void);
+
+/**
+ * \brief Sets the analogue path on the MRF to allow reception but not transmission
+ */
+void mrf_analogue_rx(void);
+
+/**
+ * \brief Sets the analogue path on the MRF to allow both transmission and reception
+ */
+void mrf_analogue_txrx(void);
+
+#endif
+
+
