87089b697fc0926860449b66f725f3adbbc2538d
binaryblade <binaryblade@localhost>
2012-06-10 03:21:34 +0000

    added some motor helper functions

---
+++ firmware/main/motor.c
@@ -0,0 +1,67 @@
+#include "motor.h"
+#include "io.h"
+
+void motor_scram() {
+	wheel_scram();
+	motor_scram();
+}
+
+uint8_t read_wheel_pwm(uint8_t num) {
+	switch(num) {
+		case 0:
+			return inb(WHEEL0_PWM);
+		case 1:
+			return inb(WHEEL1_PWM);
+		case 2:
+			return inb(WHEEL2_PWM);
+		case 3:
+			return inb(WHEEL3_PWM);
+		default:
+			return 0xFF;
+	}
+}
+
+void write_wheel_pwm(uint8_t num, uint8_t value) {
+	switch(num) {
+		case 0:
+			outb(WHEEL0_PWM,value);
+			break;
+		case 1:
+			outb(WHEEL1_PWM,value);
+			break;
+		case 2:
+			outb(WHEEL2_PWM,value);
+			break;
+		case 3:
+			outb(WHEEL3_PWM,value);
+			break;
+	}
+}
+
+void wheel_scram() {
+	outb(WHEEL_CTL,0x00);
+	for(uint8_t index=0; index < 4; index++) {
+		write_wheel_pwm(index,0x00);
+	}
+}
+
+void set_wheel(uint8_t wheel_num, direction_t direction, uint8_t pwm_level) {
+	if(wheel_num <= 3) {
+		uint8_t readback = inb(WHEEL_CTL);
+		readback |= (readback & ~(0x03 << (wheel_num*2))) | (direction << (wheel_num*2));
+		write_wheel_pwm(wheel_num,pwm_level);
+		outb(WHEEL_CTL,readback);
+	}
+}
+
+void dribbler_scram() {
+	outb(DRIBBLER_CTL, 0x00);
+	outb(DRIBBLER_PWM,0x00);
+}
+
+void set_dribbler(direction_t direction, uint8_t pwm_level) {
+	outb(DRIBBLER_CTL,direction);
+	outb(DRIBBLER_PWM,pwm_level);
+}
+
+
---
+++ firmware/main/motor.h
@@ -0,0 +1,82 @@
+#ifndef MOTORS_H
+#define MOTORS_H
+
+/**
+ * \file
+ *
+ * \brief file with motor helper functions
+ */
+
+
+#include <stdint.h>
+
+/**
+ * \brief possible motor control states
+ */
+typedef enum {
+	FLOAT,
+	BRAKE,
+	FORWARD,
+	BACKWARD
+} direction_t;
+
+/**
+ * \brief motor safety switch
+ *
+ * sets all motor pwms to 0 and floats direction
+ */
+void motor_scram();
+
+/**
+ * \brief wheel safety switch 
+ */
+void wheel_scram();
+
+/**
+ * \brief wheel control interface
+ *
+ * \param[in] wheel_num wheel index from 0
+ *
+ * \param[in] direction motor state to set to
+ *
+ * \param[in] pwm_level pwm level to set to
+ *
+ */
+void set_wheel(uint8_t wheel_num, direction_t direction, uint8_t pwm_level);
+
+/**
+ * \brief dribbler safety switch
+ */
+void dribbler_scram();
+
+/** 
+ * \brief set the dribbler direction and level
+ *
+ * \param[in] direction dribbler direction to set
+ *
+ * \param[in] pwm_level level to set dribbler speed
+ */
+void set_dribbler(direction_t direction, uint8_t pwm_level);
+
+/**
+ * \brief gets current pwm value of a motor
+ *
+ * \param[in] wheel_num wheel number to read current pwm value
+ *
+ * \return current pwm of selected wheel
+ *
+ * Just a helper function to allow for computed indexing 
+ */
+uint8_t read_wheel_pwm(uint8_t wheel_num);
+
+/**
+ * \brief set wheel pwm value
+ *
+ * \param[in] wheel_num index of wheel to control
+ *
+ * \param[in] pwm_level pwm value to set
+ *
+ * Sets motor to specific pwm value (helper for computed indexing)
+ */
+void write_wheel_pwm(uint8_t wheel_num, uint8_t pwm_level);
+#endif
