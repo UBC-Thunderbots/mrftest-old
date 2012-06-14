3b9492fc356b760989177890b139b316c05b9738
bluemonday <bluemonday@localhost>
2012-06-14 03:30:59 +0000

    Robots are now unsynchronized to take into account possible robot malfunctions

--- software/ai/hl/pass_and_shoot_challenge/pass_and_shoot_challenge_enemy_tester.cpp
+++ software/ai/hl/pass_and_shoot_challenge/pass_and_shoot_challenge_enemy_tester.cpp
@@ -17,116 +17,133 @@ namespace {
   const unsigned int min_team_size = 4;
   const double epsilon = 0.3;
 
-  Point bot0_left(-2.0, 0.6);
-  Point bot0_right(0, 0.6);
+  Point bot0_left(-2.8, 0.6);
+  Point bot0_right(-.2, 0.6);
 
-  Point bot1_left(0.5, 0.6);
-  Point bot1_right(2.5, 0.6);
+  Point bot1_left(.2, 0.6);
+  Point bot1_right(2.8, 0.6);
 
   Point bot2_left(-2.8, -0.6);
-  Point bot2_right(-0.8, -0.6);
+  Point bot2_right(-.2, -0.6);
 
-  Point bot3_left(0, -0.6);
-  Point bot3_right(2.0, -0.6);
+  Point bot3_left(.2, -0.6);
+  Point bot3_right(2.8, -0.6);  
   
   enum state{
     INITIAL,
-    ROBOTS_ON_SIDE1,
-    ROBOTS_ON_SIDE2
+    MOVE
   };
 
 };
 
-class PASCHL_ENEMY : public HighLevel {
+class PASCHL_ENEMIES : public HighLevel {
 public:
-	PASCHL_ENEMY(World &world) : world(world) {
+  PASCHL_ENEMIES(World &world) : world(world) {
+    current_state=INITIAL;
+  }
+  
+  HighLevelFactory &factory() const;
+  
+  void tick() {
+    FriendlyTeam &friendly = world.friendly_team();
 
-		current_state = INITIAL;
+    if (friendly.size() < min_team_size) {
+      return;
+    }
+    
+    Player::Ptr player0 = friendly.get(0);
+    Player::Ptr player1 = friendly.get(1);
+    Player::Ptr player2 = friendly.get(2);
+    Player::Ptr player3 = friendly.get(3);
+    
+    switch(current_state){
+    case INITIAL:{
+      bool bot0_is_left= (player0->position()-bot0_left).len() < epsilon;
+      bool bot1_is_left= (player1->position()-bot1_left).len() < epsilon;
+      bool bot2_is_left= (player2->position()-bot2_left).len() < epsilon;
+      bool bot3_is_left= (player3->position()-bot3_left).len() < epsilon; 
 
-		}
+      player0->move(bot0_left, Angle(), Point());
+      player1->move(bot1_left, Angle(), Point());
+      player2->move(bot2_left, Angle(), Point());
+      player3->move(bot3_left, Angle(), Point());
+      
+      if(bot0_is_left && bot1_is_left && bot2_is_left && bot3_is_left){
+	bot0_state=false;
+	bot1_state=false;
+	bot2_state=false;
+	bot3_state=false;
+	current_state=MOVE;
+      }
+	
+    }
+      break;
 
-		HighLevelFactory &factory() const;
+    case MOVE:{
 
-		void tick() {
-			FriendlyTeam &friendly = world.friendly_team();
+      bool bot0_is_right= (player0->position()-bot0_right).len() < epsilon;
+      bool bot1_is_right= (player1->position()-bot1_right).len() < epsilon;
+      bool bot2_is_right= (player2->position()-bot2_right).len() < epsilon;
+      bool bot3_is_right= (player3->position()-bot3_right).len() < epsilon;
 
-			if (friendly.size() < min_team_size) {
-				return;
-			}
+      bool bot0_is_left= (player0->position()-bot0_left).len() < epsilon;
+      bool bot1_is_left= (player1->position()-bot1_left).len() < epsilon;
+      bool bot2_is_left= (player2->position()-bot2_left).len() < epsilon;
+      bool bot3_is_left= (player3->position()-bot3_left).len() < epsilon; 
+    
+      if(!bot0_state)
+	player0->move(bot0_right, Angle(), Point());
+      else
+	player0->move(bot0_left, Angle(), Point());
+      
+      if(!bot1_state)
+	player1->move(bot1_right, Angle(), Point());
+      else
+	player1->move(bot1_left, Angle(), Point());
 
-			Player::Ptr player0 = friendly.get(0);
-			Player::Ptr player1 = friendly.get(1);
-			Player::Ptr player2 = friendly.get(2);
-			Player::Ptr player3 = friendly.get(3);
+      if(!bot2_state)
+	player2->move(bot2_right, Angle(), Point());
+      else
+	player2->move(bot2_left, Angle(), Point());
 
-			switch(current_state){
-			case INITIAL:
-			  player0->move(bot0_left, Angle(), Point());
-			  player1->move(bot1_left, Angle(), Point());
-			  player2->move(bot2_right, Angle(), Point());
-			  player3->move(bot3_right, Angle(), Point());
-
-			  current_state=ROBOTS_ON_SIDE1;
-			  
-			  break;
-
-			case ROBOTS_ON_SIDE1:{
-			  bool bot0_is_left= (player0->position()-bot0_left).len() < epsilon;
-			  bool bot1_is_left= (player1->position()-bot1_left).len() < epsilon;
-			  bool bot2_is_left= (player2->position()-bot2_right).len() < epsilon;
-			  bool bot3_is_left= (player3->position()-bot3_right).len() < epsilon;
-
-			  if(bot0_is_left && bot1_is_left && bot2_is_left && bot3_is_left){
-			    player0->move(bot0_right, Angle(), Point());
-			    player1->move(bot1_right, Angle(), Point());
-			    player2->move(bot2_left, Angle(), Point());
-			    player3->move(bot3_left, Angle(), Point());
-			    current_state=ROBOTS_ON_SIDE2;
-			  } else {
-			    player0->move(bot0_left, Angle(), Point());
-			    player1->move(bot1_left, Angle(), Point());
-			    player2->move(bot2_right, Angle(), Point());
-			    player3->move(bot3_right, Angle(), Point());
-
-			  }
-			  
-   			}
-			  break;
-			
-
-			case ROBOTS_ON_SIDE2:{
-			  bool bot0_is_right= (player0->position()-bot0_right).len() < epsilon;
-			  bool bot1_is_right= (player1->position()-bot1_right).len() < epsilon;
-			  bool bot2_is_right= (player2->position()-bot2_left).len() < epsilon;
-			  bool bot3_is_right= (player3->position()-bot3_left).len() < epsilon;
-
-			  if(bot0_is_right && bot1_is_right && bot2_is_right && bot3_is_right){
-			    player0->move(bot0_left, Angle(), Point());
-			    player1->move(bot1_left, Angle(), Point());
-			    player2->move(bot2_right, Angle(), Point());
-			    player3->move(bot3_right, Angle(), Point());
-			    current_state=ROBOTS_ON_SIDE1;
-			  } else {
-			     player0->move(bot0_right, Angle(), Point());
-			     player1->move(bot1_right, Angle(), Point());
-			     player2->move(bot2_left, Angle(), Point());
-			     player3->move(bot3_left, Angle(), Point()); 
-			  }
-			}
-			  break;
-			}	
-
-		}
-
-		Gtk::Widget *ui_controls() {
-			return 0;
-		}
-
-	private:
-		World &world;
-		state current_state;
+      if(!bot3_state)
+	player3->move(bot3_right, Angle(), Point());
+      else
+	player3->move(bot3_left, Angle(), Point());
 
 
+      if(bot0_is_right)
+	bot0_state=true;
+      if(bot1_is_right)
+	bot1_state=true;
+      if(bot2_is_right)
+	bot2_state=true;
+      if(bot3_is_right)
+	bot3_state=true;
+      
+      if(bot0_is_left)
+	bot0_state=false;
+      if(bot1_is_left)
+	bot1_state=false;
+      if(bot2_is_left)
+	bot2_state=false;
+      if(bot3_is_left)
+	bot3_state=false;
+    }
+      break; 
+      
+    }
+  }
+
+
+    Gtk::Widget *ui_controls() {
+      return 0;
+    }
+  
+  private:
+    World &world;
+    state current_state;
+    bool bot0_state,bot1_state,bot2_state,bot3_state;
 };
-
-HIGH_LEVEL_REGISTER(PASCHL_ENEMY)
+  
+HIGH_LEVEL_REGISTER(PASCHL_ENEMIES)
