0d3d5830d0842b4ced1033da11f175ecc3d86878
binaryblade <binaryblade@localhost>
2012-06-20 17:31:44 +0000

    updated PID6 and changed the parameters to match

--- software/ai/robot_controller/pid6_2012_controller.cpp
+++ software/ai/robot_controller/pid6_2012_controller.cpp
@@ -9,6 +9,7 @@
 #include <cmath>
 #include <functional>
 #include <vector>
+#include <iostream>
 
 using AI::RC::RobotController;
 using AI::RC::OldRobotController;
@@ -29,14 +30,14 @@ namespace {
 			Vector4 limit(const double &limit_val) {
 				double max = -1.0 / 0.0;
 				for (int i = 0; i < 4; i++) {
-					max = std::max(max, direction[i]);
+					max = std::max(max, std::abs(direction[i]));
 				}
 
-				if (std::abs(max) < limit_val) {
+				if (max < limit_val) {
 					return *this;
 				}
 
-				double ratio = limit_val / std::abs(max);
+				double ratio = limit_val / max;
 
 				Vector4 temp;
 				for (int i = 0; i < 4; i++) {
@@ -73,12 +74,21 @@ namespace {
 				return temp;
 			}
 
-			Vector4 map(std::function<double(double)> x) {
+			Vector4 map(std::function<double(const double &)> x) {
 				Vector4 temp;
 				for (int i = 0; i < 4; i++) {
-					temp.direction[i] = x(temp.direction[i]);
+					temp.direction[i] = x(direction[i]);
 				}
 				return temp;
+			}
+			
+			void print() {
+				for(int i=0;i<4;i++) {
+					std::cout << direction[i];
+					if(i != 3)
+						std::cout << ":";
+				}
+				std::cout << std::endl;
 			}
 	};
 
@@ -89,16 +99,16 @@ namespace {
 	class Vector3 {
 		public:
 			Point cartesian_direction;
-			double angular_direction;
+			Angle angular_direction;
 			static const double WHEEL_MATRIX[4][3];
 
-			explicit Vector3() : cartesian_direction(0, 0), angular_direction(0.0) {
+			explicit Vector3() : cartesian_direction(0, 0), angular_direction(Angle::ZERO) {
 			}
 
-			explicit Vector3(const Point &cart, const double &angle) : cartesian_direction(cart), angular_direction(angle) {}
+			explicit Vector3(const Point &cart, const Angle &angle) : cartesian_direction(cart), angular_direction(angle) {}
 
 			Vector3 operator*(double scale_value) const {
-				return Vector3(cartesian_direction * scale_value, angular_direction * scale_value);
+				return Vector3(cartesian_direction * scale_value, (angular_direction * scale_value).angle_mod());
 			}
 
 			Vector3 operator/(double scale_value) const {
@@ -106,17 +116,23 @@ namespace {
 			}
 
 			Vector3 operator+(const Vector3 &sum) const {
-				return Vector3(cartesian_direction + sum.cartesian_direction, angular_direction + sum.angular_direction);
+				return Vector3(cartesian_direction + sum.cartesian_direction, (angular_direction + sum.angular_direction).angle_mod());
 			}
 
 			Vector3 operator-(const Vector3 &sub) const {
-				return Vector3(cartesian_direction - sub.cartesian_direction, angular_direction - sub.angular_direction);
+				return Vector3(cartesian_direction - sub.cartesian_direction, (angular_direction - sub.angular_direction).angle_mod());
+			}
+
+			Vector3 rebase(const Vector3& new_origin) const {
+				Point p_diff = cartesian_direction - new_origin.cartesian_direction;
+				Angle a_diff = (angular_direction - new_origin.angular_direction).angle_mod();
+				return Vector3(p_diff.rotate(-new_origin.angular_direction),a_diff);
 			}
 
 			Vector4 toVector4() const {
 				Vector4 temp;
 				for (int i = 0; i < 4; i++) {
-					temp.direction[i] = WHEEL_MATRIX[i][0] * cartesian_direction.x + WHEEL_MATRIX[i][1] * cartesian_direction.y + WHEEL_MATRIX[i][2] * angular_direction;
+					temp.direction[i] = WHEEL_MATRIX[i][0] * cartesian_direction.x + WHEEL_MATRIX[i][1] * cartesian_direction.y + WHEEL_MATRIX[i][2] * angular_direction.to_radians();
 				}
 				return temp;
 			}
@@ -126,9 +142,10 @@ namespace {
 		return vec * scale;
 	}
 
+
 	DoubleParam firmware_loop_rate("Tick rate of firmware control loop in s^-1", "RC/PID6_2012", 200.0, 0.0, 48.0e6);
-	DoubleParam wheel_max_speed("Limit wheel speed (quarter degree per firmware tick)", "RC/PID6_2012", 330.0, 0, 1023);
-	DoubleParam wheel_max_accel("Limit wheel accel (quarter degree per firmware tick squared)", "RC/PID6_2012", 45, 0, 1023);
+	DoubleParam wheel_max_speed("Limit wheel speed (quarter degree per firmware tick)", "RC/PID6_2012", 257.0, 0, 1023);
+	DoubleParam wheel_max_accel("Limit wheel accel (quarter degree per firmware tick squared)", "RC/PID6_2012", 75, 0, 1023);
 	DoubleParam aggressiveness("Aggressiveness of the controller", "RC/PID6_2012", 0.8, 0, 1.0);
 
 	class PID6_2012Controller : public RobotController {
@@ -160,23 +177,30 @@ namespace {
 	}
 
 	double distance_to_velocity(const double &distance) {
-		double max_acc = firmware_loop_rate / TIMESTEPS_PER_SECOND * wheel_max_accel;
-		double dist_to_vel = 2 * max_acc / wheel_max_speed * aggressiveness;
+//		double max_acc = firmware_loop_rate / TIMESTEPS_PER_SECOND * wheel_max_accel;
+//		double dist_to_vel = 2 * max_acc / wheel_max_speed * aggressiveness;
 
-		return distance * dist_to_vel; // the velocity
+//		return distance * dist_to_vel; // the velocity
+
+			return distance * aggressiveness;
 	}
 
-	void PID6_2012Controller::move(const Point &new_position, Angle new_orientation, timespec time_of_arrival, int(&wheel_speeds)[4]) {
+	void PID6_2012Controller::move(const Point &new_point, Angle new_orientation, timespec time_of_arrival, int(&wheel_speeds)[4]) {
 		// This is the difference between where we are and where we are going rotated to robot coordinates
-		Vector3 position_delta = Vector3((new_position - player->position()).rotate(-player->orientation()), (new_orientation - player->orientation()).angle_mod().to_radians());
+		Vector3 current_position = Vector3(player->position(),player->orientation());
+		Vector3 new_position = Vector3(new_point,new_orientation);
+		Vector3 position_delta = new_position.rebase(current_position);
 
 		double time_deadline = timespec_to_double(timespec_sub(time_of_arrival, world.monotonic_time()));
 
 		time_deadline = (time_deadline < 1.0 / TIMESTEPS_PER_SECOND) ? 1.0 / TIMESTEPS_PER_SECOND : time_deadline;
 
+		//toVector4 from a meter delta provides s*q/t therefore divide by seconds
 		Vector4 wheel_target_vel = position_delta.toVector4().map(distance_to_velocity) / time_deadline;
+		position_delta.toVector4().print();
+		wheel_target_vel.print();
+		//vel error in q/t
 		Vector4 vel_error = wheel_target_vel - prev_speed;
-
 		wheel_target_vel = prev_speed + vel_error.limit(wheel_max_accel * firmware_loop_rate / TIMESTEPS_PER_SECOND);
 
 		wheel_target_vel = wheel_target_vel.limit(wheel_max_speed);
