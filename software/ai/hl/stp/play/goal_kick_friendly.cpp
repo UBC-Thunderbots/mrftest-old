e522e2c0ab4c3065f02ae4d6279adb02a862bcc9
skghosh <skghosh@localhost>
2012-06-22 14:20:38 +0000

    goal_kick_friendly_chip now chips to a side of the field instead of center, and also has a player waiting there for the ball

--- software/ai/hl/stp/play/goal_kick_friendly.cpp
+++ software/ai/hl/stp/play/goal_kick_friendly.cpp
@@ -8,27 +8,44 @@ using AI::HL::STP::Coordinate;
 /**
  *Chips ball to center of field from friendly corner
  */
+
+
+
 BEGIN_PLAY(GoalKickFriendly)
 INVARIANT((Predicates::playtype(world, AI::Common::PlayType::EXECUTE_DIRECT_FREE_KICK_FRIENDLY) || Predicates::playtype(world, AI::Common::PlayType::EXECUTE_INDIRECT_FREE_KICK_FRIENDLY)) && Predicates::our_team_size_at_least(world, 2)
 && Predicates::ball_in_our_corner(world))
 APPLICABLE(true)
 DONE(false)
 FAIL(false)
+
+
+
+
 BEGIN_ASSIGN()
+
+Point drop_point;
+//check side of field we're kicking from
+if (world.ball().position().y > 0) {
+	drop_point = Point(0, 1.5);
+} else {
+	drop_point = Point(0, -1.5);
+}
+
+
 // GOALIE
 goalie_role.push_back(goalie_dynamic(world, 1));
 
 // ROLE 1
 // kicker
-roles[0].push_back(free_kick_pass(world, Point(0,0), true));
+roles[0].push_back(free_kick_pass(world, drop_point, true));
 
 // ROLE 2
 // defend
-roles[1].push_back(defend_duo_defender(world));
+roles[1].push_back(move(world, drop_point));
 
 // ROLE 3
 // offend
-roles[2].push_back(offend(world));
+roles[2].push_back(defend_duo_defender(world));
 
 // ROLE 4
 // offend
