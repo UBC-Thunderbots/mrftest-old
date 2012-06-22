a269d9329a4b3b3e47021fe73780ac98be1ead55
mparizeau <mparizeau@localhost>
2012-06-22 14:28:07 +0000

    Clean up spaces

--- software/ai/hl/stp/play/goal_kick_friendly.cpp
+++ software/ai/hl/stp/play/goal_kick_friendly.cpp
@@ -9,18 +9,12 @@ using AI::HL::STP::Coordinate;
  *Chips ball to center of field from friendly corner
  */
 
-
-
 BEGIN_PLAY(GoalKickFriendly)
 INVARIANT((Predicates::playtype(world, AI::Common::PlayType::EXECUTE_DIRECT_FREE_KICK_FRIENDLY) || Predicates::playtype(world, AI::Common::PlayType::EXECUTE_INDIRECT_FREE_KICK_FRIENDLY)) && Predicates::our_team_size_at_least(world, 2)
 && Predicates::ball_in_our_corner(world))
 APPLICABLE(true)
 DONE(false)
 FAIL(false)
-
-
-
-
 BEGIN_ASSIGN()
 
 Point drop_point;
@@ -30,7 +24,6 @@ if (world.ball().position().y > 0) {
 } else {
 	drop_point = Point(0, -1.5);
 }
-
 
 // GOALIE
 goalie_role.push_back(goalie_dynamic(world, 1));
@@ -56,4 +49,3 @@ roles[3].push_back(offend_secondary(world));
 roles[4].push_back(defend_duo_extra1(world));
 END_ASSIGN()
 END_PLAY()
-
