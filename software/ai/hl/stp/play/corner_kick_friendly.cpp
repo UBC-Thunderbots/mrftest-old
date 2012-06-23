42a6df576b2a2ac16f70db60e9382b9d1cbc02ae
mparizeau <mparizeau@localhost>
2012-06-23 19:16:35 +0000

    Update player positions based on field testing

--- software/ai/hl/stp/play/corner_kick_friendly.cpp
+++ software/ai/hl/stp/play/corner_kick_friendly.cpp
@@ -6,8 +6,7 @@ namespace Predicates = AI::HL::STP::Predicates;
 using AI::HL::STP::Coordinate;
 
 /**
- *Chips ball to right in front of penalty spot from enemy corner
- *
+ * Chips ball to right in front of penalty spot from enemy corner
  */
 BEGIN_PLAY(CornerKickFriendly)
 INVARIANT((Predicates::playtype(world, AI::Common::PlayType::EXECUTE_DIRECT_FREE_KICK_FRIENDLY) || Predicates::playtype(world, AI::Common::PlayType::EXECUTE_INDIRECT_FREE_KICK_FRIENDLY)) && Predicates::our_team_size_at_least(world, 2)
@@ -21,12 +20,13 @@ goalie_role.push_back(goalie_dynamic(world, 1));
 
 // ROLE 1
 // kicker
-Point kick_dest = Point((world.field().enemy_goal().x - 1.0), -(world.ball().position().y) / 2);
+Point kick_dest((world.field().enemy_goal().x - 1.0), -(world.ball().position().y) / 2);
 roles[0].push_back(free_kick_pass(world, kick_dest, true));
 
 // ROLE 2
 // gets the ball
-roles[1].push_back(move(world, kick_dest));
+Point offset_dist(0.6, 0);
+roles[1].push_back(move(world, kick_dest - offset_dist));
 
 // ROLE 3
 // defend
