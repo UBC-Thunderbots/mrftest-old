96b91fcc81e24c808bd1520985d244191b66b4cc
mparizeau <mparizeau@localhost>
2012-06-23 19:12:33 +0000

    Spin in different directions depending on which side of the field you're on

--- software/ai/hl/stp/action/move_spin.cpp
+++ software/ai/hl/stp/action/move_spin.cpp
@@ -7,14 +7,25 @@ using namespace AI::HL::STP;
 
 namespace {
 	RadianParam spin_delta("change in orientation every time tick for move spin (radians)", "STP/Action/spin", 3.0, 1.0, 5.0);
+
+	const int CLOCKWISE = -1;
+	const int COUNTER_CLOCKWISE = 1;
 }
 
 void AI::HL::STP::Action::move_spin(Player::Ptr player, const Point dest) {
+	// spin in different directions depending on if you are on the top or bottom of the field
+	int direction_to_spin;
+	if (dest.y > 0) {
+		direction_to_spin = CLOCKWISE;
+	} else {
+		direction_to_spin = COUNTER_CLOCKWISE;
+	}
+
 	// spin faster when you are close to the destination point
 	if ((player->position() - dest).len() < AI::HL::Util::POS_CLOSE + Robot::MAX_RADIUS) {
-		player->move(dest, (player->orientation() + 2 * spin_delta).angle_mod(), Point());
+		player->move(dest, (player->orientation() + 2 * spin_delta * direction_to_spin).angle_mod(), Point());
 	} else {
-		player->move(dest, (player->orientation() + spin_delta).angle_mod(), Point());
+		player->move(dest, (player->orientation() + spin_delta * direction_to_spin).angle_mod(), Point());
 	}
 	player->type(AI::Flags::MoveType::RAM_BALL);
 	player->prio(AI::Flags::MovePrio::HIGH);
