7f9e95b807c54adcf8e83db0a0eb364fe1fb3a50
tere11 <tere11@localhost>
2013-03-30 22:36:06 +0000

    partially fixed tri offense

--- software/ai/hl/stp/evaluation/tri_attack.cpp
+++ software/ai/hl/stp/evaluation/tri_attack.cpp
@@ -23,41 +23,45 @@ Point AI::HL::STP::Evaluation::tri_attack_evaluation(World world) {
 	int enemy_free_zone_importance = 50;
 	int shooting_distance_importance = 30;
 	int shooting_angle_importance = 20;
-	double danger_zone = 0.4, distance, danger_level, less_danger_position = 100000, angle;
+	double danger_zone = 0.4, distance, danger_level, less_danger_position = 1e8, angle;
 	//size_t enemy_team_size = world.enemy_team().size();
 	std::vector<Robot> enemies = AI::HL::Util::get_robots(world.enemy_team());
 //			Evaluation::enemies_by_grab_ball_dist();
-	int index;
-	Point point_underevaluation = Point(0,0);
+
+	Point p = Point(0,0);
 	Point best_point = Point(0,0);
+	double ball_dist = 0;
 	int robot_count;
 
 	for (double i = 1; i <= GRID_X; i++ ) {
 		for (double j = 1; j <= GRID_Y; j++) {
-			index = 0;
 			robot_count = 0;
 			double x = world.field().width() * (i/GRID_X);
 			double y = world.field().width() * (j/GRID_Y);
 			
-			point_underevaluation.x = x;
-			point_underevaluation.y = y;
-			for (size_t i = 0 ; i < enemies.size() ; i++) {
+			p.x = x;
+			p.y = y;
+			ball_dist = (world.ball().position() - p).len() / 10.0;
+
+			for (size_t k = 0 ; k < enemies.size() ; k++) {
 				// Calculate how many enemies are within the danger_zone
-				distance = (point_underevaluation - enemies[index].position()).len();
+				distance = (p - enemies[k].position()).len();
 				if(distance < danger_zone) { robot_count++; }			
 			} 
 
 			// Calculate distance from net
-			distance = (point_underevaluation - world.field().enemy_goal()).len();
+			distance = (p - world.field().enemy_goal()).len();
 
 			// Calculate shooting angle
 			double ang = fabs((x-2.0)/y);
 			angle = tan(ang);
 			danger_level = enemy_free_zone_importance*(robot_count/6) + shooting_distance_importance*(distance/5.5) + angle*shooting_angle_importance;
 
+			danger_level *= ball_dist;
+
 			if(danger_level < less_danger_position) {
 				less_danger_position = danger_level;
-				best_point = point_underevaluation;
+				best_point = p;
 			}
 		}
 	}
