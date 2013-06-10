9ac99b76813bcaa221b446d4cd9f8d858af5b336
tere11 <tere11@localhost>
2013-06-10 22:25:44 +0000

    started wrapping up tri attack

--- software/ai/hl/stp/evaluation/tri_attack.h
+++ software/ai/hl/stp/evaluation/tri_attack.h
@@ -5,11 +5,22 @@
 
 #include <array>
 
+namespace {
+	const unsigned int MAX_ATTACKERS = 4;
+}
+
 namespace AI {
 	namespace HL {
 		namespace STP {
 			namespace Evaluation {
-
+				/**
+				 * Locations:
+				 * 0 - active tactic attacker
+				 * 1 - attacker secondary
+				 * 2 - attacker tertiary
+				 * 3 - attacker-extra (for diamond formation)
+				 */
+				const std::array<Point, MAX_ATTACKERS> evaluate_attack();
 
 				/**
 				 * return point to where player needs to go in a tri-attack
