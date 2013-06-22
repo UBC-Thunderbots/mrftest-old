074f873672a6f1b32925cde23f3ff21454674b9c
skghosh <skghosh@localhost>
2013-06-22 09:08:07 +0000

    removed chipping

--- software/ai/hl/stp/action/defend.cpp
+++ software/ai/hl/stp/action/defend.cpp
@@ -13,11 +13,11 @@ namespace {
 void AI::HL::STP::Action::defender_move(World world, Player player, const Point dest) {
 	// if the ball is too close we repel or chip
 	if ((world.ball().position() - player.position()).len() < repel_dist * Robot::MAX_RADIUS) {
-		if ((world.ball().position() - world.field().friendly_goal()).len() < repel_dist * 3) { 
-			chip_target(world, player, Point(0,0));
-		} else {
+//		if ((world.ball().position() - world.field().friendly_goal()).len() < repel_dist * 3) {
+//			chip_target(world, player, Point(0,0));
+//		} else {
 			corner_repel(world, player);
-		}
+	//	}
 		return;
 	}
 	player.move(dest, (world.ball().position() - player.position()).orientation(), Point());
--- software/ai/hl/stp/action/goalie.cpp
+++ software/ai/hl/stp/action/goalie.cpp
@@ -37,11 +37,11 @@ void AI::HL::STP::Action::lone_goalie(World world, Player player) {
 
 void AI::HL::STP::Action::goalie_move(World world, Player player, Point dest) {
 	//autokick always on, if player has chipper, its autochip
-	if (player.has_chipper()) {
-		player.autochip(1);
-	} else {
+//	if (player.has_chipper()) {
+//		player.autochip(1);
+//	} else {
 		player.autokick(BALL_MAX_SPEED);
-	}
+//	}
 
 	// if ball is inside the defense area or just too close, repel!!!!
 	if ((AI::HL::Util::point_in_friendly_defense(world.field(), world.ball().position()) || (world.ball().position() - player.position()).len() < goalie_repel_dist * Robot::MAX_RADIUS) && world.playtype() != AI::Common::PlayType::STOP) {
