1ac043b2323a5a1cfbeff968d7f57db8471243c1
cyberfish <cyberfish@localhost>
2013-06-24 10:51:14 +0000

    Changed maximum kick power to 825us (8 m/s)

--- software/ai/backend/physical/player.cpp
+++ software/ai/backend/physical/player.cpp
@@ -71,7 +71,7 @@ bool Player::chicker_ready() const {
 void Player::kick_impl(double speed) {
 	if (bot.alive) {
 		if (bot.capacitor_charged) {
-			bot.kick(false, speed / 8.0 * 3000.0);
+			bot.kick(false, speed / 8.0 * 825.0);
 		} else {
 			LOG_ERROR(Glib::ustring::compose("Bot %1 kick when not ready", pattern()));
 		}
@@ -81,7 +81,7 @@ void Player::kick_impl(double speed) {
 void Player::autokick_impl(double speed) {
 	if (bot.alive) {
 		autokick_params.chip = false;
-		autokick_params.pulse = speed / 8.0 * 3000.0;
+		autokick_params.pulse = speed / 8.0 * 825.0;
 	}
 }
 
