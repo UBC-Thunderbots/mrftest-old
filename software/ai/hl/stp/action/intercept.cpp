9ab94f6697e9566f1de9d4a437c987f3ed7d6cdd
skghosh <skghosh@localhost>
2013-06-25 08:06:12 +0000

    added slow dribble to intercept

--- software/ai/hl/stp/action/intercept.cpp
+++ software/ai/hl/stp/action/intercept.cpp
@@ -3,5 +3,6 @@
 
 void AI::HL::STP::Action::intercept(AI::HL::STP::Player player, const Point target) {
 	player.type(AI::Flags::MoveType::INTERCEPT);
+	player.dribble_slow();
 	player.move(target, (target - player.position()).orientation(), Point());
 }
