9ee8b87d917958cff8a60f73e6eb72806b205dee
skghosh <skghosh@localhost>
2013-06-28 09:49:37 +0000

    added tactic indirect chip to cherry

---
+++ software/ai/hl/stp/tactic/indirect_chip_to_cherry.cpp
@@ -0,0 +1,60 @@
+#include "ai/hl/stp/tactic/indirect_chip_to_cherry.h"
+#include "ai/hl/stp/action/chip.h"
+#include "ai/hl/stp/action/shoot.h"
+#include "ai/hl/stp/action/move.h"
+#include "ai/hl/stp/tactic/util.h"
+#include "ai/hl/stp/evaluation/ball.h"
+#include "ai/hl/stp/evaluation/player.h"
+#include "ai/hl/stp/evaluation/shoot.h"
+#include "ai/hl/stp/evaluation/indirect_cherry.h"
+#include "ai/hl/stp/param.h"
+
+#include "geom/angle.h"
+
+using AI::HL::STP::BALL_MAX_SPEED;
+using namespace AI::HL::STP::Tactic;
+using namespace AI::HL::W;
+using AI::HL::STP::Coordinate;
+namespace Evaluation = AI::HL::STP::Evaluation;
+namespace Action = AI::HL::STP::Action;
+
+namespace {
+
+	class IndirectChipToCherry : public Tactic {
+		public:
+			IndirectChipToCherry(World world) : Tactic(world, true) {
+			}
+
+		private:
+			bool done() const;
+			Player select(const std::set<Player> &players) const;
+			Point destination();
+			void execute();
+			void player_changed();
+			Glib::ustring description() const {
+				return "auto-chipped";
+			}
+
+	};
+
+	bool IndirectChipToCherry::done() const {
+		return Evaluation::cherry_at_point(world, player);
+	}
+
+	Player IndirectChipToCherry::select(const std::set<Player> &players) const {
+		return select_baller(world, players, player);
+	}
+
+	void IndirectChipToCherry::player_changed() {
+	}
+	void IndirectChipToCherry::execute() {
+		Point destination = Evaluation::cherry_pivot(world, player);
+		Action::move(world, player, destination);
+	}
+}
+
+Tactic::Ptr AI::HL::STP::Tactic::indirect_chip_to_cherry(World world) {
+	Tactic::Ptr p(new IndirectChipToCherry(world));
+	return p;
+}
+
---
+++ software/ai/hl/stp/tactic/indirect_chip_to_cherry.h
@@ -0,0 +1,22 @@
+#ifndef AI_HL_STP_TACTIC_INDIRECT_CHIP_TO_CHERRY_H
+#define AI_HL_STP_TACTIC_INDIRECT_CHIP_TO_CHERRY_H
+
+#include "ai/hl/stp/tactic/tactic.h"
+#include "ai/hl/stp/coordinate.h"
+
+namespace AI {
+	namespace HL {
+		namespace STP {
+			namespace Tactic {
+
+				/**
+				 * Chip ball to a cherry picker.
+				 */
+				Tactic::Ptr indirect_chip_to_cherry(World world);
+			}
+		}
+	}
+}
+
+#endif
+
