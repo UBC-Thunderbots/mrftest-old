bc507565499495ab9f067e74979f37d90e5c19d7
chead <chead@localhost>
2013-08-07 23:58:54 +0000

    Clean up and document Registerable.

--- software/util/registerable.h
+++ software/util/registerable.h
@@ -11,33 +11,37 @@
 /**
  * A generic, templatized class to handle interfaces that should have registerable implementations.
  *
- * \tparam T the type that is actually registerable (that is, the type of the subclass of this class).
+ * \tparam T the type that is actually registerable (that is, the type of the subclass of this class)
  */
 template<typename T> class Registerable : public NonCopyable {
 	public:
 		/**
-		 * The type of a map from collation key of object name to object.
+		 * \brief The type of a map from collation key of object name to object.
 		 */
 		typedef std::map<std::string, T *> Map;
 
 		/**
-		 * Gets a map of all currently-registered objects, keyed by their names' collation keys.
+		 * \brief Gets a map of all currently-registered objects, keyed by their names’ collation keys.
 		 *
-		 * \return the map of registered objects of this type.
+		 * \return the map of registered objects of this type
 		 */
 		static const Map &all() {
+			// Flush any created-but-not-yet-inserted-into-the-map objects into the map.
 			while (precache()) {
+				// Dequeue an object from the list.
 				T *obj = dynamic_cast<T *>(precache());
 				precache() = obj->precache_next;
 				obj->precache_next = nullptr;
-				obj->name_ = obj->name_raw;
+				// Check if the name contains a slash, which is illegal.
 				if (exists(obj->name_.begin(), obj->name_.end(), 47 /* slash */)) {
 					throw std::invalid_argument(Glib::locale_from_utf8(Glib::ustring::compose(u8"Invalid name: %1 (must not contain a slash)", obj->name())));
 				}
+				// Check if the name duplicates another object of the same type.
 				const std::string &key = obj->name().collate_key();
 				if (objects().count(key)) {
 					throw std::invalid_argument(Glib::locale_from_utf8(Glib::ustring::compose(u8"Duplicate name: %1", obj->name())));
 				}
+				// Add the object to the map.
 				objects()[key] = obj;
 			}
 			return objects();
@@ -52,36 +56,59 @@ template<typename T> class Registerable : public NonCopyable {
 
 	protected:
 		/**
-		 * Constructs a new Registerable object and registers it.
+		 * \brief Constructs a new Registerable object and registers it.
 		 *
-		 * \param[in] name the name of the new object.
+		 * \param[in] name the name of the new object (encoded in UTF-8)
 		 */
-		Registerable(const char *name) : name_raw(name) {
+		Registerable(const char *name) : name_(name) {
+			// Insert into the linked list of preconstructed objects.
 			precache_next = precache();
 			precache() = this;
 		}
 
 		/**
-		 * Destroys a Registerable object. Does not unregister it, because we might have some global object destruction order issues.
+		 * \brief Destroys a Registerable object. Does not unregister it, because we might have some global object destruction order issues.
 		 */
-		virtual ~Registerable() {
-		}
+		virtual ~Registerable();
 
 	private:
-		const char *const name_raw;
+		/**
+		 * \brief The object’s name.
+		 */
 		Glib::ustring name_;
+
+		/**
+		 * \brief The next object in the linked list of objects that have been registered but not yet inserted into the main map.
+		 */
 		Registerable<T> *precache_next;
 
+		/**
+		 * \brief Returns the head of the linked list of objects that have been registered but not yet inserted into the main map.
+		 *
+		 * Such a list is necessary because Registerable objects are constructed globally before main starts running.
+		 * This means that, were they added to the map immediately, their collation key would change when main sets the application locale, breaking the map.
+		 *
+		 * \return a reference to the list head pointer
+		 */
 		static Registerable<T> * &precache() {
 			static Registerable<T> *first;
 			return first;
 		}
 
+		/**
+		 * \brief Returns the main map object.
+		 *
+		 * \return the map
+		 */
 		static Map &objects() {
 			static Map m;
 			return m;
 		}
 };
+
+
+
+template<typename T> Registerable<T>::~Registerable() = default;
 
 #endif
 
