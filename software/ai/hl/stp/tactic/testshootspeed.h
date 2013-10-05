13233741d96c20569ac7af44c0bedcffef9139d8
WeiDTai <weidtai@localhost>
2013-10-05 22:36:12 +0000

    added plays + tactics for measuring ball shooting speed inside our HL. fixed #1125

---
+++ software/ai/hl/stp/tactic/testshootspeed.h
@@ -0,0 +1,19 @@
+#ifndef AI_HL_STP_TACTIC_TESTSHOOTSPEED_H
+#define AI_HL_STP_TACTIC_TESTSHOOTSPEED_H
+
+#include "ai/hl/stp/tactic/tactic.h"
+#include "ai/hl/stp/coordinate.h"
+
+namespace AI {
+	namespace HL {
+		namespace STP {
+			namespace Tactic {
+				Tactic::Ptr test_shoot_speed(World world, bool force = false);
+
+			}
+		}
+	}
+}
+
+#endif
+
