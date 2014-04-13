177ca1a274de54510afaca621fb385df936db006
echo3960 <echo3960@localhost>
2014-04-13 07:08:48 +0000

    Added DMA into SDcard. It may be incomplete and have errors in the implementation, please check before use.

--- firmware/main/sdcard.h
+++ firmware/main/sdcard.h
@@ -49,14 +49,115 @@ typedef enum {
 	SD_STATUS_ILLEGAL_COMMAND,
 
 	/**
-	 * \brief The card was idle when it should not be or was not idle when it should be.
+	 * \brief The card was in an unexpected state after the command is run (?)
 	 */
-	SD_STATUS_ILLEGAL_IDLE,
+	SD_STATUS_ILLEGAL_STATE,
 
 	/**
 	 * \brief The card encountered an internal error in its controller or uncorrectable corruption in its memory array.
 	 */
 	SD_STATUS_CARD_INTERNAL_ERROR,
+
+	/** NEW for STM32
+	 * \brief Command response timeout
+	 */
+	SD_STATUS_COMMAND_RESPONSE_TIMEOUT, 
+
+	/** NEW for STM32
+	 * \brief The command's argument was out of the allowed range for this card. 
+	 */
+	SD_STATUS_OUT_OF_RANGE,
+
+	/**
+	 * \brief The commands address argument positions the first data block misaligned to the card physical blocks. 
+	 */
+	SD_STATUS_ADDRESS_MISALIGN,
+
+	/* 
+	 * brief Either the argument of a SET_BLOCKLEN command exceeds the maximum value allowed for the card, or the previously defined blck length is illegal for the current coommand
+	 */
+	SD_STATUS_BLOCK_LEN_ERROR, 
+
+	/*
+	 * brief An error in the sequence of erase commands occurred
+	 */
+	SD_STATUS_ERASE_SEQ_ERROR,
+
+	/*
+	 * brief An invalid selectioin of erase groups for erase occurred
+	 */
+	SD_STATUS_ERASE_PARAM, 
+
+	/*
+	 * brief Attempt to program a write-protected block
+	 */
+	SD_STATUS_WP_VIOLATION,
+
+	/*
+	 * brief When set, signals that the card is locked by the host
+	 */
+	SD_STATUS_CARD_IS_LOCKED,
+
+	/*
+	 * brief Set when a sequence or password error has been detected in lock/unlock card command
+	 */
+	SD_STATUS_LOCK_UNLOCK_FAILED,
+
+	/*
+	 * brief The CRC check of the previous command failed
+	 */
+	SD_STATUS_COM_CRC_ERROR,
+
+	/*
+	 * brief Command not legal for the card state
+	 */
+	SD_STATUS_ILLEGAL_COMMAND, 
+
+	/*
+	 * brief Card internal ECC was applied but failed to correct the data
+	 */
+	SD_STATUS_CARD_ECC_FAILED, 
+
+	/*
+	 * brief A card error occurred, which is not related to the host command
+	 */
+	SD_STATUS_CC_ERROR, 
+
+	/*
+	 * brief Generic card error related to the ( and detected during) execution of the last host command.
+	 */
+	SD_STATUS_ERROR, 
+
+	/*
+	 * brief CSD write error
+	 */
+	SD_STATUS_CSD_OVERWRITE, 
+
+	/*
+	 * brief Set when only partial address space was erased due to existing write
+	 */
+	SD_STATUS_WP_ERASE_SKIP,
+
+	/* 
+	 * brief Command has been executed without using the internal ECC
+	 */
+	SD_STATUS_CARD_ECC_DISABLED, 
+
+	/*
+	 * brief Erase sequence was cleared before executing because an out of erase sequence command was received.
+	 */
+	SD_STATUS_ERASE_RESET, 
+
+	/*
+	 * brief Corresponds to buffer empty signalling on the bus
+	 */
+	SD_STATUS_READY_FOR_DATA, 
+
+	/* 
+	 * brief Error in the sequence of the authentication process
+	 */
+	SD_STATUS_AKE_SEQ_ERROR
+
 } sd_status_t;
 
 /**
@@ -79,6 +180,25 @@ bool sd_init(void);
  * \return the number of sectors, or 0 on failure
  */
 uint32_t sd_sector_count(void);
+
+
+/**
+ * \brief Reads a block from the SD card. 
+ * 
+ * \param Unclear yet. 
+ *
+ * \return \c true on success, or \c false on failure
+ */
+bool read_block (uint32_t * );
+
+/**
+ * \brief Writes a block to the SD card. 
+ * 
+ * \param Unclear yet. 
+ * 
+ * \return \c true on success, or \c false on failure
+ */ 
+bool write_block (uint32_t *);
 
 /**
  * \brief Reads a sector from the SD card.
