b89704020267f8e780ebb2252f2d7e01056c4e81
exiatus <exiatus@localhost>
2014-07-21 12:08:54 +0000

    pushed earlier changes into new file

---
+++ software/ai/hl/stp/action/cshoot.cpp
@@ -0,0 +1,51 @@
+#include "ai/hl/stp/action/cshoot.h"
+#include "ai/hl/stp/action/chip.h"
+#include "ai/hl/stp/action/intercept.h"
+#include "ai/hl/stp/action/pivot.h"
+#include "ai/hl/stp/action/ram.h"
+#include "ai/hl/stp/evaluation/shoot.h"
+#include "ai/hl/stp/evaluation/ball.h"
+#include "ai/hl/stp/evaluation/team.h"
+#include "ai/hl/stp/evaluation/pass.h"
+#include "ai/hl/stp/evaluation/player.h"
+#include "ai/hl/stp/action/move.h"
+#include "ai/hl/util.h"
+#include "ai/flags.h"
+#include "geom/util.h"
+#include "geom/angle.h"
+#include "geom/rect.h"
+#include "util/param.h"
+#include "util/dprint.h"
+#include <algorithm>
+#include <cmath>
+#include "ai/hl/stp/param.h"
+
+using namespace AI::HL::STP;
+
+namespace {
+}
+
+bool AI::HL::STP::Action::cshoot_target(World world, Player player, const Point target, double velocity) {
+	// Evaluation::ShootData shoot_data = Evaluation::evaluate_shoot_target(world, player, target);
+	intercept(player, target);
+
+	// if (shoot_data.can_shoot) {
+	if (!Evaluation::player_within_angle_thresh(player, target, passer_angle_threshold)) {
+		return false;
+	}
+
+	// angle is right but chicker not ready, ram the ball and get closer to target
+	if (!player.chicker_ready()) {
+		LOG_INFO(u8"chicker not ready");
+		// angle is right but chicker not ready, ram the ball and get closer to target, only use in normal play
+		if (world.playtype() == AI::Common::PlayType::PLAY) {
+			const Point diff = world.ball().position() - player.position();
+			ram(world, player, target, diff * 8.0);
+		}
+		return false;
+	}
+//make sure velocity is updated for 2013
+	LOG_INFO(u8"autokick");
+	player.autokick(velocity);
+	return true;
+}
---
+++ software/ai/hl/stp/tactic/cshoot.cpp
@@ -0,0 +1,210 @@
+#include "ai/hl/stp/tactic/cshoot.h"
+#include "ai/hl/stp/action/cshoot.h"
+#include "ai/hl/stp/tactic/util.h"
+#include "ai/hl/stp/action/intercept.h"
+#include "ai/hl/stp/evaluation/ball.h"
+#include "ai/hl/stp/evaluation/player.h"
+#include "ai/hl/stp/evaluation/shoot.h"
+#include "ai/hl/stp/param.h"
+#include "ai/hl/stp/action/move.h"
+#include "geom/util.h"
+#include "geom/angle.h"
+#include <iostream>
+
+using AI::HL::STP::BALL_MAX_SPEED;
+using namespace AI::HL::STP::Tactic;
+using namespace AI::HL::STP::Action;
+using namespace AI::HL::W;
+using AI::HL::STP::Coordinate;
+namespace Evaluation = AI::HL::STP::Evaluation;
+
+namespace {
+	class CShootGoal : public Tactic {
+		public:
+			CShootGoal(World world, bool force) : Tactic(world, true), kick_attempted(false), force(force), shoot_score(Angle::zero()),
+			position_reset(true), timer(0), best_score(std::make_pair(Point(0, 0), std::make_pair(Point(0, 0), Angle::zero())))
+			{
+				// world.friendly_team().signal_robot_removing().connect(sigc::mem_fun(this, &ShootGoal::on_player_removed));
+			}
+
+		private:
+			bool kick_attempted;
+			bool force;
+			Angle shoot_score;
+			// mutable Player shooter;
+
+			// void on_player_removed(std::size_t index) {
+			// if(passer.is() && world.friendly_team().get(index) == Player::CPtr(passer)){
+			// passer.reset();
+			// }
+			// }
+
+
+
+			bool done() const;
+			Player select(const std::set<Player> &players) const;
+			void execute();
+			void player_changed();
+			void draw_overlay(Cairo::RefPtr<Cairo::Context> ctx) const;
+			Glib::ustring description() const {
+				return u8"cshoot-goal";
+			}
+
+			bool position_reset;
+			int timer;
+			Point enemy_goal_positive, enemy_goal_negative;
+			Point initial_position;
+			Point target;
+			std::vector<std::pair<Point, std::pair<Point, Angle>>> scores;
+			std::pair<Point, std::pair<Point, Angle>> best_score;
+			std::vector<Point> obstacles;
+	};
+
+	bool CShootGoal::done() const {
+		return player /* && kick_attempted*/ && player.autokick_fired();
+	}
+
+	Player CShootGoal::select(const std::set<Player> &players) const {
+		// if a player attempted to shoot, keep the player
+		if (kick_attempted && players.count(player)) {
+			return player;
+		}
+		return select_baller(world, players, player);
+	}
+
+	void CShootGoal::player_changed() {
+		initial_position = player.position();
+		kick_attempted = false;
+	}
+
+	void CShootGoal::execute() {
+		if(timer >= 0 && !player.has_ball()) {
+			timer--;
+		}
+		else if(timer <= 10 && player.has_ball()) {
+			timer++;
+		}
+		best_score = std::make_pair(Point(0,0), std::make_pair(Point(0,0), Angle::zero()));
+
+		if((world.ball().position() - player.position()).len() > Robot::MAX_RADIUS + 0.02)
+			position_reset = true;
+
+		if(position_reset) {
+			initial_position = player.position();
+			position_reset = false;
+		}
+
+		scores.clear();
+		obstacles.clear();
+		scores.clear();
+		
+		//first: find the best location to shoot at in a circle centered at the baller with a radius of 0.5m
+		enemy_goal_positive = world.field().enemy_goal_boundary().first.x > 0.0 ? 
+			world.field().enemy_goal_boundary().first : world.field().enemy_goal_boundary().second;
+		enemy_goal_negative = world.field().enemy_goal_boundary().first.x < 0.0 ?
+			world.field().enemy_goal_boundary().first : world.field().enemy_goal_boundary().second;
+
+		for(auto i : world.enemy_team())
+			obstacles.push_back(i.position());
+		for(auto i : world.friendly_team())
+			if((i.position() - player.position()).len() > 0.1)
+				obstacles.push_back(i.position());
+
+		for(double x = initial_position.x - 0.50; x < initial_position.x + 0.50; x += 0.04) {
+			for(double y = initial_position.y - 0.50; y < initial_position.y + 0.50; y += 0.04) {
+				if((Point(x, y) - initial_position).len() < 0.45) {
+					scores.push_back(
+							std::make_pair(
+								Point(x, y),
+								angle_sweep_circles(
+									Point(x, y),
+									enemy_goal_positive,
+									enemy_goal_negative,
+									obstacles,
+									Robot::MAX_RADIUS
+									)
+								)
+							);
+				}
+			}
+		}
+
+		/* if(!player.has_ball()) {
+			player.dribble_slow();
+			Point p = player.position(), b = world.ball().position();
+			if(line_point_dist(p, b, b - Point(1, 0)) > 0.1 || p.x > b.x)
+				move(player, Angle::zero(),
+						world.ball().position() - Point(Robot::MAX_RADIUS + 0.07, 0));
+			else
+				move(player, Angle::zero(), 
+						world.ball().position());
+ 			return;
+			if(timer < 0) {
+				intercept(player, world.ball().position());
+				return;
+			}
+ 		} */
+
+		for(auto &i : scores) {
+			for(auto p : world.enemy_team()) {
+				if((p.position() - i.first).len() < Robot::MAX_RADIUS * 2 + 0.02)
+					i.second.second = Angle::zero();
+			}
+			if(i.second.second > best_score.second.second)
+				best_score = i;
+		}
+
+		target = angle_sweep_circles(
+			best_score.first, 
+			world.field().enemy_goal_boundary().first, 
+			world.field().enemy_goal_boundary().second, 
+			obstacles, 
+			Robot::MAX_RADIUS
+			).first;
+
+		if(timer < 10)
+			return;
+
+		/*
+
+		if(((best_score.first - player.position()).len() > 0.05) || 
+				((player.orientation() - (best_score.second.first - player.position()).orientation()).to_degrees() > 2.0)) {
+			move(player, Angle::of_degrees((player.orientation().to_degrees() - (best_score.second.first - player.position()).orientation().to_degrees() > 0 ?
+					-1 : 1) * 6) + player.orientation(), 
+					player.position() + (best_score.first - player.position()).norm(0.2));
+			//player.flags(AI::Flags::FLAG_CAREFUL);
+			player.dribble_slow();
+			player.type(AI::Flags::MoveType::DRIBBLE);
+			return;
+		}
+
+				*/
+
+		std::cout << (player.orientation() - (best_score.second.first - player.position()).orientation()).to_degrees() << '\n';
+		move(player, player.orientation() - (best_score.second.first - player.position()).orientation(), best_score.first);
+
+		if(((best_score.first - player.position()).len() < 0.05) && 
+				((player.orientation() - (best_score.second.first - player.position()).orientation()).to_degrees() < 2.0))
+			if (cshoot_target(world, player, target, BALL_MAX_SPEED)) {
+				kick_attempted = true;
+		}
+	}
+
+	void CShootGoal::draw_overlay(Cairo::RefPtr<Cairo::Context> ctx) const {
+		ctx->set_line_width(0.025);
+		for(auto i : scores) {
+			ctx->set_source_rgba(i.second.second.to_degrees() / 15, i.second.second.to_degrees() / 15, i.second.second.to_degrees() / 15, 0.5);
+			ctx->arc(i.first.x, i.first.y, 0.01, 0.0, 2*M_PI);
+			ctx->fill();
+		}
+		ctx->set_source_rgba(1.0, 0.0, 0.0, 1.0);
+		ctx->arc(best_score.first.x, best_score.first.y, 0.02, 0.0, 2*M_PI);
+		ctx->fill();
+	}
+}
+
+Tactic::Ptr AI::HL::STP::Tactic::cshoot_goal(World world, bool force) {
+	Tactic::Ptr p(new CShootGoal(world, force));
+	return p;
+}
+
