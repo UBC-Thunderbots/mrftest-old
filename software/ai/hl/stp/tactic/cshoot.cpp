f91e487c224c21570d87709ddc7a6ec0e182f5f7
exiatus <exiatus@localhost>
2014-07-22 16:33:43 +0000

    new shoot changes, cleaned up old shoot

--- software/ai/hl/stp/tactic/cshoot.cpp
+++ software/ai/hl/stp/tactic/cshoot.cpp
@@ -1,4 +1,5 @@
 #include "ai/hl/stp/tactic/cshoot.h"
+#include "ai/hl/stp/action/shoot.h"
 #include "ai/hl/stp/action/cshoot.h"
 #include "ai/hl/stp/tactic/util.h"
 #include "ai/hl/stp/action/intercept.h"
@@ -22,8 +23,8 @@ namespace Evaluation = AI::HL::STP::Evaluation;
 namespace {
 	class CShootGoal : public Tactic {
 		public:
-			CShootGoal(World world, bool force) : Tactic(world, true), kick_attempted(false), force(force), shoot_score(Angle::zero()),
-			position_reset(true), timer(0), best_score(std::make_pair(Point(0, 0), std::make_pair(Point(0, 0), Angle::zero())))
+			CShootGoal(World world, bool force) : Tactic(world, true), kick_attempted(false), force(force),
+			position_reset(true), timer(0), best_score(std::make_tuple(Point(0, 0), Point(0, 0), 0))
 			{
 				// world.friendly_team().signal_robot_removing().connect(sigc::mem_fun(this, &ShootGoal::on_player_removed));
 			}
@@ -56,8 +57,8 @@ namespace {
 			Point enemy_goal_positive, enemy_goal_negative;
 			Point initial_position;
 			Point target;
-			std::vector<std::pair<Point, std::pair<Point, Angle>>> scores;
-			std::pair<Point, std::pair<Point, Angle>> best_score;
+			std::vector<std::tuple<Point, Point, double>> scores;
+			std::tuple<Point, Point, double> best_score;
 			std::vector<Point> obstacles;
 	};
 
@@ -87,7 +88,7 @@ namespace {
 			timer++;
 		}
 
-		best_score = std::make_pair(Point(0,0), std::make_pair(Point(0,0), Angle::zero()));
+		best_score = std::make_tuple(Point(0,0), Point(0,0), 0.0);
 
 		if((world.ball().position() - player.position()).len() > Robot::MAX_RADIUS + 0.02)
 			position_reset = true;
@@ -115,20 +116,26 @@ namespace {
 		for(double x = initial_position.x - 0.50; x < initial_position.x + 0.50; x += 0.04) {
 			for(double y = initial_position.y - 0.50; y < initial_position.y + 0.50; y += 0.04) {
 				if((Point(x, y) - initial_position).len() < 0.45) {
-					scores.push_back(
-							std::make_pair(
-								Point(x, y),
-								angle_sweep_circles(
-									Point(x, y),
-									enemy_goal_positive,
-									enemy_goal_negative,
-									obstacles,
-									Robot::MAX_RADIUS
-									)
-								)
-							);
-					scores[scores.size() - 1].second.second += Angle::of_degrees(1 - (Point(x, y) - player.position()).len());
-					scores[scores.size() - 1].second.second += Angle::of_degrees(player.position().x - x);
+					std::pair<Point, Angle> sweep_score = angle_sweep_circles(
+							Point(x, y),
+								enemy_goal_positive,
+								enemy_goal_negative,
+								obstacles,
+								Robot::MAX_RADIUS
+								);
+					double shoot_score = sweep_score.second.to_degrees();
+
+					for(auto p : world.enemy_team()) {
+						if((p.position() - Point(x, y)).len() < Robot::MAX_RADIUS * 2 + 0.03)
+							shoot_score = 0.0;
+					}
+
+					if((Point(x, y) - world.field().enemy_goal()).len() < 0.3)
+						shoot_score -= (0.3 - (Point(x, y) - world.field().enemy_goal()).len())*40;
+
+					shoot_score += (8 - (Point(x, y) - player.position()).len() * 8);
+
+					scores.push_back(std::make_tuple(Point(x, y), sweep_score.first, shoot_score));
 				}
 			}
 		}
@@ -150,16 +157,12 @@ namespace {
  		} */
 
 		for(auto &i : scores) {
-			for(auto p : world.enemy_team()) {
-				if((p.position() - i.first).len() < Robot::MAX_RADIUS * 2 + 0.02)
-					i.second.second = Angle::zero();
-			}
-			if(i.second.second > best_score.second.second)
+			if(std::get<2>(i) > std::get<2>(best_score))
 				best_score = i;
 		}
 
 		target = angle_sweep_circles(
-			best_score.first, 
+			std::get<0>(best_score), 
 			world.field().enemy_goal_boundary().first, 
 			world.field().enemy_goal_boundary().second, 
 			obstacles, 
@@ -167,28 +170,29 @@ namespace {
 			).first;
 
 		if(!player.has_ball()) {
-			intercept_pivot(world, player, target);
+			intercept(player, target);
 			return;
 		}
 
 		if(timer < 10) return;
 
-		if(((best_score.first - player.position()).len() > 0.05) || 
-				((player.orientation() - (best_score.second.first - player.position()).orientation()).to_degrees() > 2.0)) {
-			move(player, Angle::of_degrees((player.orientation().to_degrees() - (best_score.second.first - player.position()).orientation().to_degrees() > 0 ?
+		/*
+		if(((std::get<0>best_score - player.position()).len() > 0.05) || 
+				((player.orientation() - (std::get<1>best_score - player.position()).orientation()).to_degrees() > 2.0)) {
+			move(player, Angle::of_degrees((player.orientation().to_degrees() - (std::get<1>best_score - player.position()).orientation().to_degrees() > 0 ?
 					-1 : 1) * 6) + player.orientation(), 
-					player.position() + (best_score.first - player.position()).norm(0.2));
+					player.position() + (std::get<0>best_score - player.position()).norm(0.2));
 			//player.flags(AI::Flags::FLAG_CAREFUL);
-			player.type(AI::Flags::MoveType::DRIBBLE);
 			return;
 		}
+		*/
 
-		/* if(((best_score.first - player.position()).len() > 0.05) || 
-				((player.orientation() - (best_score.second.first - player.position()).orientation()).to_degrees() > 2.0)) {
-			move(player, (best_score.second.first - player.position()).orientation(), best_score.first);
-			
+		if(((std::get<0>(best_score) - player.position()).len() > 0.05) || 
+				((player.orientation() - (std::get<1>(best_score) - player.position()).orientation()).to_degrees() > 2.0)) {
+			player.dribble_slow();
+			move(player, (std::get<1>(best_score) - player.position()).orientation(), std::get<0>(best_score), Point(0.01, 0.01));
 			return;
-		} */
+		}
 
 		player.autokick(BALL_MAX_SPEED);
 	}
@@ -196,13 +200,19 @@ namespace {
 	void CShootGoal::draw_overlay(Cairo::RefPtr<Cairo::Context> ctx) const {
 		ctx->set_line_width(0.025);
 		for(auto i : scores) {
-			ctx->set_source_rgba(i.second.second.to_degrees() / 15, i.second.second.to_degrees() / 15, i.second.second.to_degrees() / 15, 0.5);
-			ctx->arc(i.first.x, i.first.y, 0.02, 0.0, 2*M_PI);
+			ctx->set_source_rgba(std::get<2>(i) / 15, std::get<2>(i) / 15, std::get<2>(i) / 15, 0.5);
+			ctx->arc(std::get<0>(i).x, std::get<0>(i).y, 0.02, 0.0, 2*M_PI);
 			ctx->fill();
 		}
 		ctx->set_source_rgba(1.0, 0.0, 0.0, 1.0);
-		ctx->arc(best_score.first.x, best_score.first.y, 0.02, 0.0, 2*M_PI);
+		ctx->arc(std::get<0>(best_score).x, std::get<0>(best_score).y, 0.02, 0.0, 2*M_PI);
+		
 		ctx->fill();
+
+		ctx->set_source_rgba(1.0, 0.7, 0.7, 0.8);
+		ctx->move_to(std::get<0>(best_score).x, std::get<0>(best_score).y);
+		ctx->line_to(std::get<1>(best_score).x, std::get<1>(best_score).y);
+		ctx->stroke();
 	}
 }
 
