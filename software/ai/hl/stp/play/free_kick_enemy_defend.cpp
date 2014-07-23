2b48237f0ab1663eb892985e73fc7086c59ce8ca
skghosh <skghosh@localhost>
2014-07-23 00:43:49 +0000

    more sheilding rather than leaving open

--- software/ai/hl/stp/play/free_kick_enemy_defend.cpp
+++ software/ai/hl/stp/play/free_kick_enemy_defend.cpp
@@ -1,6 +1,7 @@
 #include "ai/hl/stp/tactic/wait_playtype.h"
 #include "ai/hl/stp/play/simple_play.h"
 #include "ai/hl/stp/tactic/shadow_kickoff.h"
+#include "ai/hl/stp/tactic/shadow_enemy.h"
 
 namespace Predicates = AI::HL::STP::Predicates;
 
@@ -30,7 +31,7 @@ roles[1].push_back(defend_duo_extra1(world));
 
 // ROLE 3
 // defend
-roles[2].push_back(defend_duo_extra2(world));
+roles[2].push_back(shadow_enemy(world, 0));
 
 // ROLE 4
 // offend
