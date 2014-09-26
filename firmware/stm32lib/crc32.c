b053242a331ac618cc2264cf438d0e9d016ea709
chead <chead@localhost>
2014-09-26 01:04:48 +0000

    Add CRC32 functionality.

---
+++ firmware/stm32lib/crc32.c
@@ -0,0 +1,57 @@
+#include <crc32.h>
+#include <registers/crc.h>
+
+/**
+ * \brief Computes the CRC32 of a block of data.
+ *
+ * The CRC is computed as though the data will be transmitted MSb-first.
+ *
+ * \warning This function is not reentrant.
+ *
+ * \param[in] data the data block to process
+ * \param[in] length the number of bytes
+ * \return the CRC32 value
+ */
+uint32_t crc32_be(const void *data, size_t length) {
+	const CRC_CR_t reset_cr = { .RESET = 1 };
+	uint32_t acc = 0;
+	const uint8_t *bptr = data;
+
+	while (length && ((uintptr_t) bptr & 3U)) {
+		CRC.CR = reset_cr;
+		asm volatile("dmb");
+		acc = acc ^ ((uint32_t) *bptr << 24U);
+		CRC.DR = (acc >> 24U) | 0xFFFFFF00U;
+		acc = (acc << 8U) ^ CRC.DR ^ 0xFFU;
+		--length;
+		++bptr;
+	}
+
+	const uint32_t *wptr = (const uint32_t *) bptr;
+	if (length >= 4U) {
+		CRC.CR = reset_cr;
+		asm volatile("dmb");
+		CRC.DR = acc ^ __builtin_bswap32(*wptr);
+		++wptr;
+		length -= 4U;
+		while (length >= 4U) {
+			CRC.DR = __builtin_bswap32(*wptr);
+			++wptr;
+			length -= 4U;
+		}
+		acc = ~CRC.DR;
+	}
+
+	bptr = (const uint8_t *) wptr;
+	while (length) {
+		CRC.CR = reset_cr;
+		asm volatile("dmb");
+		acc = acc ^ ((uint32_t) *bptr << 24U);
+		CRC.DR = (acc >> 24U) | 0xFFFFFF00U;
+		acc = (acc << 8U) ^ CRC.DR ^ 0xFFU;
+		--length;
+		++bptr;
+	}
+
+	return acc;
+}
