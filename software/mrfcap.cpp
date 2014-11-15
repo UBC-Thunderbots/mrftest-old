bc96fa8d54a984e4928eb4f0d602553b69f4b031
chead <chead@localhost>
2014-11-15 08:25:04 +0000

    Let mrfcap handle SIGINT properly. Fixes #1211.

--- software/mrfcap.cpp
+++ software/mrfcap.cpp
@@ -1,8 +1,13 @@
 #include "main.h"
 #include "mrf/constants.h"
+#include "util/exception.h"
+#include "util/fd.h"
 #include "util/libusb.h"
+#include "util/main_loop.h"
 #include "util/string.h"
+#include <cerrno>
 #include <chrono>
+#include <csignal>
 #include <cstdlib>
 #include <ctime>
 #include <fstream>
@@ -12,8 +17,12 @@
 #include <stdexcept>
 #include <string>
 #include <vector>
+#include <glibmm/main.h>
+#include <sys/signalfd.h>
 
 namespace {
+	unsigned int packets_captured = 0;
+
 	unsigned long long convert_number(const std::string &str) {
 		std::size_t end_pos = 0;
 		unsigned long long ull = std::stoull(str, &end_pos, 0);
@@ -21,6 +30,58 @@ namespace {
 			throw std::runtime_error("Whole string not consumed in number conversion");
 		}
 		return ull;
+	}
+
+	void handle_transfer_done(AsyncOperation<void> &asyncOp, std::ostream &ofs) {
+		USB::BulkInTransfer &transfer = static_cast<USB::BulkInTransfer &>(asyncOp);
+		transfer.result();
+		if (transfer.size() >= 4) {
+			if (transfer.data()[0] & 0x01) {
+				std::cout << "Warning, dropped packet!\n";
+			}
+			uint32_t seconds, microseconds;
+			{
+				auto stamp = std::chrono::system_clock::now();
+				std::time_t converted = std::chrono::system_clock::to_time_t(stamp);
+				std::chrono::duration<uint64_t, std::micro> micros = std::chrono::duration_cast<std::chrono::duration<uint64_t, std::micro>>(stamp.time_since_epoch());
+				if (static_cast<uint64_t>(converted) == micros.count() / UINT64_C(1000000)) {
+					// std::chrono::system_clock and time_t use the same units on this platform, so we can get a fractional part.
+					seconds = static_cast<uint32_t>(micros.count() / UINT64_C(1000000));
+					microseconds = static_cast<uint32_t>(micros.count() % UINT64_C(1000000));
+				} else {
+					// std::chrono::system_clock and time_t use different units on this platform, so we can only achieve second resolution.
+					seconds = static_cast<uint32_t>(converted);
+					microseconds = 0;
+				}
+			}
+
+			{
+				uint32_t u32;
+				u32 = static_cast<uint32_t>(seconds);
+				ofs.write(reinterpret_cast<const char *>(&u32), 4);
+				u32 = static_cast<uint32_t>(microseconds);
+				ofs.write(reinterpret_cast<const char *>(&u32), 4);
+				u32 = static_cast<uint32_t>(transfer.size() - 4);
+				ofs.write(reinterpret_cast<const char *>(&u32), 4);
+				ofs.write(reinterpret_cast<const char *>(&u32), 4);
+			}
+			ofs.write(reinterpret_cast<const char *>(transfer.data() + 2), static_cast<std::streamsize>(transfer.size() - 4));
+			ofs.flush();
+			std::cout << "Captured packet of length " << transfer.size() << '\n';
+			++packets_captured;
+		} else {
+			std::cout << "Bad capture size " << transfer.size() << " (must be ≥4):";
+			for (std::size_t i = 0U; i < transfer.size(); ++i) {
+				std::cout << ' ' << static_cast<unsigned int>(transfer.data()[i]);
+			}
+			std::cout << '\n';
+		}
+		transfer.submit();
+	}
+
+	bool handle_sigint(Glib::IOCondition) {
+		MainLoop::quit();
+		return false;
 	}
 }
 
@@ -51,6 +112,20 @@ int app_main(int argc, char **argv) {
 	uint16_t pan_id = static_cast<uint16_t>(convert_number(argv[3]));
 	uint64_t mac_address = static_cast<uint64_t>(convert_number(argv[4]));
 	uint16_t capture_flags = static_cast<uint16_t>(convert_number(argv[5]));
+
+	// Start listening for CTRL+C (SIGINT).
+	FileDescriptor signalfd;
+	{
+		sigset_t sigs;
+		sigemptyset(&sigs);
+		sigaddset(&sigs, SIGINT);
+		if (sigprocmask(SIG_BLOCK, &sigs, 0) < 0) {
+			throw SystemError("sigprocmask", errno);
+		}
+		int rawfd = ::signalfd(-1, &sigs, SFD_NONBLOCK);
+		signalfd = FileDescriptor::create_from_fd(rawfd);
+	}
+	Glib::signal_io().connect(&handle_sigint, signalfd.fd(), Glib::IO_IN);
 
 	// Open the dongle
 	std::cout << "Addressing dongle… ";
@@ -148,43 +223,16 @@ int app_main(int argc, char **argv) {
 	ofs.flush();
 	std::cout << "OK\n";
 
-	// Go into a loop
-	for (;;) {
-		uint8_t buffer[131];
-		std::size_t len = devh.bulk_in(1, buffer, sizeof(buffer), 0);
-		if (len >= 4) {
-			uint32_t seconds, microseconds;
-			{
-				auto stamp = std::chrono::system_clock::now();
-				std::time_t converted = std::chrono::system_clock::to_time_t(stamp);
-				std::chrono::duration<uint64_t, std::micro> micros = std::chrono::duration_cast<std::chrono::duration<uint64_t, std::micro>>(stamp.time_since_epoch());
-				if (static_cast<uint64_t>(converted) == micros.count() / UINT64_C(1000000)) {
-					// std::chrono::system_clock and time_t use the same units on this platform, so we can get a fractional part.
-					seconds = static_cast<uint32_t>(micros.count() / UINT64_C(1000000));
-					microseconds = static_cast<uint32_t>(micros.count() % UINT64_C(1000000));
-				} else {
-					// std::chrono::system_clock and time_t use different units on this platform, so we can only achieve second resolution.
-					seconds = static_cast<uint32_t>(converted);
-					microseconds = 0;
-				}
-			}
+	// Start accepting capture data.
+	USB::BulkInTransfer transfer(devh, 1, 256, false, 0);
+	transfer.signal_done.connect(sigc::bind(&handle_transfer_done, std::ref(ofs)));
+	transfer.submit();
 
-			{
-				uint32_t u32;
-				u32 = static_cast<uint32_t>(seconds);
-				ofs.write(reinterpret_cast<const char *>(&u32), 4);
-				u32 = static_cast<uint32_t>(microseconds);
-				ofs.write(reinterpret_cast<const char *>(&u32), 4);
-				u32 = static_cast<uint32_t>(len - 4);
-				ofs.write(reinterpret_cast<const char *>(&u32), 4);
-				ofs.write(reinterpret_cast<const char *>(&u32), 4);
-			}
-			ofs.write(reinterpret_cast<const char *>(buffer + 2), static_cast<std::streamsize>(len - 4));
-			ofs.flush();
-			std::cout << "Captured packet of length " << len << '\n';
-		} else {
-			std::cout << "Bad capture size " << len << " (must be ≥4)\n";
-		}
-	}
+	// Go into a main loop.
+	MainLoop::run();
+
+	// Report results.
+	std::cout << "Captured " << packets_captured << " packets.\n";
+	devh.mark_shutting_down();
+	return 0;
 }
-
