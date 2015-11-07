0b1d7d89af70b30311752b47b76fc33445d24939
chead <chead@localhost>
2015-11-07 23:48:38 +0000

    Fix bug in drive packet encoding.

--- software/mrf/robot.cpp
+++ software/mrf/robot.cpp
@@ -440,7 +440,7 @@ void MRFRobot::encode_drive_packet(void *out) {
 	uint8_t extra_encoded = static_cast<uint8_t>(extra | (slow ? 0x80 : 0x00));
 
 	words[2] = static_cast<uint16_t>(words[2] | static_cast<uint16_t>((extra_encoded & 0xF) << 12));
-	words[3] = static_cast<uint16_t>(words[2] | static_cast<uint16_t>((extra_encoded >> 4) << 12));
+	words[3] = static_cast<uint16_t>(words[3] | static_cast<uint16_t>((extra_encoded >> 4) << 12));
 
 	// Convert the words to bytes.
 	uint8_t *wptr = static_cast<uint8_t *>(out);
