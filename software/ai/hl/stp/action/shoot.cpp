bc6efe9e491f30841f2104a674c43291165f7583
evanm <evanm@localhost>
2017-07-27 09:35:23 +0000

    I don't write code; I eat D'asses

--- software/ai/hl/stp/action/shoot.cpp
+++ software/ai/hl/stp/action/shoot.cpp
@@ -14,6 +14,28 @@
 
 using namespace AI::HL::STP;
 
+void get_behind_ball(caller_t& ca, World world, Player player, Point target) {
+	double playerproj = Geom::proj_len(Geom::Seg(world.ball().position(), target), player.position());
+	double behinddist = Robot::MAX_RADIUS + 0.18;
+	Point behind_ball = world.ball().position() + (world.ball().position() - target).norm(behinddist);
+
+	if (playerproj > -behinddist + 0.10) {
+		Point dest1 = behind_ball + (world.ball().position() - target).norm(0.4).rotate(Angle::quarter());
+		Point dest2 = behind_ball + (world.ball().position() - target).norm(0.4).rotate(-Angle::quarter());
+
+		// Go to the closest position to the player out of the two sideways points from the behind-ball point
+		Point dest = (dest1 - player.position()).lensq() > (dest2 - player.position()).lensq() ? dest2 : dest1;
+
+		// Get behind the ball (relative to the target)
+		Action::move(ca, world, player, dest, (target - world.ball().position()).orientation());
+	}
+	else {
+		Point dest = behind_ball;
+		player.flags(AI::Flags::calc_flags(world.playtype()) | AI::Flags::MoveFlags::AVOID_BALL_MEDIUM);
+		player.move_move(dest, (target - world.ball().position()).orientation());
+	}
+}
+
 void AI::HL::STP::Action::shoot_goal(caller_t& ca, World world, Player player, bool chip) {
 	Point shot = Evaluation::get_best_shot(world, player);
 
@@ -27,9 +49,7 @@ void AI::HL::STP::Action::shoot_target(caller_t& ca, World world, Player player,
 	} else {
 		// Get behind the ball without hitting it
 		// TODO: account for slowly moving ball (fast ball handled by catch)
-		Point dest = world.ball().position() + (world.ball().position() - target).norm(0.21);
-		player.flags(AI::Flags::calc_flags(world.playtype()) | AI::Flags::MoveFlags::AVOID_BALL_MEDIUM);
-		player.move_move(dest, (target - world.ball().position()).orientation());
+		get_behind_ball(ca, world, player, target);
 	}
 }
 
@@ -47,9 +67,7 @@ void AI::HL::STP::Action::catch_and_shoot_target(caller_t& ca, World world, Play
 	} else {
 		// Get behind the ball without hitting it
 		// TODO: account for slowly moving ball (fast ball handled by catch)
-		Point dest = world.ball().position() + (world.ball().position() - target).norm(0.21);
-		player.flags(AI::Flags::calc_flags(world.playtype()) | AI::Flags::MoveFlags::AVOID_BALL_MEDIUM);
-		player.move_move(dest, (target - world.ball().position()).orientation());
+		get_behind_ball(ca, world, player, target);
 	}
 }
 
