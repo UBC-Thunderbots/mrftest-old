63a8f0e4efd729c273465bebfbaf10214ef02875
evanm <evanm@localhost>
2017-07-28 05:29:53 +0000

    added corner kick logic to free kick

--- software/ai/hl/stp/tactic/free_kick_to_goal.cpp
+++ software/ai/hl/stp/tactic/free_kick_to_goal.cpp
@@ -21,8 +21,8 @@ namespace Evaluation = AI::HL::STP::Evaluation;
 namespace Action = AI::HL::STP::Action;
 using namespace Geom;
 
-
 namespace {
+DoubleParam chip_power(u8"chipping power level", "AI/HL/STP/Tactic/free_kick_to_goal", 1, 0.0, 8.0);
 	class FreeKickToGoal final : public Tactic { public:
 			explicit FreeKickToGoal(World world) : Tactic(world) {
 			}
@@ -47,8 +47,34 @@ namespace {
 
 			Angle total_angle = vertex_angle(gp1 , world.ball().position(), gp2).angle_mod().abs();
 
-			//player can see 15% of the net. Will shoot on goal
-			if(Evaluation::get_best_shot_pair(world, player()).second / total_angle > 0.15){
+
+			//if the ball is really close to the far end of the field, run the corner-kick play, if not, run normal free kick play
+			if ( world.ball().position().x > ( world.field().enemy_goal().x - world.field().defense_area_radius() ) ) {
+
+				Point targetDefensiveCrease = world.field().enemy_goal();
+
+				targetDefensiveCrease.x = targetDefensiveCrease.x - world.field().defense_area_radius(); //set the target to be on the "D" of the goal crease
+
+				//check to see which side of the net to shoot on
+				if (world.ball().position().y > 0) {
+				targetDefensiveCrease.y = targetDefensiveCrease.y + 0.5;	//TODO: change this to change side depending on which side he robot is on
+				}
+				else {
+					targetDefensiveCrease.y = targetDefensiveCrease.y - 0.5;
+				}
+
+//				ALTERNATEIVE LOCATION
+//				if (world.ball().position().y > 0) {
+//					targetDefensiveCrease.y = targetDefensiveCrease.y - 0.5;	//TODO: change this to change side depending on which side he robot is on
+//				}
+//				else {
+//					targetDefensiveCrease.y = targetDefensiveCrease.y + 0.5;
+//				}
+
+
+				Action::shoot_target(ca, world, player(), targetDefensiveCrease, chip_power, true );
+
+		}  else if(Evaluation::get_best_shot_pair(world, player()).second / total_angle > 0.15) { //player can see 15% of the net. Will shoot on goal
 				Action::shoot_target(ca, world, player(), Evaluation::get_best_shot(world, player()));
 				//LOGF_INFO(u8"TARGET: %1, POWER: %2, TYPE: %3", "net", "max", "shot on goal");
 			}
