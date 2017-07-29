bec1aacd80d36f19f494e7110aa43943f5ed23f0
g0rd0n <g0rd0n@localhost>
2017-07-29 08:03:38 +0000

    improve goal line defense

--- software/ai/hl/stp/tactic/goal_line_defense.cpp
+++ software/ai/hl/stp/tactic/goal_line_defense.cpp
@@ -9,62 +9,68 @@ using namespace AI::HL::STP::Tactic;
 using namespace AI::HL::W;
 
 namespace {
-	DoubleParam dist_from_goal_factor(u8"goal line defense dist from goal factor", u8"AI/HL/STP/Action/defend", 1.25, 0.0, 8.0);
+	DoubleParam goal_line_defense_dist_from_goal_factor(u8"goal line defense dist from goal factor", u8"AI/HL/STP/Tactic/goal_line_defense", 1.15, 0.0, 10);
+	DoubleParam goal_line_defense_gap_between_defenders(u8"goal line defense gap between defenders", u8"AI/HL/STP/Tactic/goal_line_defense", AI::Common::Robot::MAX_RADIUS, 0.0, 5);
 
 	class GoalLineDefense final : public Tactic {
 		public:
-  	  explicit GoalLineDefense(World world, std::string position) : Tactic(world), position(position){
+  	  explicit GoalLineDefense(World world, std::string position) : Tactic(world), position(position),
+				dist_from_goal_factor(goal_line_defense_dist_from_goal_factor),
+				gap_between_defenders(goal_line_defense_gap_between_defenders){
   		}
 
 		private:
 			Player select(const std::set<Player> &players) const override;
+			Point getDefendPoint(std::string role) const;
 			Point getDefendPoint() const;
-			Point getDefendPointTangent() const;
 			void execute(caller_t& ca) override;
 			const std::string position;
+			const double dist_from_goal_factor;
+			const double gap_between_defenders;
 
 			Glib::ustring description() const override {
-				std::string play_name = "goal line defense";
+				std::string play_name = "goal line defense ";
 				return play_name.append(position);
 			}
 	};
 
-	# warning improve player select functions in general. Look at hungarian again?
-	Player GoalLineDefense::select(const std::set<Player> &players) const{
+	Player GoalLineDefense::select(const std::set<Player> &players) const {
   	return *std::min_element(players.begin(), players.end(), AI::HL::Util::CmpDist<Player>(getDefendPoint()));
 	}
 
-	Point GoalLineDefense::getDefendPoint() const {
-		Point ball_goal_traj;
-		Point base = position == "bottom" ? world.field().friendly_goal_boundary().first :
-			world.field().friendly_goal_boundary().second;
-		ball_goal_traj = base - world.ball().position();
+	Point GoalLineDefense::getDefendPoint(std::string role) const {
+		Point ball_goalside_traj;
+		Point goalside_traj;
 		const double goal_crease_radius = world.field().defense_area_radius() + world.field().defense_area_stretch() / 2;
-		Point goal_boundary_trag = - ball_goal_traj.norm(dist_from_goal_factor * goal_crease_radius);
-		const double friendly_outline_boundry = -world.field().length() / 2;
+		Point offset;
 
-	//	if(ball_goal_traj.len() < goal_crease_radius || ball_goal_traj.x < friendly_outline_boundry) {
-	//		Point ball_goal_center_traj = world.field().friendly_goal() - world.ball().position();
-	//		return Point(ball_goal_center_traj.x - goal_crease_radius, ball_goal_center_traj.y + world.field().defense_area_stretch() / 2);
-	//	}
+		if (role == "bottom") {
+			goalside_traj = world.field().friendly_goal_boundary().first;
+			ball_goalside_traj = goalside_traj - world.ball().position();
+		} else {
+			goalside_traj = world.field().friendly_goal_boundary().second;
+			ball_goalside_traj = goalside_traj - world.ball().position();
+		}
 
-		return base + goal_boundary_trag;
+		Point goalside_defpoint_traj = -ball_goalside_traj.norm(dist_from_goal_factor * goal_crease_radius);
+		Point ball_defpoint_traj = goalside_traj + goalside_defpoint_traj;
+
+		return ball_defpoint_traj;
 	}
 
-	Point GoalLineDefense::getDefendPointTangent() const {
-		const double goal_crease_radius = world.field().defense_area_radius() + world.field().defense_area_stretch() / 2;
-		const double gap_between_defenders = AI::Common::Robot::MAX_RADIUS * 2;
-		Point ball_goal_traj = world.field().friendly_goal() - world.ball().position();
-		Point ball_crease_traj = ball_goal_traj.norm() * (ball_goal_traj.len() - goal_crease_radius);
-
-		Point pos_offset = ball_goal_traj.perp().norm(gap_between_defenders);
-		return position == "bottom" ? ball_crease_traj - pos_offset : ball_crease_traj + pos_offset;
+	Point GoalLineDefense::getDefendPoint() const {
+		Point defend_point_this = getDefendPoint(position);
+		const Point defend_point_other = getDefendPoint(position == "top" ? "bottom" : "top");
+		const Point this_other_traj = defend_point_this - defend_point_other;
+		double offset = this_other_traj.len() > gap_between_defenders ?
+			this_other_traj.len() - gap_between_defenders:
+			0;
+		return defend_point_this = defend_point_this - this_other_traj.norm(offset);
 	}
 
 	void GoalLineDefense::execute(caller_t& ca) {
 		while(true) {
-			Point defend_point = getDefendPoint();
-		  AI::HL::STP::Action::move(ca, world, player(), defend_point, (world.ball().position() - player().position()).orientation());
+		  AI::HL::STP::Action::move(ca, world, player(), getDefendPoint(), (world.ball().position() - player().position()).orientation());
 			yield(ca);
 			player().clear_prims();
 		}
