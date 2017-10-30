e03c83c1187761d17b773ddeaa8416f61774769d
Muchen He <mansurhe@outlook.com>
2017-10-30 03:16:37 +0000

    Spin primitive and fwView

--- tools/fwsim/make.sh
+++ tools/fwsim/make.sh
@@ -1,5 +1,18 @@
 #!bin/bash
-gcc -std=c99 *.c -o sim -lm
+if gcc -std=c99 -Wall *.c -o sim -lm; then
+	if [ $? -ne 0 ]
+	then
+		echo "Compile failed!"
+		exit 1
+	fi
+else
+	echo "compile failed!"
+	exit 1
+fi
+
 ./sim > test.csv
 python show_sim.py test.csv
 #./sim
+
+# cp test.csv fwview/data/
+# bash fwview/fwView
