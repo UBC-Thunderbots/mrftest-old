6d942df0bb50f33ab83b2902bdeb39b843dcf24b
Bruce Long <brucelong92@gmail.com>
2018-02-17 18:01:36 -0800

    pivot.c added, no modification needed

--- tools/fwsim/m.sh
+++ tools/fwsim/m.sh
@@ -9,12 +9,13 @@ main.c \
 ../../firmware/main/primitives/move.c \
 ../../firmware/main/primitives/shoot.c \
 ../../firmware/main/primitives/spin.c \
+../../firmware/main/primitives/pivot.c \
 -o sim -lm
 #./sim 
 #./sim test.csv 3 1000 1000 0 0 0 0 
 #python show_sim.py test.csv
 
-#../../firmware/main/primitives/pivot.c \
+
 #../../firmware/main/primitives/jcatch.c \
 #../../firmware/main/primitives/direct_wheels.c \
 #../../firmware/main/primitives/direct_velocity.c \
