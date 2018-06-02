bf30972dca2a68ecf90e75a15635a5a4f5a62f03
Akhil Veeraghanta <akhil.veeraghanta@outlook.com>
2018-06-02 21:22:05 +0000

    Pivot Primitive

--- firmware/main/primitives/shoot.c
+++ firmware/main/primitives/shoot.c
@@ -77,8 +77,6 @@ void to_log(log_record_t *log, float time_target, float accel[3]) {
     log_time_target(log, time_target);
 }
 
-// Only need two data points to form major axis vector.
-
 /**
  * \brief Initializes the shoot primitive.
  *
@@ -87,85 +85,36 @@ void to_log(log_record_t *log, float time_target, float accel[3]) {
 static void shoot_init(void) {
 }
 
-/**
- * \brief Starts a movement of this type.
- *
- * This function runs each time the host computer requests to start a shoot
- * movement.
- *
- * \param[in] params the movement parameters, which are only valid until this
- * function returns and must be copied into this module if needed
- *	there are two shoot methods the second bit in extra byte indicate which
- *	method is called
- * 
- *       params[0] = dest.x * 1000.0;
- *       params[1] = dest.y * 1000.0;
- *       params[2] = 0.0;
- *       params[3] = power * 1000.0;
- *       extra = chip;
- *
- *	method two
- *
- *       params[0] = dest.x * 1000.0;
- *       params[1] = dest.y * 1000.0;
- *       params[2] = orientation.angle_mod().to_radians() * 100.0;
- *       params[3] = power * 1000.0;
- *       extra = static_cast<uint8_t>(2 | chip);
- *
- *	What this function do
- *	1. record the movement intent
- *	2. there is no need to worry about recording the start position 
- *	   because the primitive start function already does it
- *
- */
 static void shoot_start(const primitive_params_t *params) {
+    // Convert into m/s and rad/s because physics is in m and s
+    destination[0] = ((float) (params->params[0]) / 1000.0f);
+    destination[1] = ((float) (params->params[1]) / 1000.0f);
+    destination[2] = ((float) (params->params[2]) / 100.0f);
 
-
-	// Convert into m/s and rad/s because physics is in m and s
-	destination[0] = ((float) (params->params[0]) / 1000.0f);
-	destination[1] = ((float) (params->params[1]) / 1000.0f);
-	destination[2] = ((float) (params->params[2]) / 100.0f);
-	
-
-	// cosine and sine of orientation angle to global x axis
-	major_vec[0] = cosf(destination[2]); 
-	major_vec[1] = sinf(destination[2]);
-	minor_vec[0] = major_vec[0];
-	minor_vec[1] = major_vec[1];
-	rotate(minor_vec, P_PI / 2);
+    // cosine and sine of orientation angle to global x axis
+    major_vec[0] = cosf(destination[2]); 
+    major_vec[1] = sinf(destination[2]);
+    minor_vec[0] = major_vec[0];
+    minor_vec[1] = major_vec[1];
+    rotate(minor_vec, P_PI / 2);
 
     dr_data_t states;
     dr_get(&states);
     total_rot = min_angle_delta(destination[2], states.angle);	
 
-	// arm the chicker
+    // arm the chicker
 #ifndef FWSIM
-	chicker_auto_arm((params->extra & 1) ? CHICKER_CHIP : CHICKER_KICK, params->params[3]);
+    chicker_auto_arm((params->extra & 1) ? CHICKER_CHIP : CHICKER_KICK, params->params[3]);
 #endif
-	
+
 }
 
-/**
- * \brief Ends a movement of this type.
- *
- * This function runs when the host computer requests a new movement while a
- * shoot movement is already in progress.
- */
 static void shoot_end(void) {
 #ifndef FWSIM
-	chicker_auto_disarm();
+    chicker_auto_disarm();
 #endif
 }
-
-/**
- * \brief Ticks a movement of this type.
- *
- * This function runs at the system tick rate while this primitive is active.
- *
- * \param[out] log the log record to fill with information about the tick, or
- * \c NULL if no record is to be filled
- */
-static void shoot_tick(log_record_t *log) {
+static void shoot_tick(log_record_t *log){
     dr_data_t states;
     dr_get(&states);
     PhysBot pb = setup_bot(states, destination, major_vec, minor_vec);
@@ -192,9 +141,9 @@ static void shoot_tick(log_record_t *log) {
  * \brief The shoot movement primitive.
  */
 const primitive_t SHOOT_PRIMITIVE = {
-	.direct = false,
-	.init = &shoot_init,
-	.start = &shoot_start,
-	.end = &shoot_end,
-	.tick = &shoot_tick,
+    .direct = false,
+    .init = &shoot_init,
+    .start = &shoot_start,
+    .end = &shoot_end,
+    .tick = &shoot_tick,
 };
